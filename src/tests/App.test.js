7a407be6ba50227a20fa4efef7add558b049b206
Peter Gray <peter@petergraycreative.com>
2018-03-27 12:42:21 -0500

    Added a test to ensure child component renders inside App
    Checks for the correct number of buttons inside <Panel />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,6 +4,8 @@ import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
+import Button from '../components/Button/Button';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -11,5 +13,10 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+  it('renders 19 <Button /> components', () => {
+    const wrapper = shallow(<Panel />);
+    console.log(wrapper.find(Button));
+    expect(wrapper.find(Button)).toHaveLength(19);
   });
 });
