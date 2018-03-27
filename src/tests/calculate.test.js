10832d6db4b90d04dbd0cf8cd94c8e820186c6ab
Smithface <kylewibblesmith@gmail.com>
2018-03-27 19:12:34 -0400

    added test for isNumber

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -15,3 +15,34 @@ describe('`calculate`', () => {
     expect(newState).toHaveProperty('operation');
   });
 });
+
+function isNumber(x) {
+  return !!x.match(/[0-9]+/);
+}
+
+describe('isNumber', () => {
+  it ('should be a function', () => {
+    expect(typeof isNumber).toBe('function');
+  });
+
+  it ('should return a boolean', () => {
+    expect(typeof isNumber('banana')).toBe('boolean');
+    expect(typeof isNumber('6')).toBe('boolean');
+  });
+
+  it ('should return true for a number and false for strings', () => {
+    expect(isNumber('banana')).toBe(false);
+    expect(isNumber('40')).toBe(true);
+    expect(isNumber('6.256')).toBe(true);
+    expect(isNumber('0.256')).toBe(true);
+  });
+
+  it ('should handle negative numbers', () => {
+    expect(isNumber('-5')).toBe(true);
+    expect(isNumber('-.01')).toBe(true);
+  });
+
+  it ('should return false for infinity (dividing by 0)', () => {
+    expect(isNumber(`${6/0}`)).toBe(false);
+  });
+});
