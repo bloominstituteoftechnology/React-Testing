8bc08ea27862ce4e0e1dbe8085b4809ee730e134
Lo Saephan <teamsky916@gmail.com>
2018-03-27 10:56:44 -0700

    added test to see if state is defined in app component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
@@ -11,5 +11,9 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+  it('should have state defined', () => {
+  		const component = shallow(<App />); 
+  		expect(component.state('total', 'next', 'operation')).toBeDefined();
   });
 });
