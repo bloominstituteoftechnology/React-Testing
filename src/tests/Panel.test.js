32ff7e9adacae1c54e823220bf54688d3c4f2aa9
Igor Yermak <igor.yermak@gmail.com>
2018-03-27 17:33:58 -0400

    Button and Panel tests

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
+import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -11,5 +12,9 @@ describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
+  });
+  it('should render the buttons', () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find(Button )).toHaveLength(19);
   });
 });
