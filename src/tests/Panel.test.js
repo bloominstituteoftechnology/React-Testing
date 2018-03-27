a767ee9201ea3981ba0d290367a87003d855f0f4
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-03-27 18:08:06 -0500

    Panel Tests
    - in Panel.test.js:
    - add test to check render of 19 Button components (pass)
    - add test to check render of button with specific name and prop (fail)

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
+import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -11,5 +12,15 @@ describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
+  });
+  it('renders 19 Buttons', () => {
+    const component = shallow(<Panel />);
+    expect(component.find('Button')).toHaveLength(19);
+  });
+  it('renders a button named "0", with property "wide"', () => {
+    const wrapper = mount(<Panel />);
+    // console.log(wrapper.find({ name: '0' })).toHaveLength(1);
+    const rendered = wrapper.find('Button');
+    expect(wrapper.find({ value: 'food' }).everyWhere(n => n.prop('name') === "0"));
   });
 });
