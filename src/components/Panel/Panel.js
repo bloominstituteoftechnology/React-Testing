3a1abfce03d4b6b0bbc4185d1019df5296c65ee9
Jonathan Bry <jonathanbry@Jonathans-iMac.local>
2018-03-27 13:53:39 -0700

    Jon bry Commit Tuesday 13:52

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -11,31 +11,31 @@ class Panel extends Component {
     render() {
         return (
             <div className="component-panel">
-                <div>
+                <div className="(1)4buttonComponent">
                     <Button name="AC" clickHandler={this.handleClick} />
                     <Button name="+/-" clickHandler={this.handleClick} />
                     <Button name="%" clickHandler={this.handleClick} />
                     <Button name="÷" clickHandler={this.handleClick} orange />
                 </div>
-                <div>
+                <div className="(2)4buttonComponent">
                     <Button name="7" clickHandler={this.handleClick} />
                     <Button name="8" clickHandler={this.handleClick} />
                     <Button name="9" clickHandler={this.handleClick} />
                     <Button name="x" clickHandler={this.handleClick} orange />
                 </div>
-                <div>
+                <div className="(3)4buttonComponent">
                     <Button name="4" clickHandler={this.handleClick} />
                     <Button name="5" clickHandler={this.handleClick} />
                     <Button name="6" clickHandler={this.handleClick} />
                     <Button name="-" clickHandler={this.handleClick} orange />
                 </div>
-                <div>
+                <div className="(4)4buttonComponent">
                     <Button name="1" clickHandler={this.handleClick} />
                     <Button name="2" clickHandler={this.handleClick} />
                     <Button name="3" clickHandler={this.handleClick} />
                     <Button name="+" clickHandler={this.handleClick} orange />
                 </div>
-                <div>
+                <div className="(5)4buttonComponent">
                     <Button name="0" clickHandler={this.handleClick} wide />
                     <Button name="." clickHandler={this.handleClick} />
                     <Button name="=" clickHandler={this.handleClick} orange />
--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -12,4 +12,52 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+// THE FIVE DIV CONTAINERS
+  it('should render the first 4 button div', () => {
+    const wrapper = shallow((
+      <div>
+        <Button className="AC" />
+        <Button className="+/-" />
+        <Button className="%" />
+        <Button className="÷" />
+      </div>
+    ));
+    expect(wrapper.find(< Button />)).to.have.length(4);
+  });
+  it('should render the second 4 button div', () => {
+    const wrapper = shallow((
+      <div>
+        <Button className="7" />
+        <Button className="8" />
+        <Button className="9" />
+        <Button className="x" />
+      </div>
+    ));
+    expect(wrapper.find(< Button />)).to.have.length(4);
+  });
+  it('should render the third 4 button div', () => {
+    const wrapper = shallow(< div className="(3)4buttonComponent" />);
+    expect(wrapper.find(< Button />)).to.have.length(4);
+  });
+  it('should render the fourth 4 button div', () => {
+    const wrapper = shallow(< div className="(4)4buttonComponent" />);
+    expect(wrapper.find(< Button />)).to.have.length(4);
+  });
+  it('should render the fifth 4 button div', () => {
+    const wrapper = shallow(< div className="(5)4buttonComponent" />);
+    expect(wrapper.find(< Button />)).to.have.length(4);
+  });
+// END FIVE DIV CONTAINERS
+});
+
+
+
+
+it('should render children when passed in', () => {
+  const wrapper = shallow((
+    <MyComponent>
+      <div className="unique" />
+    </MyComponent>
+  ));
+  expect(wrapper.contains(<div className="unique" />)).to.equal(true);
 });
