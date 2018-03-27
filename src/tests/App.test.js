cc30ef8b231cdae7e4dc4ed3c10978f821db8a68
Mephestys <mephestys@gmail.com>
2018-03-27 12:56:19 -0600

    Beginning work on app.test suite - Tests for state, checks if 'total' 'next' and 'operation' are defined

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,4 +12,11 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should have a state with the properties `total`, `next`, and `operation`', () => {
+    const component = shallow(<App />);
+    expect(component.state('total')).toBeDefined();
+    expect(component.state('next')).toBeDefined();
+    expect(component.state('operation')).toBeDefined();
+  });
 });
