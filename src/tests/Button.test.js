0c583c84c84737d17dd409f403a90ed313add954
Dixie Korley <dixiekorley@gmail.com>
2018-03-27 15:44:08 -0400

    Button.test.js completed

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -12,4 +12,21 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('should render with a className of component-button wide', () => {
+    const componentButton = shallow(<Button wide/>);
+    expect(componentButton.find('.component-button')).toHaveLength(1);
+    expect(componentButton.find('.wide')).toHaveLength(1);
+  });
+  it('should render with a className of component-button orange', () => {
+    const componentButton = shallow(<Button orange/>);
+    expect(componentButton.find('.component-button')).toHaveLength(1);
+    expect(componentButton.find('.orange')).toHaveLength(1);
+  });
+  it('should have click handler working', () => {
+    const click = sinon.spy();
+    const clickFunction = shallow(<Button name="Anything" clickHandler={click} />);
+    clickFunction.find('button').simulate('click');
+    expect(click.calledOnce).toEqual(true);
+    expect(click.calledWith('Anything')).toEqual(true);
+  });
 });
