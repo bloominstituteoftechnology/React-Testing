eef6c0928cef249116d0faf20920520da37bf0fc
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-27 14:01:37 -0400

    Wrote a few tests for app.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
+    const component = shallow(<App />);
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  it('renders a component-app div', () => {
+    // const component = shallow(<App />);
+    expect(component.find('div').length).toBe(1)
+  })
+  it('renders a display tag', () => {
+    // const component = shallow(<App />);
+    expect(component.find('Display').length).toBe(1)
+  })
+  it('renders a panel tag', () => {
+    expect(component.find('Panel').length).toBe(1)
+  })
 });
