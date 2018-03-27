05d0fb3b9698996ce2e6062734401f6ff1794b90
Smithface <kylewibblesmith@gmail.com>
2018-03-27 16:34:53 -0400

    added panel.test

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
@@ -11,5 +10,18 @@ describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
+  });
+  it('should render 19 buttons', () => {
+    const component = shallow(<Panel />);
+    expect(component.find('Button')).toHaveLength(19);
+  });
+  it('should have a method called handleClick passed from props', () => {
+    const testHandleClick = function(button) {let x = 'foo'}
+    const component = shallow(<Panel clickHandler={testHandleClick} />);
+    expect(component.instance().handleClick).toBeDefined();
+  });
+  it ('should render a div with class `component-panel`', () => {
+    const component = shallow( <Panel />);
+    expect(component.find('.component-panel')).toHaveLength(1);
   });
 });
