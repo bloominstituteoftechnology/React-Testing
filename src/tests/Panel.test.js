b9c8f88937cc1ef7f09d532b589474b5ad7186c3
algorithmsrock <algorithmsrock@gmail.com>
2018-03-27 15:48:41 -0700

    Panel testing working

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
+import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -12,4 +13,19 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+  it('should have a `handleClick` event handler', () => {
+    const clickSpy = sinon.spy();
+    const component = shallow(<Panel clickHandler={clickSpy} />);
+    component.find("Button").last().simulate('click');
+   // expect(component.instance().clickHandler.toBeDefined());
+    expect(clickSpy.calledOnce).toBeTruthy;
+   }); 
+  it('should render a div with a `component-panel` class', () => {
+    const component = shallow(<Panel />);
+    expect(component.find('div.component-panel')).toHaveLength(1);
+  });
+  it ('should render 19 buttons', () => {
+    const component = shallow(<Panel />);
+    expect(component.find('Button')).toHaveLength(19);
+  })
 });
