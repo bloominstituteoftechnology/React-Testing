bd135736ac313992cab0333d29d4ad14dcc6015b
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-27 16:36:29 -0600

    Write tests for Display.js

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -8,13 +8,22 @@ import Display from '../components/Display/Display';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
+  const component = shallow(<Display />);
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
 
-  it('should return component-display exits', () => {
-    const component = shallow(<Display component-display/>);
+  it('should return if component-display exits', () => {
     expect(component.find('.component-display')).toHaveLength(1);
+  });
+
+  it('should return 1 child', () => {
+    expect(component.find('.component-display').children()).toHaveLength(1);
+  });
+
+  it('should contain a div', () => {
+    expect(component.contains(<div></div>)).toEqual(true);
   });
 });
