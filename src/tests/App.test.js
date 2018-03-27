84f1514f14da63bb3cac48c252f5898601cac877
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-27 13:28:19 -0400

    Committing so Tests Work...?
    App.css --- made it less ugly.
    
    App.test.js --- Wrote a few tests that aren't testing... says something about no changes since last commit, thought I may need to commit before the test will run...

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,4 +12,18 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  it('should render one <Display /> component', () => {
+    const component = shallow(<App />);
+    expect(component.find(Display)).to.have.length(1);
+  })
+  it('should render one <Panel /> component', () => {
+    const component = shallow(<App />);
+    expect(component.find(Panel)).to.have.length(1);
+  })
+  it('should use clickHandler on <Panel /> component', () => {
+    const clickHandler = sinon.spy();
+    const component = shallow(<App clickHandler={this.handleClick}/>);
+    component.find('button'.simulate('click'));
+    expect(clickHandler).to.have.property('callCount', 19);
+  })
 });
