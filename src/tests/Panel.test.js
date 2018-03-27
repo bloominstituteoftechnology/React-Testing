cb31b7e5fe80a019548d7a96acd3071cb9ab5a41
shobanavv <shobanavv@gmail.com>
2018-03-27 16:07:14 -0700

    Wip - Panel.tests.js

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -8,8 +8,24 @@ import Panel from '../components/Panel/Panel';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
+  const component = shallow(<Panel component-panel />)
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
+  });
+  
+  it('should return if component-display exits', () => {
+    expect(component.find('.component-panel')).toHaveLength(1);
+  });
+
+  it('should return 5 children', () => {
+    expect(component.find('.component-panel').children()).toHaveLength(5);
+  });
+
+  it('should return true for 19 buttons', () => {
+    let count = 0;
+    component.forEach(child, count += child.length );
+    console.log(count);
+    //expect(component.find('.component-panel')).toHaveLength(19);
   });
 });
