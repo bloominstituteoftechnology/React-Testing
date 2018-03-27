6b7dd6175dbece0f6191a5d5554134d4f6007979
Aaron Burk <aarondburk@gmail.com>
2018-03-27 16:57:33 -0500

    update Display.test.js
    - add test for an element with 'component-display'
    - add test for value argument

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -12,4 +12,28 @@ describe('<Display />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+  it('renders an element with `componet-display` as a class name', () => {
+    const component = shallow(<Display />);
+    expect(component.find('.component-display')).toHaveLength(1);
+  });
+  // it('renders the display with `test` value given', () => {
+  //   const component = shallow(<Display value="test"/>);
+  //   console.log(component.prop('value'))
+  //   expect(component.prop('children')).toEqual((<div>test</div>));
+  //   /**
+  //    * we console logged and found children on the component prop
+  //    * object. It rendered inside a div tag for some reason.  So 
+  //    * we found that this tests for the property on the key 'value'
+  //    */
+  // });
+  it('should render a div with value "test" and class name component-display', () => {
+    const component = shallow(<Display value='test' />);
+    expect(component.contains(
+      <div className="component-display">
+        <div>
+          test
+        </div>
+      </div>
+    )).toBeTruthy();
+  });
 });
