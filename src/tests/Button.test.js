6a67100475137cfcaadd1d6836408ba737cb5aca
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-27 16:21:13 -0400

    Wrote a couple tests for Button.test.js

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
+  const component = shallow(<Button />);
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
+  });
+  it('should exist', () => {
+    expect(component.exists());
+  });
+  it('should validate whether name is a string', () => {
+    const component = mount(<Button name="test" />);
+    expect('name' in component.type('string'));
   });
 });
