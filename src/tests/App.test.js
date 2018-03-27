1e0e88894b2add15e88e719123ad8cfe7f5c728a
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-03-27 13:06:27 -0500

    Start App Component Tests
    in App.test.js:
    add expect statement for total to be defined on state (pass)
    add expect statement for next to be defined on state (pass)
    add expect statement to include handleClick method (fail)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,8 +8,18 @@ import App from '../App';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
+  const component = shallow(<App />);  
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+  it('should store total in state', () => {
+    expect(component.state('total')).toBeDefined();
+  });
+  it('should store next in state', () => {
+    expect(component.state('next')).toBeDefined();
+  });
+  it('should contain a handleClick function', () => {
+    expect(App.prototype).toInclude('handleClick');
   });
 });
