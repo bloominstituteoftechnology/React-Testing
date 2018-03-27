e6300b50a94201bbfdaed492ce4a185032b4b903
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-27 19:19:44 -0400

    Wrote a test for Panel.test.js

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
 
@@ -11,5 +11,9 @@ describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
+  });
+  it('renders the correct number of buttons', () => {
+    const component = shallow(<Panel />);
+    expect(component.find('Button')).toHaveLength(19);
   });
 });
