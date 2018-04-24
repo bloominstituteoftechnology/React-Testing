b3172db74fe0c454101ba6c2d953dee13679d858
Juber Ahmed <a.juber@hotmail.com>
2018-04-24 18:15:01 -0400

    Completed App.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,11 +8,31 @@ import App from '../App';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  const wrapper= shallow(<App/>);
+  const wrapper = shallow(<App />);
   it('renders without crashing', () => {
-    expect(wrapper). toHaveLength(1);
+    expect(wrapper).toHaveLength(1);
   });
   it('should have two children siblings', () => {
     expect(wrapper.find('div').children()).toHaveLength(2);
-  })
+  });
+
+  it('should include all properties for the state', () => {
+    expect(wrapper.state('total')).toBeDefined();
+    expect(wrapper.state('next')).toBeDefined();
+    expect(wrapper.state('operation')).toBeDefined();
+  });
+
+  it('should have default states set', () => {
+    // expect(wrapper.state().total).toEqual("0");
+    expect(wrapper.state('total')).toEqual('0');
+    expect(wrapper.state().next).toBeNull();
+    expect(wrapper.state().operation).toBeNull();
+  });
+
+  it('should have a handleClick that passes in button name', () => {
+    const inst = wrapper.instance().handleClick('1');
+    expect(wrapper.state().next).toEqual('1');
+    expect(wrapper.state().total).toEqual('0');
+    expect(wrapper.state().operation).toBeNull();
+  });
 });
