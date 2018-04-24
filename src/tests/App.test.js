304f0a263ab215cb50ac69c34c8de97fca629a40
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-04-24 16:12:56 -0400

    getting started

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,4 +12,29 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should have total, next, and operation in state', () => {
+    const component = shallow(<App />);
+    expect(component.state().total).toBeDefined();
+    expect(component.state().next).toBeDefined();
+    // expect(component.state('operation')).toBeDefined(); // can also use
+    expect(component.state().operation).toBeDefined();
+    
+  });
+
+  it('should render a div class with component-app', () => {
+    const component = shallow(<App />);
+    expect(component.find('.component-app')).toHaveLength(1);
+  });
+
+  it('should render class Panel', () => {
+    console.log(component.find('Panel'))
+    const component = shallow(<App />);
+    expect(component.find('Panel')).toHaveLength(1);
+  });
+
+  it('should render class Display', () => {
+    const component = shallow(<App />);
+    expect(component.find('Display')).toHaveLength(1);
+  });
 });
