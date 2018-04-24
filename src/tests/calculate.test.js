49ebd0e9f2cd2b703c8d7198e13aea6273054abc
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-04-24 17:43:32 -0500

    Started the logic tests in calculate

---
+++ src/tests/calculate.test.js
@@ -0,0 +1,6 @@
+const calculate = require("../logic/calculate");
+
+describe("calculate", () => {
+  it("should reset the calc when AC is sent", () => {});
+  it("should calculate the total when = is sent", () => {});
+});
