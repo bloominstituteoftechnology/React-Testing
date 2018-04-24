137b93f6c754a7704a0a72277cd8fd5a0cedd99c
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-04-24 18:53:24 -0400

    Tiffany Robbins

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -11,5 +11,12 @@ describe('<Button />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
-  });
+
 });
+  it('should update count on state when button is clicked', () => {
+  	const wrapper =shallow(<Button/>);
+  	wrapper.find('button').simulate('onClick');
+  	expect(wrapper.state().count).toEqual();
+
+  });
+ });
