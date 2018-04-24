1960dd94f9289f01da51bd17456657febda763cb
Justin Kaseman <justinkaseman@live.com>
2018-04-24 14:38:42 -0700

    Display Tests

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Display from '../components/Display/Display';
 
@@ -11,5 +11,9 @@ describe('<Display />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
+  });
+  it('should display a value passed in as a prop', () => {
+    const wrapper = mount(<Display value={'333'} />);
+    expect(wrapper.contains(<div>333</div>)).toEqual(true);
   });
 });
