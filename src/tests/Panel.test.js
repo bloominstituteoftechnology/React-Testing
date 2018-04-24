231e99ace918d60f7d68e59248e01c81901c03c6
Cristina Laryea <crispad06@gmail.com>
2018-04-24 19:29:35 -0400

    added some tests for button, display, and panel

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -7,9 +7,14 @@ import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+describe("<Panel />", () => {
+  it("renders without crashing", () => {
+      const wrapper = shallow(<Panel />);
+      expect(wrapper).toHaveLength(1);
+  });
+
+  it("has two children", () => {
+      const wrapper = shallow(<Panel />);
+      expect(wrapper.find("div").children()).toHaveLength(24);
   });
 });
