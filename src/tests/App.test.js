a3126345491aaae5dd93236b03d3c6e3fcf4ac31
unknown <luisd824@gmail.com>
2018-04-24 17:42:19 -0400

    app test done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,4 +12,19 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  it('should have one children to render', () => {
+    const wrapper = shallow(<App />);
+    // console.log(wrapper);
+    expect(wrapper).toHaveLength(1);
+  });
+  it('should have 2 children components to render', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div').children()).toHaveLength(2);
+  });
+
+  it('should have totals initial state of 0 string', () => {
+    const wrapper = shallow(<App />);
+    // console.log(wrapper.state());
+    expect(wrapper.state().total).toEqual('0');
+  });
 });
