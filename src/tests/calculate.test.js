ddd5744adf1b8916aaac7114fade5d6a727d9d51
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-04-24 18:03:07 -0500

    Finsihed calculate test

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,6 +1,13 @@
-const calculate = require("../logic/calculate");
+import calculate from "../logic/calculate";
 
 describe("calculate", () => {
-  it("should reset the calc when AC is sent", () => {});
-  it("should calculate the total when = is sent", () => {});
+  it("should reset the calc when AC is sent", () => {
+    const input = { total: "57", next: "23", operation: "X" };
+    const result = calculate(input, "AC");
+    expect(result).toEqual({ total: "0", next: null, operation: null });
+  });
+  it("should calculate the total when = is sent", () => {
+    const input = { total: "15", next: "23", operation: "x" };
+    const result = calculate(input, "=");
+  });
 });
