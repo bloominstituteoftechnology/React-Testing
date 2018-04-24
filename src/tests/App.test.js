4c3bbe3a2a1e93586d47aaebfc743fd6753e67e5
habib1234731 <habibrehman1234731@gmail.com>
2018-04-24 15:07:11 -0400

    inital commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
+  const wrapper= shallow(<App/>);
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    expect(wrapper). toHaveLength(1);
   });
+  it('should have two children siblings', () => {
+    expect(wrapper.find('div').children()).toHaveLength(2);
+  })
 });
