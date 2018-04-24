086625f0bea3fcb1145a916f18ee7a8488be0ea0
Edward Manda <lilkups@yahoo.com>
2018-04-24 17:53:14 -0500

    Edward Manda - React Testing WIP

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -8,8 +9,16 @@ import App from '../App';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
+  const wrapper = Enzyme.shallow(<App />);
+
+  // tests
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('should have two children siblings', () => {
+    expect(wrapper.find('div').children()).toHaveLength(2);
   });
 });
