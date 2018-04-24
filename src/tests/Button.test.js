d66f5a1428c3c1fc2d454657259659beb75b1f0f
Justin Kaseman <justinkaseman@live.com>
2018-04-24 14:57:42 -0700

    Button tests

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
 
@@ -11,5 +11,21 @@ describe('<Button />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
+  });
+  it('calls a callback on click with the arguement of name prop', () => {
+    const onButtonClick = sinon.spy();
+    const component = shallow(
+      <Button name="dog" clickHandler={onButtonClick} />
+    );
+    component.find('button').simulate('click');
+    expect(onButtonClick.firstCall.args[0]).toEqual('dog');
+  });
+  it('wide', () => {
+    const component = shallow(<Button name="dog" wide />);
+    expect(component.find('.wide')).toHaveLength(1);
+  });
+  it('orange', () => {
+    const component = shallow(<Button name="dog" orange />);
+    expect(component.find('.orange')).toHaveLength(1);
   });
 });
