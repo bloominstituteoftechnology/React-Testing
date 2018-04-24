849319517fa6397ea7fee24f94b28fec9327ba92
zackhitch <zackhitch@users.noreply.github.com>
2018-04-24 12:41:50 -0700

    WIP - Zack Hitchcock

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -12,4 +12,8 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('renders without crashing, checking with wrapper', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper).toHaveLength(1);
+  });
 });
--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -12,4 +12,8 @@ describe('<Display />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+  it('redners without crashing, using wrapper', () => {
+    const wrapper = shallow(<Display />);
+    expect(wrapper).toHaveLength(1);
+  });
 });
