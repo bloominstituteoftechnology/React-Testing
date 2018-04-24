50ce48bc48fc65d85d3d18be3354b2f0e3049f93
arcadia-ego <arcadia-ego@github.com>
2018-04-24 16:00:14 -0700

    button and app test added

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,10 +6,13 @@ import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
-
+const wrapperApp = shallow(<App />);
 describe('<App />', () => {
-  it('renders without crashing', () => {
+  it('renders with crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  it('should have two children', () => {
+    expect(wrapperApp.find('div').children()).toHaveLength(2);
+  })
 });
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
+import App from '../App.js';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -12,4 +13,10 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('should trigger onclick events', () => {
+    const onClick = sinon.spy();
+    const wrapperButton = shallow(<Button clickHandler={onClick} />);
+    wrapperButton.find('button').prop('onClick')();
+    expect(onClick.calledOnce).toEqual(true);
+  })
 });
