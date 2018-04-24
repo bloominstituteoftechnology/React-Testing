1db812ca126f11c4c83a8f80df0d9882df7aa3de
Justin Kaseman <justinkaseman@live.com>
2018-04-24 14:27:27 -0700

    Panel Tests

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
 
@@ -11,5 +11,15 @@ describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
+  });
+  it('should render 19 buttons', () => {
+    const wrapper = render(<Panel />);
+    expect(wrapper.find('Button')).toHaveLength(19);
+  });
+  it('each button calls a callback on click', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = mount(<Panel clickHandler={onButtonClick} />);
+    wrapper.find('button').forEach(button => button.simulate('click'));
+    expect(onButtonClick.callCount).toEqual(19);
   });
 });
