8c93521d786b3e57032ca146f8f30b568f4f3fce
Cesar Mejia Leiva <cmejia@gmail.com>
2018-04-25 05:53:33 -0700

    refactoring

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import sinon from 'sinon';
-import Operate from '../logic/operate.js';
+import operate from '../logic/operate.js';
 
 describe('operate', () => {
   it("'+' should add two values together", () => {
-    const operateTest = Operate(2, 3, '+');
+    const operateTest = operate(2, 3, '+');
     expect(operateTest).toBe('5');
   });
   it("'-' should subtract one value from another", () => {
-    const operateTest = Operate(2, 3, '-');
+    const operateTest = operate(2, 3, '-');
     expect(operateTest).toBe('-1');
   });
   it("'x' should multiply two values", () => {
-    const operateTest = Operate(2, 3, 'x');
+    const operateTest = operate(2, 3, 'x');
     expect(operateTest).toBe('6');
   });
   it("'÷' should divide one value from another", () => {
-    const operateTest = Operate(4, 2, '÷');
+    const operateTest = operate(4, 2, '÷');
     expect(operateTest).toBe('2');
   });
   it("'%' should return the modulo of two values", () => {
-    const operateTest = Operate(4, 2, '%');
+    const operateTest = operate(4, 2, '%');
     expect(operateTest).toBe('0');
   });
-  it("'asdf' should return an error", () => {
-    const operation = 'undefinedOperation';
+  it("'**' should return an error", () => {
+    const operation = '**';
     expect(() => {
-      Operate(4, 2, 'undefinedOperation');
+      operate(4, 2, 'undefinedOperation');
     }).toThrowError(`Unknown operation ${operation}`);
   });
 });
