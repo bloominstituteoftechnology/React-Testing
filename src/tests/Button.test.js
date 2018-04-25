3046c0244d2ee00274dddb117479ee1ae8df5185
Shaun Kelly <Shaun@redskelly.local>
2018-04-25 00:02:38 -0400

    setup of test suites

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -9,7 +9,29 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+    const wrapper = shallow(<Button />);
+    expect(wrapper).toHaveLength(1);
+  });
+  // via Enzyme docs: API Reference > Shallow Rendering > children() && simulate()
+  // Notice the object w/in `shallow()` ex for help w/ .orange test and .wide test
+  it('returns a div with one child', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.find('div').children()).toHaveLength(1);
+  });
+
+  it('renders an orange-colored button', () => {
+    const wrapper = shallow(<Button orange />);
+    expect(wrapper.find('.component-button.orange')).toHaveLength(1);
+  });
+  it('renders a wider button', () => {
+    const wrapper = shallow(<Button wide />);
+    expect(wrapper.find('.component-button.wide')).toHaveLength(1);
+  });
+  // simulate()
+  it('calls click event once per click', () => {
+    const onClick = sinon.spy();
+    const wrapper = shallow(<Button clickHandler={onClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onClick.calledOnce).toEqual(true);
   });
 });
--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -9,7 +9,16 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+    const wrapper = shallow(<Panel />);
+    expect(wrapper).toHaveLength(1);
+  });
+  it('returns className .component-panel with five children', () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find('.component-panel').children()).toHaveLength(5);
+  });
+  // study differences between above and below blocks, note similar reference location
+  it("returns className .component-panel's divs & buttons", () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find('div').children()).toHaveLength(24);
   });
 });
