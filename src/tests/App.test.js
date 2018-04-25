8c97c0ce236823f38941aef700120f64506e8458
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-04-25 12:27:52 -0400

    App and Button test complete

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -13,22 +14,22 @@ describe('<App />', () => {
     ReactDOM.render(<App />, div);
   });
 
-  it('should have total, next, and operation in state', () => {
+  it('should have total, next, and operation fields in state', () => {
     const component = shallow(<App />);
+    // expect(component.state('total')).toBeDefined(); // can also use
     expect(component.state().total).toBeDefined();
     expect(component.state().next).toBeDefined();
-    // expect(component.state('operation')).toBeDefined(); // can also use
     expect(component.state().operation).toBeDefined();
     
   });
 
   it('should render a div class with component-app', () => {
     const component = shallow(<App />);
+    console.log(component.find('.component-app').length)
     expect(component.find('.component-app')).toHaveLength(1);
   });
 
   it('should render class Panel', () => {
-    console.log(component.find('Panel'))
     const component = shallow(<App />);
     expect(component.find('Panel')).toHaveLength(1);
   });
