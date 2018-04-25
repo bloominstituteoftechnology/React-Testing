e575488b8277f67cfd491fabde4759adbc0ed22f
nourmhassan <nour-pop1@hotmail.com>
2018-04-25 08:39:14 -0700

    done without stretch

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
+  const wrapper = shallow(<App />);
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    expect(wrapper).toHaveLength(1);
+  });
+  it('should have two children siblings', () => {
+    expect(wrapper.find('div').children()).toHaveLength(2);
+  })
+  it('should include all properties for the state', () => {
+    expect(wrapper.state('total')).toBeDefined();
+    expect(wrapper.state('next')).toBeDefined();
+    expect(wrapper.state('operation')).toBeDefined();
+  });
+  it('should have default states set', () => {
+    // expect(wrapper.state().total).toEqual("0");
+    expect(wrapper.state('total')).toEqual('0');
+    expect(wrapper.state().next).toBeNull();
+    expect(wrapper.state().operation).toBeNull();
+  });
+
+  it('should have a handleClick that passes in button name', () => {
+    const inst = wrapper.instance().handleClick('1');
+    expect(wrapper.state().next).toEqual('1');
+    expect(wrapper.state().total).toEqual('0');
+    expect(wrapper.state().operation).toBeNull();
   });
 });
+ 
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -8,8 +8,39 @@ import Button from '../components/Button/Button';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
+  const wrapper = shallow(<Button />);
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
+  });
+  it('should click', () => {
+    const myFunction = jest.fn();
+    const wrapper = shallow(<Button clickHandler={myFunction} />);
+
+    wrapper.find('button').simulate('click'); // same as above
+    wrapper.find('button').simulate('click');
+    expect(myFunction).toHaveBeenCalledTimes(2);
+    // expect(wrapper.state().count).toEqual(2);
+  });
+
+  it('should  return component-button ', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.find('.component-button').exists()).toBeTruthy();
+    expect(wrapper.find('.wide').exists()).toBeFalsy();
+    expect(wrapper.find('.orange').exists()).toBeFalsy();
+  });
+
+  it('should  return props.orange ', () => {
+    const wrapper = shallow(<Button orange />);
+    expect(wrapper.find('.orange').exists()).toBeTruthy();
+    expect(wrapper.find('.wide').exists()).toBeFalsy();
+    expect(wrapper.find('.component-button').exists()).toBeTruthy();
+  });
+
+  it('should  return props.wide ', () => {
+    const wrapper = shallow(<Button wide />);
+    expect(wrapper.find('.wide').exists()).toBeTruthy();
+    expect(wrapper.find('.orange').exists()).toBeFalsy();
+    expect(wrapper.find('.component-button').exists()).toBeTruthy();
   });
 });
