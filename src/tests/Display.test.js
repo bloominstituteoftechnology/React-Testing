6fb7e41bbe675ccde35751c147165779bfdabd1d
Velsu <riftmage50@gmail.com>
2018-05-22 22:44:56 +0200

    Completed tests for Display component

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Display from '../components/Display/Display';
+import React from "react";
+import ReactDOM from "react-dom";
+import sinon from "sinon";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Display from "../components/Display/Display";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Display />, div);
+  });
+  it("should return a value", () => {
+    const wrapper = shallow(<Display value={"10"} />);
+    expect(wrapper.contains("10")).toEqual(true);
+  });
+  it("should display div with class component-display", () => {
+    const wrapper = shallow(<Display />);
+    expect(wrapper.find(".component-display")).toHaveLength(1);
   });
 });
--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Panel from '../components/Panel/Panel';
+import React from "react";
+import ReactDOM from "react-dom";
+import sinon from "sinon";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Panel from "../components/Panel/Panel";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Panel />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Panel />, div);
+  });
+  it("should contain a wrapping div named component-panel", () => {
+    const wrapper = shallow(<Panel />);
+    expect(
+      wrapper
+        .find("div")
+        .first()
+        .hasClass("component-panel")
+    ).toEqual(true);
+  });
+  it("should have 5 sibling children divs", () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find("div").children("div")).toHaveLength(5);
+  });
+  it("should have 19 Buttons", () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find("div").children("Button")).toHaveLength(19);
   });
 });
