37ccea54f70ac1aaf70564dc7d6046dc15b79196
Henry K <mykim1891@hotmail.com>
2018-05-23 14:21:38 -0700

    Some basic test set for App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import sinon from "sinon";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<App />", () => {
+  const component = shallow(<App />);
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<App />, div);
+  });
+
+  it("should set total: '0' when component mounts ", () => {
+    const instance = component.instance();
+    expect(instance.state.total).toEqual("0");
+  });
+
+  it("should setState to appropriate value when button is pushed", () => {
+    const instance = component.instance();
+
+    instance.handleClick("AC");
+    expect(instance.state.operation).toEqual(null);
+
+    instance.handleClick("7");
+    expect(instance.state.next).toEqual("7");
+
+    instance.handleClick(".");
+    expect(instance.state.next).toEqual("7.");
+
+    instance.handleClick("2");
+    expect(instance.state.next).toEqual("7.2");
+
+    instance.handleClick("+");
+    expect(instance.state.total).toEqual("7.2");
+
+    instance.handleClick("2");
+    expect(instance.state.next).toEqual("2");
+
+    instance.handleClick("=");
+    expect(instance.state.next).toEqual(null);
+    expect(instance.state.total).toEqual("9.2");
   });
 });
