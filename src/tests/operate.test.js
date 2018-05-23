d383b632dc7de26e428900513be501bf1edda19d
unknown <bsa6.23.94@gmail.com>
2018-05-23 16:39:22 -0400

    something

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -5,9 +5,29 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import operate from '../logic/operate';
 
-describe('<Operate />', () => {
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<operate />, div);
+describe('operate reducer', () => {
+    test('should handle +', () => {
+        const params = operate(1, 2, '+');
+        expect(params).toBe('3');
+    });
+    test('should handle -', () => {
+        const params = operate(10, 2, '-');
+        expect(params).toBe('8');
+    });
+    test('should handle x', () => {
+        const params = operate(1, 2, 'x');
+        expect(params).toBe('2');
+    });
+    test('should handle ÷', () => {
+        const params = operate(1, 2, '÷');
+        expect(params).toBe('0.5');
+    });
+    test('should handle %', () => {
+        const params = operate(1, 2, '%');
+        expect(params).toBe('1');
+    });
+    test('should handle error', () => {                                                                                                                                                                                                                                                                                                                                                                             const johnson = '\n▄██████████████▄▐█▄▄▄▄█▌\n██████▌▄▌▄▐▐▌███▌▀▀██▀▀\n████▄█▌▄▌▄▐▐▌▀███▄▄█▌\n▄▄▄▄▄██████████████';
+        const params = operate(1, 2, johnson);
+        expect(params).isEmpty();
     });
 });
