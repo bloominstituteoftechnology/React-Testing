dcb4e3c4bf56891a162155261db6cba396c0ab5c
2940cristian <2940cristian@gmail.com>
2018-05-23 13:09:09 -0700

    Added two tests for button.test.js

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
 
@@ -9,9 +9,16 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
   it('Should return correct button name ', () => {
-    const wrapper = shallow(<Button/>)
-    wrapper.simulate('click');
-    expect(sinon.spy().calledOnce).toBe(false);
-
+    const wrapper = shallow(
+    <Button name={"AC"} orange={true} wide={true} clickHandler={() => {}}/>
+  );
+  expect(wrapper).toMatchSnapshot()
   });
+
+  it("Button renders correctly", () => {
+    const wrapper = render(
+      <Button name={"AC"} orange={true} wide={true} clickHandler={() => {}}/>
+    );
+    expect(wrapper).toMatchSnapshot()
+  })
 });
