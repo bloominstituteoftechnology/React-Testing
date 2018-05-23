b79751179489ad84c3e9b935dfeba537ccec5efe
Axhon <axhonruiz@gmail.com>
2018-05-23 13:11:46 -1000

    added button text test

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -12,26 +12,42 @@ describe('<Button />', () => {
     ReactDOM.render(<Button />, div);
   });
 
-  it('calls the clickhandler when clicked', () => {
-    const handlerMock = jest.fn()
-    const wrapper = shallow(<Button clickHandler={handlerMock} />)
-    wrapper.find('button').simulate('click')
-    expect(handlerMock.mock.calls.length).toBe(1)
+  it('displays its name', () => {
+    const Name = 'Steve'
+    const wrapper = shallow(<Button name={Name} />)
+    expect(wrapper.render().text()).toBe(Name)
   })
-
+  
   it('can be an orange button', () => {
     const wrapper = shallow(<Button orange />)
     expect(wrapper.hasClass('orange')).toBeTruthy()
   })
-
+  
   it('can be a wide button', () => {
     const wrapper = shallow(<Button wide />)
     expect(wrapper.hasClass('wide')).toBeTruthy()
   })
-
+  
   it('can be an orange AND wide button', () => {
     const wrapper = shallow(<Button orange wide />)
     expect(wrapper.hasClass('orange')).toBeTruthy()
     expect(wrapper.hasClass('wide')).toBeTruthy()
+  })
+
+  describe('click handler', () => {
+    it('calls the clickhandler when clicked', () => {
+      const handlerMock = jest.fn()
+      const wrapper = shallow(<Button clickHandler={handlerMock} />)
+      wrapper.find('button').simulate('click')
+      expect(handlerMock.mock.calls.length).toBe(1)
+    })
+
+    it('calls the clickhandler with the name prop', () => {
+      const handlerMock = jest.fn()
+      const Name = 'bill'
+      const wrapper = shallow(<Button name={Name} clickHandler={handlerMock} />)
+      wrapper.find('button').simulate('click')
+      expect(handlerMock).toBeCalledWith(Name)
+    })
   })
 });
