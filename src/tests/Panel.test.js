51c6b4cd79093b2b8d99ee27450cfc06ad1036da
Nedu Robert <nedumrobert@gmail.com>
2018-05-23 23:49:42 +0100

    wrote tests for the Panel component

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
 
@@ -11,5 +11,25 @@ describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
+  });
+
+  it('should render a parent div tag with a class of component-panel', () => {
+    const component = shallow(<Panel />);
+    expect(component
+        .find('div')
+        .first()
+      .hasClass('component-panel')).toEqual(true);
+  });
+
+  it('should render all buttons', () => {
+    const component = shallow(<Panel />);
+    expect(component.find('Button')).toHaveLength(19);
+  });
+
+  it('should call the clickHandler function', () => {
+    const fn = jest.fn();
+    const component = mount(<Panel clickHandler={fn} />);
+    component.find(`[name='9'] button`).simulate('click');
+    expect(fn).toHaveBeenCalledTimes(1);
   });
 });
