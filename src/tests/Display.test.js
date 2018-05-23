e0056201da56d6ca7e0fa4323e1d8784aa671884
harrisonbrock <harrison@harrisonbrock.com>
2018-05-23 17:16:00 -0400

    Added test case to test is div had correct class name in display

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -8,10 +8,17 @@ import Display from '../components/Display/Display';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
-  it('renders without crashing', () => {
+    it('renders without crashing', () => {
+        shallow(<Display/>);
+    });
 
-      it('renders without crashing', () => {
-          shallow(<Display/>);
-      });
-  });
+    it('should contain a wrapping div with the class name component-display', () => {
+        const wrapper = shallow(<Display/>);
+        expect(
+            wrapper
+                .find('div')
+                .first()
+                .hasClass('component-display')
+        ).toEqual(true)
+    });
 });
