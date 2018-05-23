eca39bd8235daad558ac51cfbdea9d35fa6e80e3
cgoez <goez2001@gmail.com>
2018-05-23 15:12:59 -0700

    Panel.test.js test see if 19 buttons rendered

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Panel from '../components/Panel/Panel';
+import React from "react";
+import ReactDOM from "react-dom";
+import sinon from "sinon";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Panel from "../components/Panel/Panel";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Panel />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Panel />, div);
+  });
+
+  it("renders all 19 Button components", () => {
+    const wrapper = shallow(<Panel />);
+    const buttons = wrapper.find("Button");
+
+    expect(buttons.length).toBe(19);
   });
 });
