a3e682b4c70483e6b64947b2cfcbf2ba76387ed1
Nedu Robert <nedumrobert@gmail.com>
2018-05-23 23:26:35 +0100

    wrote tests for the Display component

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -12,4 +12,27 @@ describe('<Display />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+
+  it('should render two div tags', () => {
+    const component = shallow(<Display />);
+    expect(component.find('div')).toHaveLength(2);
+  })
+
+  it('should render a parent div tag with a class of component-display', () => {
+    const component = shallow(<Display />);
+    expect(component.find('div').first().hasClass('component-display')).toEqual(true); 
+  })
+
+  it('should render a secondary div tag', () => {
+    const component = shallow(<Display />);
+    expect(component.find('.component-display').children('div')).toHaveLength(1);
+  })
+
+  it('renders the value correctly', () => {
+    const component = shallow(<Display value='10' />);
+    expect(component
+        .find('.component-display')
+        .children('div')
+        .text()).toEqual('10');
+  })
 });
