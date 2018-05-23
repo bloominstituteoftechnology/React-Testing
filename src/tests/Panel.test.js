a90e023ad561afa4a0b9fafe258f775080dc9c44
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-05-23 18:15:21 -0500

    tests done

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -12,4 +12,8 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+  it('should render 19 buttons', () => {
+    const component = shallow(<Panel />);
+    expect(component.find('Button')).toHaveLength(19);
+  });
 });
