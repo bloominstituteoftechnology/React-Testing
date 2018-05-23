ea4c718e4e835ed21f4de09265fc89cf1304de14
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-23 16:00:28 -0700

    Added test code for Button component

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -13,3 +13,30 @@ describe('<Button />', () => {
     ReactDOM.render(<Button />, div);
   });
 });
+
+//My Test Code
+it("Pass button `C` for clear on click", () => {
+  const func = sinon.spy();
+  const component = shallow(<Button name="C" clickHandler={func} />);
+  component.find('button').simulate('click');
+
+  expect(func.calledOnce).toEqual(true);
+  expect(func.calledWith('C')).toEqual(true);
+});
+//----------------------------------------------------------------------------------------------
+
+it('Pass button component when orange prop is passed', () => {
+  const component = shallow(<Button orange/>);
+
+  expect(component.find('.orange').exists()).toBe(true);
+  expect(component.find('.component-button').exists()).toBe(true);
+  expect(component.find('.wide').exists()).toBe(false);
+});
+//----------------------------------------------------------------------------------------------
+
+it('Pass component-button wide when passing wide prop', () => {
+  const component = shallow(<Button wide/>);
+
+  expect(component.find('.component-button').exists()).toBe(true);
+  expect(component.find('.wide').exists()).toBe(true);
+});
