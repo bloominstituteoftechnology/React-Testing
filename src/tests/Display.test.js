5864f3c98976579f6fc8352434615a29e316d051
harrisonbrock <harrison@harrisonbrock.com>
2018-05-23 17:10:01 -0400

    Added test case to test not crashing for display

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -9,7 +9,9 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
+
+      it('renders without crashing', () => {
+          shallow(<Display/>);
+      });
   });
 });
