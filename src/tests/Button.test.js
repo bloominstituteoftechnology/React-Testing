32b9700ff5e6c7b3e51e7f50fcc49f1997dc18e8
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-05-23 16:10:47 -0700

    React-Testing Faulkenberry

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -8,8 +8,16 @@ import Button from '../components/Button/Button';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
+  const wrapper = shallow(<Button name='hello world' clickHandler={() => {wrapper.setProps({name:'hello'})}}/>);
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('return one child',()=>{
+    expect(wrapper).toHaveLength(1);
+  })
+  it ('test', () => {
+    wrapper.find('button').simulate('click')
+    expect(wrapper.instance().props.name).toBe('hello');
+  })
 });
