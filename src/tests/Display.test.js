d568e4a44dc718fb4bbf8ff34c1eb917b2cbfe5f
Yasin Shuman <email@yasin.io>
2018-05-23 15:58:29 -0700

    finsihed testing for display but i was trying to figure out how to test for one div instead of two and with a specific classname for the div but i couldnt get it like i did with the buttons

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Display from '../components/Display/Display';
+import React from "react";
+import ReactDOM from "react-dom";
+import sinon from "sinon";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Display from "../components/Display/Display";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Display />, div);
+  });
+  it("should display as a value in the div of whatever is passed to it in props ", () => {
+    const component = shallow(<Display value={"125.33"} />);
+    expect(component.contains(<div>125.33</div>)).toEqual(true);
+  });
+  it("should should contain two divs", () => {
+    const component = shallow(<Display />);
+    expect(component.find("div")).toHaveLength(2);
   });
 });
