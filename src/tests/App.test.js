2da17026b90ee4833c041903aca58784dda17bd8
Justin Hong <jhong2009@gmail.com>
2018-05-23 13:41:53 -0700

    installed dependencies and set up basic test in Button.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import sinon from "sinon";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
+describe("<App />", () => {
+	it("renders without crashing", () => {
+		const div = document.createElement("div");
+		ReactDOM.render(<App />, div);
+	});
 });
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Button from '../components/Button/Button';
+import React from "react";
+import ReactDOM from "react-dom";
+import sinon from "sinon";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "../App.js";
+import Button from "../components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
+describe("<Button />", () => {
+	// test if button renders without crashing
+	it("renders without crashing", () => {
+		const div = document.createElement("div");
+		ReactDOM.render(<Button />, div);
+	});
+	// test if button triggers this.props.handleClick
+	// test if button changes this.props.name
+	// test if button changes Display props
+	it("should change Display props", () => {
+		// arrange
+		const Btn = shallow(<Button />);
+		const AppComponent = shallow(<App />);
+		const instance = AppComponent.instance();
+		let prevState = instance.state;
+		const button = Btn.find(".component-button");
+		// act
+		button.simulate("click");
+		// assert
+		expect(instance.state).toEqual(prevState);
+	});
 });
