030729c45214bae9046abdef83491dce089a3e25
Justin Hong <jhong2009@gmail.com>
2018-05-23 14:15:30 -0700

    in Button.test.js: set up test to make sure handleClick() is triggered onclick

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -4,6 +4,7 @@ import sinon from "sinon";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import App from "../App.js";
+import Display from "../components/Display/Display";
 import Button from "../components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -14,19 +15,29 @@ describe("<Button />", () => {
 		const div = document.createElement("div");
 		ReactDOM.render(<Button />, div);
 	});
+	// test if button renders its name
+	// it("should have a button name");
 	// test if button triggers this.props.handleClick
-	// test if button changes this.props.name
-	// test if button changes Display props
-	it("should change Display props", () => {
-		// arrange
-		const Btn = shallow(<Button />);
-		const AppComponent = shallow(<App />);
-		const instance = AppComponent.instance();
-		let prevState = instance.state;
-		const button = Btn.find(".component-button");
-		// act
+	it("should trigger this.props.handleClick", () => {
+		const handleClick = jest.fn();
+		const Btn = shallow(<Button clickHandler={handleClick} />);
+		const instance = Btn.instance();
+		const button = Btn.find("button");
+
 		button.simulate("click");
-		// assert
-		expect(instance.state).toEqual(prevState);
+
+		expect(handleClick).toHaveBeenCalledTimes(1);
 	});
+	// test if button changes this.props.name
+
+	// test if button changes Display props
+
+	// it("should change Display props", () => {
+	// 	// arrange
+
+	// 	// act
+	// 	button.simulate("click");
+	// 	// assert
+	// 	expect(instance.state).toEqual(prevState);
+	// });
 });
