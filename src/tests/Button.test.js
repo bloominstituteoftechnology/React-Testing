57bbae04ed1d7ddc8294f6a58aed626d508592d2
David Finley <dfinley2917@gmail.com>
2018-05-23 18:01:18 -0400

    button testing complete

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -8,8 +8,28 @@ import Button from '../components/Button/Button';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Button />, div);
+	});
+	it('should  return component-button ', () => {
+		const wrapper = shallow(<Button />);
+		expect(wrapper.find('.component-button').exists()).toBeTruthy();
+		expect(wrapper.find('.wide').exists()).toBeFalsy();
+		expect(wrapper.find('.orange').exists()).toBeFalsy();
+	});
+
+	it('should  return props.orange ', () => {
+		const wrapper = shallow(<Button orange />);
+		expect(wrapper.find('.orange').exists()).toBeTruthy();
+		expect(wrapper.find('.wide').exists()).toBeFalsy();
+		expect(wrapper.find('.component-button').exists()).toBeTruthy();
+	});
+
+	it('should  return props.wide ', () => {
+		const wrapper = shallow(<Button wide />);
+		expect(wrapper.find('.wide').exists()).toBeTruthy();
+		expect(wrapper.find('.orange').exists()).toBeFalsy();
+		expect(wrapper.find('.component-button').exists()).toBeTruthy();
+	});
 });
