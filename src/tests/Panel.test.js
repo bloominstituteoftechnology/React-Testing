b08b193165fee87bbd10beff945249a25c658e68
Axhon <axhonruiz@gmail.com>
2018-05-23 20:24:26 -1000

    first panel test

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Panel from '../components/Panel/Panel';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Panel from '../components/Panel/Panel'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('<Panel />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
-  });
-});
+    const div = document.createElement('div')
+    ReactDOM.render(<Panel />, div)
+  })
+
+  it('has 19 buttons', () => {
+    const wrapper = shallow(<Panel />)
+    expect(wrapper.find('Button').length).toBe(19)
+  })
+
+  // it('calls the clickhandler', () => {
+  //   const mockHandler = jest.fn()
+  //   const wrapper = shallow(<Panel clickHandler={mockHandler} />)
+  //   expect(
+  //     wrapper
+  //       .find('Button')
+  //       .first()
+  //       .props().clickHandler
+  //   ).toBe(mockHandler)
+  // })
+})
