1dc43749c2a93a78e776e20e3664681a99fd1d4e
Mark Wong <fmarkwong@gmail.com>
2018-05-24 01:38:50 -0700

    app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,4 +12,44 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should not differ from snapshot', () => {
+    const app = shallow(<App />);
+
+    expect(app).toMatchSnapshot();
+  });
+
+  it('renders display and panel', () => {
+    const app = shallow(<App />); 
+    const display = app.find('Display');
+    const panel = app.find('Panel');
+
+    expect(display.length).toBe(1);
+    expect(panel.length).toBe(1);
+  });
+
+  it('updates state correctly for button clicks', () => {
+    const app = shallow(<App />); 
+    const appInstance = app.instance();
+
+    appInstance.handleClick('1');
+    expect(appInstance.state.total).toBe(null);
+    expect(appInstance.state.next).toBe('1');
+    expect(appInstance.state.operation).toBe(null);
+
+    appInstance.handleClick('+');
+    expect(appInstance.state.total).toBe('1');
+    expect(appInstance.state.next).toBe(null);
+    expect(appInstance.state.operation).toBe('+');
+
+    appInstance.handleClick('2');
+    expect(appInstance.state.total).toBe('1');
+    expect(appInstance.state.next).toBe('2');
+    expect(appInstance.state.operation).toBe('+');
+
+    appInstance.handleClick('=');
+    expect(appInstance.state.total).toBe('3');
+    expect(appInstance.state.next).toBe(null);
+    expect(appInstance.state.operation).toBe(null);
+  })
 });
