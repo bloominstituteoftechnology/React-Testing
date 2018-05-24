7a5c8525f745bca6afb8bf7a8cfa207b649c00e5
tanking <tanking@github.com>
2018-05-24 15:50:24 -0400

    Completed App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,8 +8,45 @@ import App from '../App';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should have total state set to 0', () => {
+    // arrange
+    const expected = '0';
+    
+    // act
+    const wrapper = shallow(<App />)
+    const inst = wrapper.instance();
+
+    // assert
+    expect(inst.state.total).toEqual(expected);
+  })
+
+  it('should have next state set to null', () => {
+    // arrange
+    const expected = null;
+    
+    // act
+    const wrapper = shallow(<App />)
+    const inst = wrapper.instance();
+
+    // assert
+    expect(inst.state.next).toEqual(expected);
+  })
+
+  it('should have operation state set to null', () => {
+    // arrange
+    const expected = null;
+    
+    // act
+    const wrapper = shallow(<App />)
+    const inst = wrapper.instance();
+
+    // assert
+    expect(inst.state.operation).toEqual(expected);
+  })
 });
