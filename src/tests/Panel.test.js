31d7a93417b3871f8374370a27440eb95a472ff3
Paul Fang <paulfang@pauls-mbp.home>
2018-05-23 23:43:16 -0700

    Panel.test.js

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Panel from '../components/Panel/Panel';
+import Display from '../components/Display/Display';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Panel />', () => {
+describe('<Display />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+    ReactDOM.render(<Display />, div);
   });
+  it('renders the props.value passed through it', () => {
+    const wrapper = shallow(<Display value='fumo'/>);
+    expect(wrapper.find('div>div').text()).toEqual('fumo')
+  })
 });
