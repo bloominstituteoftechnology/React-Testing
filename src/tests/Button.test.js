f9a9a0c8859b46bdb60ec289c7024ec325cf1a6a
odax <nr10@albion.edu>
2018-05-24 12:24:45 -0400

    completed button.test

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -7,9 +7,39 @@ import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
+it('will render without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
-});
+  it('should  return button ', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.find('.component-button').exists()).toBeTruthy();
+    expect(wrapper.find('.wide').exists()).toBeFalsy();
+    expect(wrapper.find('.orange').exists()).toBeFalsy();
+  });
+
+  it('successfully returns props.orange, when passed it ', () => {
+    const wrapper = shallow(<Button orange />);
+    expect(wrapper.find('.orange').exists()).toBeTruthy();
+    expect(wrapper.find('.wide').exists()).toBeFalsy();
+    expect(wrapper.find('.component-button').exists()).toBeTruthy();
+  });
+
+  it('successfully returns props.wide, when passed it ', () => {
+    const wrapper = shallow(<Button wide />);
+    expect(wrapper.find('.wide').exists()).toBeTruthy();
+    expect(wrapper.find('.orange').exists()).toBeFalsy();
+    expect(wrapper.find('.component-button').exists()).toBeTruthy();
+  });
+
+  it('should fire button prop clickhandler when clicked', () => {
+    const clickHandler = jest.fn();
+    const button = shallow(<Button clickHandler={clickHandler} />);
+    button.children().simulate('click');
+    expect(clickHandler.mock.calls.length).toBe(1);
+  });
+
+  it('display prop name passed to it', () => {
+    const button = shallow(<Button name="hello" />);
+    expect(button.text()).toBe("hello");
+ });
