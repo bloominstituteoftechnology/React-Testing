a543e9e54c9a65cf5d4d8d05daab5ceb6ef6134b
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-24 02:45:27 -0400

    Robert Hogan, Project completed to best of ability currnetly. All snapshot tests passing for all components.

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -12,4 +12,9 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it('should not differ from snapshot', () => {
+    let element = shallow(<Panel />);
+    expect(element).toMatchSnapshot();
+  })
 });
