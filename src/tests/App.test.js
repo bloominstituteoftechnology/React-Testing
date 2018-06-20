8649e78573467d1fdc28424d6b45bce79769b7a9
sagardesai90 <saagardesai@gmail.com>
2018-06-20 15:52:12 -0500

    yarn add dependencies

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
+import sinon from 'sinon';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
+describe(<App />, () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    shallow(<App />);
   });
 });
