f10e3e00f5620738c2b762972a7281f1e8d07668
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-20 14:13:26 -0700

    app.test and button.test partially built

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
+// import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
@@ -9,7 +9,28 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
+    shallow(<App />);
+  })
+  it('should render the display and panel components', () => {
+    const app = shallow(<App />)
+    const display = app.find('Display')
+    const panel = app.find('Panel')
+
+    expect(display.length).toEqual(1)
+    expect(panel.length).toEqual(1)
+  })
+  it('should have the correct default state', () => {
+    const app = shallow(<App />)
+    const instance = app.instance()
+
+    expect(instance.state.total).toBe('0')
+    expect(instance.state.next).toBe(null)
+    expect(instance.state.operation).toBe(null)
+  })
+  it('should handleClick properly', () => {
+    const app = shallow(<App />)
+    const button = app.find('buttonName')
+    button.simulate('click')
+    expect(jest.fn()).toHaveBeenCalledWith()
+  })
 });
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
+// import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
@@ -12,4 +12,19 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('only renders one button?', () => {
+    const button = shallow(<Button />);
+    const componentButton = button.find('.component-button')
+    expect(componentButton.length).toEqual(1);
+  });
+  it('should execute handleClick', () => {
+    const button = shallow(<Button />);
+    button.simulate('click');
+  })
+  it('handles props correctly', () => {
+    const button = shallow(<Button />);
+    
+
+
+  })
 });
