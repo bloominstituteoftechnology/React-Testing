50568c677f8d3ed3dc31306204417b805e658406
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-20 15:55:25 -0500

    added tests for app.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
@@ -12,4 +11,48 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should render 1 panel element', () => {
+    const app = shallow(<App />);
+
+    const panels = app.find("Panel");
+
+    expect(panels.length).toEqual(1);
+  });
+
+  it('should render 1 display element', () => {
+    const app = shallow(<App />);
+
+    const display = app.find("Display");
+
+    expect(display.length).toEqual(1);
+  });
+
+  it('should have correct default state', () => {
+    const app = shallow(<App />);
+
+    const instance = app.instance();
+
+    const defaultState = {
+      total: '0',
+      next: null,
+      operation: null
+    };
+    expect(instance.state).toEqual(defaultState);
+  })
+
+  it('should respond to button press', () => {
+    const app = shallow(<App />);
+
+    const instance = app.instance();
+
+    instance.handleClick("5");
+
+    expect(instance.state).toEqual({
+      total: null,
+      next: "5",
+      operation: null
+    });
+  })
 });
+
