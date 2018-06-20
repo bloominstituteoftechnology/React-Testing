c1d85f2a658566897af001d9f91a44eda9fbf1ab
Danny Vail <danny_vail@outlook.com>
2018-06-20 17:15:59 -0400

    Wrote tests for Display component

--- src/components/Display/Display.test.js
+++ src/components/Display/Display.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Display from './Display';
 
-describe('<Display />', () => {
+describe('Display component', () => {
+  const expected = '0';
+  const props = { value: expected };
+
   it('renders without crashing', () => {
-    shallow(<Display value={'0'} />);
+    shallow(<Display {...props} />);
+  });
+
+  it('renders a div', () => {
+    const display = shallow(<Display {...props} />);
+    const div = display.find('.component-display');
+    expect(div.length).toBe(1);
+  });
+
+  it('renders provided value prop', () => {
+    const display = shallow(<Display {...props} />);
+    const div = display.find('.component-display');
+    expect(div.text()).toEqual(expected);
   });
 });
