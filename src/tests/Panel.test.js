87260e099de43c4ab1a9bbfd61ec213d2c1d7796
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-20 17:31:51 -0500

    added panel test - proj complete

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -11,4 +11,23 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it("should have parent div with classname component-panel", () => {
+    const panel = shallow(<Panel />);
+
+    expect(panel.find(".component-panel")).toHaveLength(1);
+  });
+
+  it("parent div should have 5 child divs", () => {
+    const panel = shallow(<Panel />);
+
+    expect(panel.find(".component-panel").children("div")).toHaveLength(5);
+  });
+
+  it("should have 19 buttons in total", () => {
+    const panel = shallow(<Panel />);
+    
+    expect(panel.find("Button")).toHaveLength(19);
+  });
+
 });
