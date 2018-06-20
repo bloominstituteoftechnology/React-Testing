30360819176c167740efd23053b0226e2d194eb8
udykas <udykas@gmail.com>
2018-06-20 17:10:59 -0400

    Finally thought of some tests to run. Forgot to commit along the way. App, Display and Button all have a few tests each

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
@@ -14,5 +13,10 @@ describe('<App />', () => {
   });
   it('renders without crashing', () => {
     shallow(<App />);
+  })
+  it('should have Display and Panel component', () => {
+    let app = shallow(<App />);
+    expect(app.find("Display").length).toBe(1);
+    expect(app.find("Panel").length).toBe(1);
   })
 });
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
@@ -12,4 +11,21 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('should render a button', () => {
+    const button = shallow(<Button />);
+    const componentButton = button.find('.component-button')
+    expect(componentButton.length).toEqual(1);
+  })
+  it('passes the class wide when in props', () => {
+    const button = shallow(<Button wide />);
+    expect(button.find('.wide').length).toEqual(1);
+  })
+  it('passes the class orange when in props', () => {
+    const button = shallow(<Button orange />);
+    expect(button.find('.orange').length).toEqual(1);
+  })
+  it('should match snapshot', () => {
+    const button =  shallow(<Button />);
+    expect(button).toMatchSnapshot();
+  })
 });
