d5ee6a0c476c5c6a42d939d3d0962518934de37a
AmyShackles <amyshackles@gmail.com>
2018-06-20 14:06:45 -0700

    Made tests for App.js
    - There is a test for checking if App.js renders without crashing
    - There is a test for checking if App.js renders the Display component
    - There is a test for checking if App.js renders the Panel component
    - There is a test for checking if App.js is initialized with the correct state
    - There is a test for checking if the state changes appropriately in response to button clicks
    - - Tested addition
    - - Tested subtraction
    - - Tested multiplicaton
    - - Tested division

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
+import Button from '../components/Button/Button'
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -24,5 +25,96 @@ describe('<App />', () => {
     const panel = app.find('Panel')
 
     expect(panel.length).toEqual(1)
+  })
+  it('should initalize with state = { total: "0", next: null, operation: null }', () => {
+    const app = shallow(<App />)
+
+    expect(app.state().total).toEqual('0')
+    expect(app.state().next).toEqual(null)
+    expect(app.state().operation).toEqual(null)
+  })
+  it('should change state if the button is clicked', () => {
+    const app = shallow(<App />)
+    const instance = app.instance()
+    
+    //addition
+    instance.handleClick('6')
+    expect(instance.state.total).toEqual(null)
+    expect(instance.state.next).toEqual('6')
+    expect(instance.state.operation).toEqual(null)
+    instance.handleClick('+')
+    expect(instance.state.total).toEqual('6')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual('+')
+    instance.handleClick('8')
+    expect(instance.state.total).toEqual('6')
+    expect(instance.state.next).toEqual('8')
+    expect(instance.state.operation).toEqual('+')
+    instance.handleClick('=')
+    expect(instance.state.total).toEqual('14')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual(null)
+
+    // subtraction
+    instance.handleClick('2')
+    expect(instance.state.total).toEqual(null)
+    expect(instance.state.next).toEqual('2')
+    expect(instance.state.operation).toEqual(null)
+    instance.handleClick('7')
+    expect(instance.state.total).toEqual(null)    
+    expect(instance.state.next).toEqual('27')
+    expect(instance.state.operation).toEqual(null)
+    instance.handleClick('-')
+    expect(instance.state.total).toEqual('27')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual('-')
+    instance.handleClick('9')
+    expect(instance.state.total).toEqual('27')
+    expect(instance.state.next).toEqual('9')
+    expect(instance.state.operation).toEqual('-')
+    instance.handleClick('=')
+    expect(instance.state.total).toEqual('18')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual(null)
+    
+    // multiplication
+    instance.handleClick('8')
+    expect(instance.state.total).toEqual(null)
+    expect(instance.state.next).toEqual('8')
+    expect(instance.state.operation).toEqual(null)
+    instance.handleClick('x')
+    expect(instance.state.total).toEqual('8')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual('x')
+    instance.handleClick('7')
+    expect(instance.state.total).toEqual('8')
+    expect(instance.state.next).toEqual('7')
+    expect(instance.state.operation).toEqual('x')
+    instance.handleClick('=')
+    expect(instance.state.total).toEqual('56')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual(null)
+
+    // division
+    instance.handleClick('2')
+    expect(instance.state.total).toEqual(null)
+    expect(instance.state.next).toEqual('2')
+    expect(instance.state.operation).toEqual(null)
+    instance.handleClick('7')
+    expect(instance.state.total).toEqual(null)    
+    expect(instance.state.next).toEqual('27')
+    expect(instance.state.operation).toEqual(null)
+    instance.handleClick('÷')
+    expect(instance.state.total).toEqual('27')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual('÷')
+    instance.handleClick('2')
+    expect(instance.state.total).toEqual('27')
+    expect(instance.state.next).toEqual('2')
+    expect(instance.state.operation).toEqual('÷')
+    instance.handleClick('=')
+    expect(instance.state.total).toEqual('13.5')    
+    expect(instance.state.next).toEqual(null)
+    expect(instance.state.operation).toEqual(null)
   })
 });
