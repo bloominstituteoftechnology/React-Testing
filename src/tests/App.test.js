d26f5f9117a1661fd5f840c5cac77db31631e22b
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-20 15:18:43 -0400

    -installed missing dependencies and first test passes -Second test fails

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -7,9 +7,22 @@ import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+// describe('<App />', () => {
+//   it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<App />, div);
+//       shallow(<Panel />);
+//   });
+
+describe('App component', () => {
+    it('renders without crashing', () => {
+      shallow(<App />);
+  });
+    it('should render a panel of a calculator', () => {
+    const display = shallow(<App />);
+
+    const displays = display.find('panel')
+
+    expect(displays.length).toEqual(1)
   });
 });
