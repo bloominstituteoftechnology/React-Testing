b57958442a83eeb664d3644edf07d4a869f6e194
Steve <shalomlandsman@gmail.com>
2018-06-20 15:42:27 -0400

    setup and app.js testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
@@ -11,5 +10,20 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+  it('renders div with class component-app', () => {
+    const app = shallow(<App />);
+    const compApp = app.find(".component-app");
+    expect(compApp.length).toEqual(1);
+  });
+  it('renders a display component', () => {
+    const app = shallow(<App />);
+    const display = app.find("Display");
+    expect(display.length).toEqual(1);
+  });
+  it('renders a panel component', () => {
+    const app = shallow(<App />);
+    const panel = app.find("Panel");
+    expect(panel.length).toEqual(1);
   });
 });
