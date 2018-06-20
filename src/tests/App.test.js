889e36e759588fe8411db0d63ee25cd97f022ac1
Marco A Guzman <marcoguzman16@gmail.com>
2018-06-20 12:57:11 -0700

    Marco Guzman: PR

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
