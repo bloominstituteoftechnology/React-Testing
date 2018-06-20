70d7fa42049e7b359075278233e5984b31469d9b
Danny Vail <danny_vail@outlook.com>
2018-06-20 15:58:40 -0400

    Initialize and setup project

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('<App />', () => {
+describe('App component', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    shallow(<App />);
   });
 });
