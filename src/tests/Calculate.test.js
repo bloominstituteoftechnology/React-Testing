c7032fea588f6fd53b1adeec30f6aef074e9fbd2
Lavell Burton <taylormade800@gmail.com>
2018-06-20 15:19:27 -0700

    working on stretch with calculate and operate

---
+++ src/tests/Calculate.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import calculate from '../logic/calculate';
+
+
+describe('calculate', () => {
+    it('renders the calculation', () => {
+        expect().toBe();
+    })
+})
---
+++ src/tests/Operate.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import operate from '../logic/operate';
+
+
+describe('operate', () => {
+    it('renders some operation', () => {
+       expect().toBe()
+    })
+})
