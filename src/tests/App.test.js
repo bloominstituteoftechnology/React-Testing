a1158c05799ab5c5aaeb534c0c8596ae6d572395
Zach Campbell <beazlebubbles@gmail.com>
2018-06-20 14:38:41 -0600

    Completed app.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("should render exactly one display element", () => {
+    const app = shallow(<App />);
+
+    const displays = app.find("Display");
+
+    expect(displays.length).toEqual(1);
+  });
+
+  it("should render exactly one panel element", () => {
+    const app = shallow(<App />);
+
+    const panels = app.find("Panel");
+
+    expect(panels.length).toEqual(1);
+  });
+
+  it("should have the correct default state", () => {
+    const app = shallow(<App />);
+    const expectedState = {
+      total: "0",
+      next: null,
+      operation: null
+    };
+    expect(app.instance().state).toEqual(expectedState);
+  });
+
+  it("should respond correctly to button presses", () => {
+    const app = shallow(<App />);
+    const instance = app.instance();
+    instance.handleClick("6");
+    expect(app.instance().state).toEqual({
+      total: null,
+      next: "6",
+      operation: null
+    });
+    instance.handleClick("x");
+    expect(app.instance().state).toEqual({
+      total: "6",
+      next: null,
+      operation: "x"
+    });
+    instance.handleClick("7");
+    expect(app.instance().state).toEqual({
+      total: "6",
+      next: "7",
+      operation: "x"
+    });
+    instance.handleClick("=");
+    expect(app.instance().state).toEqual({
+      total: "42",
+      next: null,
+      operation: null
+    });
   });
 });
--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Panel from '../components/Panel/Panel';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Panel from "../components/Panel/Panel";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Panel />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Panel />, div);
   });
 });
