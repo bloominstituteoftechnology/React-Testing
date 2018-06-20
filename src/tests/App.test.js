ef20e1d43259db5905522f1af007290bb55c6bff
jesuarva <jesuarva.developer@gmail.com>
2018-06-20 22:57:01 +0200

    CHECK POINT: checking error with some 'jest's Globals'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
+// import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
+import Display from '../components/Display/Display';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -12,4 +14,18 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it.each([[Display], [Panel]])('Should have given components', comp => {
+    const app = shallow(<App />);
+    expect(app.find(comp)).toHaveLength(1);
+  });
+  test('Have Display', () => {
+    const app = shallow(<App />);
+    expect(app.find(Display)).toHaveLength(1);
+  });
+  test('Have Panel', () => {
+    const app = shallow(<App />);
+    expect(app.find(Panel).length).toEqual(1);
+  });
+  test();
 });
