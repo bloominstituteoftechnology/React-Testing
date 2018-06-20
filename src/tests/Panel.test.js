711fde3e3e09ce3dc18ba325c90d314ba79ee9e5
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-21 06:41:58 +0800

    Wrote test for Panel component.

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
+import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+  it('Renders without crashing', () => {
+    shallow(<Panel />);
+  });
+
+  it('Renders several buttons.', () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find(Button).length).toEqual(19);
   });
 });
