9a95f85f409f596b84435c752fa9bd967c4b6dc8
Jacob Henry <jacobiswinning@gmail.com>
2018-06-20 15:06:51 -0600

    added test for app.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, {
+  shallow
+} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
+import Display from '../components/Display/Display';
+import Panel from '../components/Panel/Panel';
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render( < App / > , div);
+  });
+  it('should render <Display /> and <Panel /> components', () => {
+    const wrapper = shallow( < App / > );
+    expect(wrapper.find(Display).length).toEqual(0);
+    expect(wrapper.find(Panel).length).toEqual(1);
   });
 });
