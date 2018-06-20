4e7eed21b7bd6705dba24840bcf3af92f4d59076
Chris Honda <christopher.honda92@gmail.com>
2018-06-20 14:30:26 -0700

    Finished button tests

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -12,8 +12,27 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
   it('should have className of orange if it is passed that prop', () => {
     const orange = shallow(<Button orange />);
     expect(orange.find('.orange')).toHaveLength(1);
+  });
+
+  it('should be the same of the snapshot', () => {
+    const button = shallow(<Button />);
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should have a class of wide when it is given that prop', () => {
+    const button = shallow(<Button />);
+    expect(button.is('.wide')).toBe(false);
+
+    const wideButton = shallow(<Button wide />);
+    expect(wideButton.is('.wide')).toBe(true);
+  });
+
+  it('should display the props that were passed to it', () => {
+    const button = shallow(<Button name='testButton' />);
+    expect(button.text()).toBe('testButton');
   });
 });
