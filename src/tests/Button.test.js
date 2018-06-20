7d3db58ef69a5b26cbb79a5f10aef989ea1753da
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-20 13:34:22 -0700

    button.test done

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -12,4 +12,28 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+  it('should render the button element', () => {
+    const button = shallow(<Button />);
+    expect(button.find('button')).toHaveLength(1);
+  });
+
+  it('should check if clickHandler is functioning properly', () => {
+    window.alert = jest.fn();
+    const button = shallow(<Button clickHandler={window.alert} />);
+    button.find("button").simulate("click");
+    expect(window.alert).toHaveBeenCalledTimes(1);
+  });
+
+  it("should have orange button", () => {
+    const props = "orange";
+    const button = shallow(<Button orange />);
+    expect(button.find(".orange")).toHaveLength(1);
+  });
+
+  it("should have wide button", () => {
+    const props = "wide";
+    const button = shallow(<Button wide />);
+    expect(button.find(".wide")).toHaveLength(1);
+  });
 });
