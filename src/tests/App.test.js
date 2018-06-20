6ac4a31aa27cce839749b754c09452b611e8c22d
sagardesai90 <saagardesai@gmail.com>
2018-06-20 17:47:33 -0500

    added tests to app test file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,9 +4,8 @@ import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
-import sinon from 'sinon';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import Button from '../components/Button/Button';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -14,4 +13,9 @@ describe(<App />, () => {
   it('renders without crashing', () => {
     shallow(<App />);
   });
+  it('should return a display component', () => {
+    const app = shallow(<App />);
+    const display = app.find('Display');
+    expect(display.length).toEqual(1);
+  })
 });
