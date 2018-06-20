a0bc1ac53a5a65e484005492afcd083afe20c751
David <AquilaVirtual>
2018-06-20 14:51:53 -0700

    MVP: test files for APP.js, Button.js, Display.js, and Panel.js done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
+
+describe('App component', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    shallow(<App />); // notice this only renders App and will not render any child components
   });
+
+  it('should render exactly Panel component', () => {
+    const component = shallow(<App />);
+    expect(component.find('Panel')).toHaveLength(1)
+  });
+  it('should render Display component', () => {
+    const component = shallow(<App />);
+    expect(component.find('Display')).toHaveLength(1)
+  })
 });
--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
@@ -12,4 +11,12 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+  it('should render a div with class component panel', () => {
+    const component = shallow(<Panel />)
+    expect(component.find('.component-panel')).toHaveLength(1)
+  })
+  it('should render 19 buttons', () => {
+    const component = shallow(<Panel />)
+    expect(component.find('Button')).toHaveLength(19)
+  })
 });
