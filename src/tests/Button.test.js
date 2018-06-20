be127748a3bf5008b4b4480b384cf2fe85f8938f
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-20 18:53:41 -0400

    Some Button component test

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -6,9 +6,48 @@ import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+// describe('<Button />', () => {
+//   it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<Button />, div);
+//   });
+// });
+
 describe('<Button />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+    shallow(<Button />)
   });
+});
+
+describe('<Button />', () => {
+  it('renders with className `component-button` when not passed orange or wide props', () => {
+    const button = shallow(<Button />);
+
+    const className = button.find('.component-button');
+
+    expect(className.length).toEqual(1);
+  });
+
+  it('renders with className `component-button orange` when passed the orange prop', () => {
+    const button = shallow(<Button orange/>);
+    const className = button.find('.component-button .orange');
+
+    expect(className.length).toEqual(1);
+
+  });
+
+  it('renders with className `component-button wide` when passed the wide prop', () => {
+    const button = shallow(<Button wide/>);
+    const className = button.find('.component-button .wide');
+
+    expect(className.length).toEqual(1);
+  });
+
+  it('renders with className `component-button orange wide` when passed the orange and wide props', () => {
+    const button = shallow(<Button wide orange/>);
+    const className = button.find('.component-button .orange .wide');
+
+    expect(className.length).toEqual(1);
+  });
+
 });
