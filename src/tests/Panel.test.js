d9fd7d8b3cf9f4988353f5403f41812ba76fa0de
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-20 15:22:20 -0700

    almost complete with panel.js tests. have one test that isnt passing

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -11,4 +11,26 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it("should have a div with class name component-panel", () => {
+    const panel = shallow(<Panel />);
+    expect(panel.find(".component-panel")).toHaveLength(1);
+  });
+
+  it("parent div should have 5 children divs", () => {
+    const panel = shallow(<Panel />);
+    expect(panel.find(".component-panel").children("div")).toHaveLength(5);
+  });
+
+  it("panel should have 19 different buttons", () => {
+    const panel = shallow(<Panel />);
+    expect(panel.find("Button")).toHaveLength(19);
+  });
+
+  it("should pass click handler and function properly", () => {
+    window.alert = jest.fn();
+    const panel = shallow(<Panel clickHandler={window.alert} />);
+    panel.find(`[name="AC"] button`).simulate("click");
+    expect(window.alert).toHaveBeenCalledTimes.toBe(1);
+  });
 });
