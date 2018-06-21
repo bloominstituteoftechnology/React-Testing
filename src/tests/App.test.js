82d662996a642364095607b48cf9034fe2c525dd
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-21 14:10:32 -0700

    write tests for App and Button

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from '../App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from '../App'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
+    shallow(<App />)
+  })
+
+  it('should by default have total = "0"', () => {
+    const _App = shallow(<App/>)
+    const instance = _App.instance()
+    expect(instance.state.total).toEqual('0')
+  })
+
+  it('should by default have next and operation set to null', () => {
+    const _App = shallow(<App/>)
+    const instance = _App.instance()
+    expect(instance.state.next).toBe(null)
+    expect(instance.state.operation).toBe(null)    
+  })
+
+  it('should update the state when handleClick is called', () => {
+    const _App = shallow(<App/>)
+    const instance = _App.instance()
+    const numberButton = '5'
+    const operationButton = '+'
+
+    expect(instance.state.next).toBe(null)
+    instance.handleClick(numberButton)
+    expect(instance.state.next).toEqual('5')
+    instance.handleClick(operationButton)
+    expect(instance.state.operation).toEqual('+')
+  })
 });
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -8,7 +8,24 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+    shallow(<Button />)
   });
+
+  it('button name matches name prop', () => {
+    const props = { name: '5' }
+    const _Button = shallow(<Button {...props} />)
+    const button = _Button.find('button')
+
+    expect(button.text()).toEqual('5')
+  })
+
+  it('triggers click handler when clicked', () => {
+    const exampleHandler = jest.fn()
+    const props = { clickHandler: exampleHandler }
+    const _Button = shallow(<Button {...props} />)
+    const button = _Button.find('button')
+    
+    button.simulate('click')
+    expect(exampleHandler).toHaveBeenCalledTimes(1)
+  })
 });
