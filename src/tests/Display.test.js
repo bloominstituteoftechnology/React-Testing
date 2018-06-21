f85597ef1ec89bcce8c19f6ab8fa93bb39ce3020
A99 <A99@99s-macbook-pro.home>
2018-06-21 03:12:36 +0200

    Add Display tests

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Display from '../components/Display/Display';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
+describe.only('<Display />', () => {
+
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
+    shallow(<Display/>);
   });
+
+  // render
+  it('renders a div with component-display class', () => {
+    const display = shallow(<Display/>);
+    const div = display.find('.component-display');
+    expect(div.length).toBe(1);
+  });
+  it('renders one child div of component-display', () => {
+    const display = shallow(<Display/>);
+    const div = display.find('.component-display > div');
+    expect(div.length).toBe(1);
+  });
+
+  // props
+  it('renders the value 0.07 when passed to its props', () => {
+    const props = { value: '0.07'};
+    const display = shallow(<Display {...props} />);
+    const div = display.find('.component-display > div');
+    expect(div.text()).toEqual('0.07');
+  });
+  it('renders the value -0.07 when passed to its props', () => {
+    const props = { value: '-0.07'};
+    const display = shallow(<Display {...props} />);
+    const div = display.find('.component-display > div');
+    expect(div.text()).toEqual('-0.07');
+  });
+
 });
