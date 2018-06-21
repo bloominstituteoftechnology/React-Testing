c1a3f23ee4506b6c04c7a1504e621f820ad11af4
Lavell Burton <taylormade800@gmail.com>
2018-06-20 17:56:36 -0700

    updating test files calculate test is failing

--- src/tests/Calculate.test.js
+++ src/tests/Calculate.test.js
@@ -3,6 +3,7 @@ import calculate from '../logic/calculate';
 
 describe('calculate', () => {
     it('renders the calculation', () => {
-        expect(calculate({ "next": "2", "operation": "+", "total": "8" }, "=")).toBe({ "next": null, "operation": null, "total": "6" });
+        expect(calculate({ "next": "2", "operation": "+", "total": "8" }, "="))
+            .toBe({ "next": "null", "operation": "null", "total": "10" });
     })
 })
--- src/tests/Operate.test.js
+++ src/tests/Operate.test.js
@@ -7,6 +7,7 @@ describe('operate', () => {
     it('renders the calculation', () => {
         expect(operate(1,1,'+')).toBe('2');
         expect(operate(1,2,'x')).toBe('2');
-
+        expect(operate(1,2,'%')).toBe('1');
+        expect(operate(10,5,'-')).toBe('5');
     })
 })
