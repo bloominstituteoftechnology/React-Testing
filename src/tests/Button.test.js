7e06d0446d06e4a0a057c6927e8b9516b5c214cd
AmyShackles <amyshackles@gmail.com>
2018-06-20 17:37:42 -0700

    Wrote tests for Button
    - renders without crashing
    - renders a button
    - clicking the button calls a callback
    - renders button with correct class name
    - invokes its callback with its name when callback is invoked

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -4,6 +4,7 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
 
+
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
@@ -11,4 +12,39 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('should render a button', () => {
+    const component = shallow(<Button />)
+    const button = component.find('button')
+
+    expect(button.length).toEqual(1)
+  })
+  it('clicking the button should call a callback', () => {
+    const mySpyGuy = jest.fn(() => {})    
+    const button = shallow(<Button clickHandler={mySpyGuy}/>)
+    const instance = button.instance()
+
+    instance.handleClick()
+    expect(mySpyGuy).toHaveBeenCalledTimes(1)
+    instance.handleClick()
+    instance.handleClick()
+    instance.handleClick()
+    instance.handleClick()
+    expect(mySpyGuy).toHaveBeenCalledTimes(5)
+  })
+  it('should render button with correct class name', () => {
+    const orangeButton = shallow(<Button orange />)
+    const wideButton = shallow(<Button wide />)
+    
+    expect(orangeButton.find('.component-button').hasClass('orange')).toEqual(true)
+    expect(wideButton.find('.component-button').hasClass('wide')).toBe(true)
+  })
+  it('should invoke its callback with its name when handleClick is invoked', () => {
+    const mySpyGuy = jest.fn(() => {})
+    
+    const button = shallow(<Button clickHandler={mySpyGuy} name="I am a spy, guy" />)
+    const instance = button.instance()
+
+    instance.handleClick()
+    expect(mySpyGuy.mock.calls[0][0]).toBe("I am a spy, guy")
+  })
 });
