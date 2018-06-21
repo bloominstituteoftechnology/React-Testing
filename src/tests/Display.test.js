6e007f187a589dbd9c5021f179d2c6201dbff94d
AmyShackles <amyshackles@gmail.com>
2018-06-20 17:38:47 -0700

    Tests for Panel.js
    - it renders without crashing
    - it renders two divs
    - it should render the provided value

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -11,4 +11,21 @@ describe('<Display />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+  it('should render two divs', () => {
+    const display = shallow(<Display />)
+    const instance = display.instance()
+
+    const div = display.find('div')
+
+    expect(div.length).toEqual(2)
+  })
+  it('should render provided value', () => {
+    const expected = "I are a value";
+    const props = { value: expected }
+    const display = shallow(<Display {...props} />)
+
+    const div = display.find('.component-display')
+
+    expect(div.text()).toEqual(expected)
+  })
 });
