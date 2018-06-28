68a129832fb7699ee641c65b52c41f39ad077bb7
JSNavey <jsnavey@gmail.com>
2018-06-27 20:33:14 -0400

    finished Display test

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -7,8 +7,27 @@ import Display from '../components/Display/Display';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
+  // it('renders without crashing', () => {
+  //   const div = document.createElement('div');
+  //   ReactDOM.render(<Display />, div);
+  // });
+
+  it('should renders without crashing', () => {
+    shallow(<Display />)
+  })
+
+  it('should render one display class', () => {
+    const display = shallow(<Display />)
+    const foundDisplay = display.find('.component-display');
+    expect(foundDisplay.length).toEqual(1);
   });
+
+  it('should render provided value', () => {
+    const expected = 'A/C';
+    const props = { value: expected };
+    const display = shallow(<Display {...props} />);
+    const foundDisplay = display.find('.component-display');
+    expect(foundDisplay.text()).toEqual(expected)
+  })
 });
+
