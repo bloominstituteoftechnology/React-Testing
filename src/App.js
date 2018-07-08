c8a16dd01e940ef3707be4e0450b1d56677ca0d5
Kevin Mei <derrick.mei.168@gmail.com>
2018-07-08 03:53:30 -0500

    apps and button tests

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import Display from './components/Display/Display';
-import Panel from './components/Panel/Panel';
-import calculate from './logic/calculate';
-import './App.css';
+import React, { Component } from "react";
+import Display from "./components/Display/Display";
+import Panel from "./components/Panel/Panel";
+import calculate from "./logic/calculate";
+import "./App.css";
 
 class App extends Component {
-    state = {
-        total: '0',
-        next: null,
-        operation: null
-    }
+  state = {
+    total: "0",
+    next: null,
+    operation: null
+  };
 
-    handleClick = (buttonName) => {
-        this.setState(calculate(this.state, buttonName));
-    }
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
 
-    render() {
-        return (
-            <div className='component-app'>
-                <Display value={this.state.next || this.state.total || '0'} />
-                <Panel clickHandler={this.handleClick} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <Panel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Button from '../components/Button/Button';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Button from "../components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Button />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Button />, div);
+  });
+
+  it("should have a button", () => {
+    const button = shallow(<Button />);
+
+    expect(button.find("button").length).toEqual(1);
+  });
+
+  it("should have classname orange when passed an orange prop", () => {
+    const button = shallow(<Button orange />);
+
+    expect(button.find(".orange")).toBeTruthy();
+  });
+  it("should have classname wide when passed an wide prop", () => {
+    const button = shallow(<Button wide />);
+
+    expect(button.find(".wide")).toBeTruthy();
+  });
+
+  it("should call clickHandler when clicked", () => {
+    const spy = jest.fn();
+    const button = shallow(<Button clickHandler={spy} />);
+
+    button.children().simulate("click");
+    expect(spy.mock.calls.length).toBe(1);
   });
 });
