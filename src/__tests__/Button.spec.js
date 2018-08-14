2a4c35ae9ea8f9f7ae1986e5f7936d28d2891c4f
Sam Khaled <samkhaled@outlook.com>
2018-08-14 17:47:27 -0400

    added tests to the button

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -12,4 +12,29 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+
+
+    it('has className of component-button when no props provided', () => {
+    const button = shallow(<Button />)
+    expect(button.hasClass('component-button')).toBe(true)
+  })
+   it('has className of orange when orange prop provided', () => {
+    const button = shallow(<Button orange />)
+    expect(button.hasClass('orange')).toBe(true)
+  })
+   it('has className of wide when wide prop provided', () => {
+    const button = shallow(<Button wide />)
+    expect(button.hasClass('wide')).toBe(true)
+  })
+   it('has classNames of component-button, orange, and wide, when both orange and wide props provided', () => {
+    const button = shallow(<Button orange wide />)
+    expect(button.hasClass('component-button')).toBe(true)
+    expect(button.hasClass('orange')).toBe(true)
+    expect(button.hasClass('wide')).toBe(true)
+  })
+   it('has inner text set to name prop', () => {
+    const button = shallow(<Button name='x' />)
+    expect(button.text()).toBe('x')
+  })
 });
