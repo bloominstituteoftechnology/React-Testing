cb927d2d2d6635ed9bfb51817011427429df1b5f
DirupT <iamcooled@gmail.com>
2018-08-14 14:49:38 -0500

    added tests for panel

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -10,3 +10,4 @@ describe('<Button />', () => {
     ReactDOM.render(<Button />, div);
   });
 });
+
--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -10,3 +10,18 @@ describe('<Panel />', () => {
     ReactDOM.render(<Panel />, div);
   });
 });
+
+it('should render 19 Button components to the screen', () => {
+  const wrapper = shallow(<Panel />);
+  const buttons = wrapper.find('Button');
+
+  expect(buttons).toHaveLength(19);
+})
+
+it('should have a method called handleClick', () => {
+  const wrapper = shallow(< Panel />);
+  const instance = wrapper.instance();
+
+  expect(instance.handleClick).toBeDefined();
+})
+
