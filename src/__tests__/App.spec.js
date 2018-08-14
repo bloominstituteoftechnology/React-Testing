ba6674bba58cc8996d04ec27a289549b0c1ff6b8
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-14 14:42:00 -0700

    tested App component to check if the default total value is 0 and rending of p tag with text 'Calculator' in it

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -8,8 +8,27 @@ import App from '../App';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
+
+	
+it('renders without crashing', () => {
+        shallow(<App />);
+});
+
+
+it('should render a p tag with "Calculator text"', () => {
+
+        const app = shallow(<App />);
+
+        const paragraph = app.find('.calculator-title');
+        expect(paragraph.text()).toEqual('Calculator');
+
+});
+
+
+it('should display total to be 0  by default', () => {
+  	const app = shallow(<App />);
+  	const instance = app.instance();
+
+  	expect(instance.state.total).toEqual("0");
+});	
 });
