8213683bc4ffd6b435107979f62b58d145ba28da
CLN <cln@Sags-MacBook-Pro.local>
2018-08-14 18:28:15 -0400

    Pull,.

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -12,4 +12,16 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  // it('render a p tag with "Welcome to React"' , () => {
+  //   const div = document.createElement('div');
+  //   const app = shallow(<App />);
+  //   const paragraph = app.find('App.intro');
+  //   expect(paragraph.text()).toEqual('Welcome to React')
+  // });
+  it('render a p tag with "Welcome to React"' , () => {
+    const div = document.createElement('div');
+    const app = shallow(<App />);
+    const paragraph = app.find('App.intro');
+    expect(paragraph.text()).toEqual('Welcome to React')
+  });
 });
