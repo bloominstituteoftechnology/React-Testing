bc2cfbcc8f1d832af12139eac3486ce41479d592
Tyson Gushiken <tysonism@gmail.com>
2018-08-14 13:51:27 -0700

    Create tests for App and Display

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,7 +4,17 @@ import { shallow } from 'enzyme';
 import App from '../App';
 
 describe('<App />', () => {
+  const app = shallow(<App />);
+
   it('renders without crashing', () => {
     shallow(<App />);
+  });
+  it('should render exactly one Display component', () => {
+    const display = app.find('Display');
+    expect(display.length).toEqual(1);
+  });
+  it('should render exactly one Panel component', () => {
+    const panel = app.find('Panel');
+    expect(panel.length).toEqual(1);
   });
 });
--- src/__tests__/Display.test.js
+++ src/__tests__/Display.test.js
@@ -4,7 +4,17 @@ import { shallow } from 'enzyme';
 import Display from '../components/Display/Display';
 
 describe('<Display />', () => {
+
   it('renders without crashing', () => {
     shallow(<Display />);
+  });
+
+  it('should render provided title', () => {
+    const expected = '0'
+    const props = { value: expected };
+    const display = shallow(<Display {...props} />);
+    const titleDiv = display.find('.component-display');
+    
+    expect(titleDiv.text()).toEqual(expected);
   });
 });
