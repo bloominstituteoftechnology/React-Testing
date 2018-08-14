09bd229e903851f28fc7667eefae311a14bbb9c3
Tyson Gushiken <tysonism@gmail.com>
2018-08-14 13:11:45 -0700

    Create setupTests.js for Enzyme adapter

---
+++ src/__tests__/Display.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Display from '../components/Display/Display';
+
+describe('<Display />', () => {
+  it('renders without crashing', () => {
+    shallow(<Display />);
+  });
+});
---
+++ src/__tests__/Panel.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Panel from '../components/Panel/Panel';
+
+describe('<Panel />', () => {
+  it('renders without crashing', () => {
+    shallow(<Panel />);
+  });
+});
