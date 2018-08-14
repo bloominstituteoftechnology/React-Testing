5244dc3de041d3165df2444d623c46bbc7bd35b7
snowcoding <vimalshah77@gmail.com>
2018-08-14 16:03:49 -0700

    Button Testing - div
    - div is always rendered
    - contains everything in the div

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -8,12 +8,8 @@ import renderer from 'react-test-renderer';
 Enzyme.configure({ adapter: new Adapter() });
 
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
-  
+describe('Button Snapshot', () => {
+    
   it('matches the snapshot', () => {
     const tree = renderer.create(<Button />).toJSON()
     
@@ -22,3 +18,59 @@ describe('<Button />', () => {
   })
 });
 
+/*
+Figure out the contract
+- a div is always rendered
+- the rendered div contains everything else that gets rendered
+- the button is always rendered
+- 
+
+*/
+
+describe("Button", () => {
+  //Create variables available in the entire describe function
+  let props;
+  let mountedButton;
+
+  //Create a button function that either mounts the Button with
+  //the current props or returns the one already mounted
+  const button = () => {
+    if (!mountedButton) {
+      mountedButton = mount(<Button {...props} />);
+    }
+    return mountedButton
+  }
+
+  //Reset the state of the test to a know starting point
+  beforeEach(() => {
+    props = {
+      name : undefined,
+      handleClick : undefined,
+      orange : undefined,
+      wide : undefined,
+    }
+    mountedButton = undefined;
+  })
+
+  it('always renders a div', () => {
+    const divs = button().find('div');
+    expect(divs.length).toBeGreaterThan(0);
+  })
+
+  describe('the rendered div', () => {
+    it('contains everything else that gets rendered', () => {
+      const div = button().find('div');
+      /*
+      Using .find in enzyme arranges the nodes in order of such that 
+      the outermost node is first:
+      */
+      const wrappingDiv = div.first();
+
+      /*
+
+      */
+      console.log(wrappingDiv.children())
+    })
+  })
+
+})
