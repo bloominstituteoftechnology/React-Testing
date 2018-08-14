be547c7461e92d292c4da72ed51baf9cb97fb95a
Jasper <joshtietz11@gmail.com>
2018-08-14 17:42:32 -0500

    Josh Tietz -- react testing

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
+import sinon from 'sinon';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Button from '../components/Button/Button';
@@ -12,4 +13,38 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+
+it('should be orange if passed', () => {
+  const app = shallow(<Button orange={true} />)
+
+  const button = app.find('.component-button')
+
+  expect(button.hasClass('orange')).toEqual(true);
+})
+
+it('should be wide if passed', () => {
+  const app = shallow(<Button wide={true} />)
+
+  const button = app.find('.component-button')
+
+  expect(button.hasClass('wide')).toEqual(true);
+})
+
+it('should display a name passed as a prop', () => {
+  const app = shallow(<Button name="this name" />)
+
+  const button = app.find('.component-button')
+
+  expect(button.text()).toEqual('this name');
+})
+
+it('simulates click events', () => {
+    const spy = sinon.spy();
+    const button = shallow(<Button handleClick={spy} />);
+    button.find('button').first().simulate('click');
+    expect(spy.calledOnce).toBe(true)
+  });
+
+
 });
