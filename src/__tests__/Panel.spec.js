796c7ccdbe360754d4a9d658b7c4a79d401f8f82
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-14 15:27:33 -0400

    Panel.spec

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -12,4 +12,15 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it("should display 19 buttons", () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find("Button")).toHaveLength(19);
+  });
+
+   it('should have className .component-panel with five children', () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find('.component-panel').children()).toHaveLength(5);
+  });
+  
 });
