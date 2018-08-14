0b9a41415f39477918b8bdb48f1bc70334aa9351
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-14 13:52:31 -0700

    added one test to Panel

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -8,8 +8,14 @@ import Panel from '../components/Panel/Panel';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
-  });
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Panel />, div);
+    });
+
+    it('has a div with a className "component-panel"', () => {
+        const wrapper = shallow(<Panel />);
+
+        expect(wrapper.find('.component-panel')).toHaveLength(1);
+    })
 });
