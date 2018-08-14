68c60948b8642516f16bfcdeccc5cff3eb28245e
Kevin Tena <kevten09@gmail.com>
2018-08-14 17:47:51 -0500

    Wrote test for panel

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -12,4 +12,10 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it('Should have component-panel as main div', () => {
+    const panel = shallow(<Panel />)
+
+    expect(panel.find('div').first().hasClass('component-panel')).toEqual(true);
+  })
 });
