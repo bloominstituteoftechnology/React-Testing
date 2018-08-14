d596420f0167c1b78001340d8e6df5657184e76a
Kevin <brackkevin@gmail.com>
2018-08-14 17:52:06 -0500

    init

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Display />, div);
+  });
+
+  it("should print the value prop to the screen", () => {
+    const display = shallow(<Display />);
+    const componentDisplay = display.find(".component-display");
+    expect(componentDisplay.text()).toEqual("0");
   });
 });
