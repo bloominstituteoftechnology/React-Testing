46894817332ba53bbb883029b1901061b8326a62
Nicky Chen <nicky.chen0@gmail.com>
2018-08-14 14:57:30 -0700

    app & button done

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -11,4 +11,22 @@ describe("<App />", () => {
   it("renders without crashing", () => {
     shallow(<App />);
   });
+
+  it("should have the default properties total, next, and operation in its state", () => {
+    const app = shallow(<App />);
+    expect(app.state("total")).toEqual("0");
+    expect(app.state("next")).toEqual(null);
+    expect(app.state("operation")).toEqual(null);
+  });
+
+  it("should render `component-app`", () => {
+    const app = shallow(<App />);
+    expect(app.find(".component-app").length).toEqual(1);
+  });
+
+  it("should have a handleClick method that determines state", () => {
+    const app = shallow(<App />);
+    app.instance().handleClick("1");
+    expect(app.state("next")).toEqual("1");
+  });
 });
