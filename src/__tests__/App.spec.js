b082af55ae67cddfbbdd0e1c167950702f6c1037
DirupT <iamcooled@gmail.com>
2018-08-14 14:01:37 -0500

    removed un needed enzymes

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import App from '../App';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
