ef795d200f9b34024f0398547942b8e5c4ae1aba
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-14 13:37:34 -0700

    one test for display

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -8,8 +8,15 @@ import Display from '../components/Display/Display';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
-  });
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Display />, div);
+    });
+
+    it('should display value passed as a props', () => {
+        const value = { value: '5' }
+        const display = shallow(<Display {...value} />).find('div').at(1)
+
+        expect(display.text()).toEqual('5')
+    })
 });
