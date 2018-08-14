1e56abf888d92a686fca2415a8ab66d252f885ba
Vu Cao <cdhvu@yahoo.com>
2018-08-14 13:17:18 -0700

    Added some tests for App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
+import { shallow } from 'enzyme';
 import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
+  const app=shallow(<App />);
+  const instance = app.instance();
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should render a Display component', () => {
+    const display = app.find('Display');
+    expect(display.length).toBe(1);
+  })
+
+  it('should render a Panel component', () => {
+    const panel = app.find('Panel');
+    expect(panel.length).toBe(1);
+  })
+
+  it('should have state Total with initial value 0', () => {
+    expect(app.state().total).toEqual('0');
+  })
+  // console.log(instance.state.total);
+  console.log(app.state('total'));
 });
