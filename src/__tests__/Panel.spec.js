042fb2f786bc083e7c368c62d1c036c87054bea0
Luis Hernandez <luishrd@gmail.com>
2018-08-14 08:59:40 -0600

    updates instructions

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
