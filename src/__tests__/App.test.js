cfee13abc8ef79a35b7a8b27a7fef857ea3145fc
Tyson Gushiken <tysonism@gmail.com>
2018-08-14 14:23:47 -0700

    Create tests for Panel

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -9,10 +9,12 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     shallow(<App />);
   });
+  
   it('should render exactly one Display component', () => {
     const display = app.find('Display');
     expect(display.length).toEqual(1);
   });
+
   it('should render exactly one Panel component', () => {
     const panel = app.find('Panel');
     expect(panel.length).toEqual(1);
--- src/__tests__/Panel.test.js
+++ src/__tests__/Panel.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import Panel from '../components/Panel/Panel';
 
 describe('<Panel />', () => {
+  const panel = shallow(<Panel />);
+  const mountPanel = mount(<Panel />); 
+
   it('renders without crashing', () => {
     shallow(<Panel />);
   });
+
+  it('should render exactly 19 Button components', () => {
+    const button = panel.find('Button');
+    expect(button.length).toEqual(19);
+  });
+
+  it('should render exactly 5 orange buttons', () => {
+    const orange = mountPanel.findWhere(b => b.hasClass('orange'));
+    expect(orange.length).toEqual(5);
+  });
+
+  it('should render exactly 1 wide button', () => {
+    const wide = mountPanel.findWhere(b => b.hasClass('wide'));
+    expect(wide.length).toEqual(1);
+  });
+
+  it('should have an equals button', () => {
+    expect(mountPanel.find('button').contains('=')).toBeTruthy();
+  })
 });
