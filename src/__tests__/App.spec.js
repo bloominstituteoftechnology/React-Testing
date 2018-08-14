c9287935cc7baf8f8894cb1c36c58d917a03366b
jeffyjkang <jeffyjkang@gmail.com>
2018-08-14 13:15:52 -0700

    jeff kang testing

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import App from '../App';
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<App />, div);
+  });
+  it("should display value of 0, null, null on default", () => {
+    const app = shallow(<App />);
+    const instance = app.instance();
+    expect(instance.state.total).toEqual("0");
+    expect(instance.state.next).toEqual(null);
+    expect(instance.state.operation).toEqual(null);
+  });
+  it.skip("should display a number passed as a prop", () => {
+    const app = shallow(<App />);
+    const instance = app.instance();
+    const next = app.instance(4);
+    expect(instance.state.next).toEqual("4");
   });
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Button from '../components/Button/Button';
+import Button from "../components/Button/Button";
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ buttonAdapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Button />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Button />, div);
+  });
+  it.skip("returns button ....", () => {
+    const Button = shallow(<Button />);
+    const but = app.find(button);
+    but.simulate();
   });
 });
