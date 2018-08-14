8e3306bb6f4636d2afcc8a644587bc19583c75d3
Allen Hai <allenhai03@gmail.com>
2018-08-14 16:34:26 -0500

    write tests for Display

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Display from '../components/Display/Display';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+
+  it('renders the value provided as a prop', () => {
+    const display = shallow(<Display value='256' />)
+    expect(display.text()).toBe('256')
+  })
+
+  it('has a single child div', () => {
+    const display = shallow(<Display />)
+    expect(display.children().length).toBe(1)
+    expect(display.childAt(0).type()).toBe('div')
+  })
 });
