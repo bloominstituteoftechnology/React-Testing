00f61484ba314188401b76a16a78c26665771f8c
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-14 16:18:03 -0400

    William DiFulvio [React-Testing]
    Began creating tests for App, Display and Button

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -12,4 +12,32 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should display value of 0 on default', () => {
+    const app = shallow(<App/>);
+    const instance = app.instance();
+    expect(instance.state.total).toEqual('0')
+  });
+  it('should display null on default', () => {
+    const app = shallow(<App/>);
+    const instance = app.instance();
+    expect(instance.state.next).toEqual(null)
+  });
+  it('should display null on default', () => {
+    const app = shallow(<App/>);
+    const instance = app.instance();
+    expect(instance.state.operation).toEqual(null)
+  });
+  // it('should display a value passed as a prop', () => {
+  //   const app = shallow(<App/>);
+  //   const instance = app.instance();
+
+
+
+  //   expect(instance.state.total).toEqual('2')
+  // })
+
+
+
+
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -12,4 +12,17 @@ describe('<Display />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+
+  it('should display a value passed in as a prop', () => {
+    const display = shallow(<Display value='a value'/>);
+    const possValue = display.find('.component-display');
+    expect(possValue.text()).toEqual('a value')
+  })
+  it('should display zero as a default value', () => {
+    const display = shallow(<Display value ="0"/>);
+    const possValue = display.find('.component-display');
+    expect(possValue.text()).toEqual('0');
+  })
+
+
 });
