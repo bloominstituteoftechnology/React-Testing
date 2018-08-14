0484b5c0f31f0e837a9fecb1d981264af8470509
Tyson Gushiken <tysonism@gmail.com>
2018-08-14 15:07:29 -0700

    Create tests for Button

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import Button from '../components/Button/Button';
 
 describe('<Button />', () => {
+  const props = {
+    name: 'Submit',
+    orange: true,
+    wide: false,
+    clickHandler: jest.fn(),
+  };
+  const button = shallow(<Button {...props} />);
+
   it('renders without crashing', () => {
     shallow(<Button />);
   });
+
+  it(`displays the name 'Submit' when passed through props`, () => {
+    expect(button.find('button').text()).toEqual('Submit');
+  });
+
+  it('contains the class .orange when orange is set to true', () => {
+    expect(button.hasClass('orange')).toBeTruthy();
+  });
+
+  it('does not contain the class .wide when wide is set to false', () => {
+    expect(button.hasClass('wide')).toBeFalsy();
+  });
+
+  it('executes its passed click handler when clicked', () => {
+    button.find('button').simulate('click');
+    expect(props.clickHandler).toHaveBeenCalled();
+  });
+
 });
