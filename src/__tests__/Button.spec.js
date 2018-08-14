a3f37b27c0b64fbc452b1357c1f715c726c952e7
snowcoding <vimalshah77@gmail.com>
2018-08-14 15:31:17 -0700

    Basic snapshot test of Button

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import Button from '../components/Button/Button';
+import renderer from 'react-test-renderer';
 
 Enzyme.configure({ adapter: new Adapter() });
+
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Button />).toJSON()
+    
+    expect(tree).toMatchSnapshot()
+    
+  })
 });
+
