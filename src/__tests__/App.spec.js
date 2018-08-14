4e99ed95264b9f2302a33395ad495cf4719ed4c9
Simon <Simon_Freelance@hotmail.com>
2018-08-14 16:29:36 -0400

    Tests added for default state in App component

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import App from '../App';
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+describe("<App />", () => {
+  const app = shallow(<App />);
+  const instance = app.instance();
+  it("renders without crashing", () => {
+    shallow(<App />);
   });
+
+  describe("checks default state for <App />", () => {
+    it("should have total as 0 by default in state", () => {
+      expect(instance.state.total).toMatch("0");
+    });
+    it("should have next as null by default in state", () => {
+      expect(instance.state.next).toBe(null);
+    });
+    it("should have operation as null by default in state", () => {
+      expect(instance.state.operation).toBe(null);
+    });
+  });
+
 });
