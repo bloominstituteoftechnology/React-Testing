3c48a53ad01d347583691a60991c4d18abbaa13c
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-14 16:10:05 -0600

    Created tests for Display.js

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -8,11 +8,17 @@ import Display from '../components/Display/Display';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
-  it('renders without crashing', () => {
+  test('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
-  it('Should have class named "component-display"', () => {
-    
+  test('Should have class named "component-display"', () => {
+    const display = shallow(<Display />);
+    expect(display.hasClass("component-display")).toEqual(true);
+  });
+  test('Should display the prop as value', () => {
+    const display = shallow(<Display value='test-props' />);
+    const value = display.find('.value')
+    expect(value.text()).toEqual('test-props');
   });
 });
