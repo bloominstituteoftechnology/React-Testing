f90d8ac8e10249713a87627c0897425a3776493d
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-14 15:11:38 -0700

    tested App component to check if Display component exists in App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -4,6 +4,7 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import App from '../App';
+import Display from '../components/Display/Display';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -31,4 +32,22 @@ it('should display total to be 0  by default', () => {
 
   	expect(instance.state.total).toEqual("0");
 });	
+
+
+it('should render the p tag"', () => {
+
+        const app = shallow(<App />);
+
+        const div = app.find('.component-app');
+	expect(div.contains(<p className="calculator-title">Calculator</p>)).toEqual(true);
+
+});
+
+it('should render the Calculator Component', () => {
+ 	const app = shallow(<App />);
+
+        const div = app.find('.component-app');	
+    	expect(div.containsMatchingElement(<Display />)).toEqual(true);
+  });
+
 });
