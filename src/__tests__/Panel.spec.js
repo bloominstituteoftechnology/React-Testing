36d2aef059034c20109b79dc60d55530a8a9dc2c
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-15 16:30:22 -0700

    tested to check if Panel renders 19 Button components

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -5,11 +5,24 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import Panel from '../components/Panel/Panel';
 
+jest.mock('../logic/calculate');
+
 Enzyme.configure({ adapter: new Adapter() });
 
+
 describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
-  });
+
+it('renders without crashing', () => {
+        shallow(<Panel />);
 });
+
+ it('should render the Button components', () => {
+      const app = shallow(<Panel />);
+
+      const button = app.find('Button');
+
+      expect(button.length).toBe(19);
+    });
+  });
+
+
