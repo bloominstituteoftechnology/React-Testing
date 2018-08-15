1daf675686bb9963b97c607f361447fb61c29b35
emaquino44 <prexy44@yahoo.com>
2018-08-14 23:56:39 -0700

    still testing app.test.js

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -10,7 +10,9 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    shallow(<App />)
   });
+
+
+
 });
