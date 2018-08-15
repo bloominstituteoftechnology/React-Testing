0626d000f079c25d09ec1f11e14850e7e5a3d79c
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-08-15 14:01:55 -0700

    minor

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import sinon from 'sinon'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+
 import Button from '../components/Button/Button'
 
 Enzyme.configure({ adapter: new Adapter() })
--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+
 import Panel from '../components/Panel/Panel'
 
 Enzyme.configure({ adapter: new Adapter() })
