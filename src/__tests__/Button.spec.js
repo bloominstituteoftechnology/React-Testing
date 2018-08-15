dcaecc960bc8692fb2c3f80226bd3301f5352191
JJ Ashcraft <jj@jjashcraft.com>
2018-08-15 17:47:15 -0400

    updating component testing, button complete, display in progress

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from '../App';
+import Button, {handleClick} from '../components/Button/Button';
 
-import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -12,4 +13,19 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+it('should display a button', ()=>{
+  const root = shallow(<Button />);
+  const newButton = root.find('button');
+
+  expect(newButton.length).toBe(1);
+})
+
+ it('should display its name', ()=>{
+   
+    const newButton = shallow(<Button name={'test'} />);
+    expect(newButton.text()).toBe('test');
+    
+  })
+
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import App from '../App';
 import Display from '../components/Display/Display';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -12,4 +12,12 @@ describe('<Display />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+
+  it('should display an empty string on startup', ()=>{
+    const app = shallow(<App />)
+   
+    const display = shallow(<Display value={'0'} />);
+    expect(display.text()).toBe('0');
+    
+  })
 });
