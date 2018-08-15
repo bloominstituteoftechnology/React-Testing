75484e6d8417bf417923859a937ea8bdbf4465e3
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-15 15:20:55 -0600

    Created 6 tests on calculate.js and fixed bug with . button

--- src/__tests__/Operate.spec.js
+++ src/__tests__/Operate.spec.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Operate from '../logic/operate.js';
 import operate from '../logic/operate.js';
 
 Enzyme.configure({ adapter: new Adapter() });
