90d3fcf5be5c590857e41cbc80fe072ae061b2c8
emaquino44 <prexy44@yahoo.com>
2018-08-15 06:58:07 -0700

    finished app and button

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -12,7 +12,18 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     shallow(<App />)
   });
+ it('should by default have total = "0"', () => {
+    const _App = shallow(<App/>)
+    const instance = _App.instance()
+    expect(instance.state.total).toEqual('0')
+  })
+ it('should setState when handleClick is called', () => {
+    const _App = shallow(<App/>)
+    const instance = _App.instance()
+    const numberButton = '5'
+    const operationButton = '+'
 
-
-
+    instance.handleClick(operationButton)
+    expect(instance.state.operation).toEqual('+')
+  });
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -9,7 +9,21 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+    shallow(<Button />)
+  });
+
+
+  it('has a "orange" prop', () => {
+        const orange = { name: '+' };
+        const button = shallow(<Button {...orange} />).find('button');
+        expect(button.text()).toBe('+');
+
+});
+  it('button name matches name prop', () => {
+    const props = { name: '5' }
+    const _Button = shallow(<Button {...props} />)
+    const button = _Button.find('button')
+
+    expect(button.text()).toEqual('5')
   });
 });
