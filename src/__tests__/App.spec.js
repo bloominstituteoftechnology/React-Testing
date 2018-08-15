d3880d747c3abae6eee29b0242b60404742c97ad
DirupT <iamcooled@gmail.com>
2018-08-15 14:38:35 -0500

    added snapshot for App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
@@ -9,7 +10,11 @@ describe('<App />', () => {
         const div = document.createElement('div');
         ReactDOM.render(<App />, div);
     });
-});
+    it('should match snapshot', () => {
+        const tree = renderer.create(<App />).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+}); 
 
 it('should have total, next, operation in state as default values', () => {
     const wrapper = shallow(<App />);
