51c0309227682963fe6d357fc8897ed49c18abe0
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-15 14:50:39 -0700

    tested if the Button component has the correct className on passing different prop values

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -4,12 +4,24 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Button from '../components/Button/Button';
+import App from '../App';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
+
+it('renders without crashing', () => {
+        shallow(<Button />);
+});
+
+
+it('should test if a className passed as props exists', () => {
+	//const orange=true;
+	const app = shallow(<Button orange/>);
+	console.log(app.debug());
+
+	expect(app.hasClass('component-button orange')).toEqual(true);
+});
+
 });
