628016c09228bf111d0b2a3adb7bedda8fa3b3c5
emaquino44 <prexy44@yahoo.com>
2018-08-15 23:01:50 -0700

    added tests for panel.spec.js

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -9,7 +9,16 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+    shallow(<Panel />)
+  });
+
+ it('will show 19 buttons', () => {
+    const _Panel = shallow(<Panel/>)
+    const buttons = _Panel.find('Button')
+    expect(buttons).toHaveLength(19)
+  });
+ 	it('has a div with a className "component-panel"', () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find('.component-panel')).toHaveLength(1);
   });
 });
