4760b669a101b4fb9e37fddbbfe9e51518bf4e51
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-19 15:47:48 -0600

    Added snapshot test of the panel, since UI should not change

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import renderer from 'react-test-renderer';
 import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -12,4 +12,10 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Panel />).toJSON(); 
+    expect(tree).toMatchSnapshot(); 
+  });
+  
 });
