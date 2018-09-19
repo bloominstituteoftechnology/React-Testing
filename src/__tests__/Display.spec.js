352df37e2516d8977a8cd3c1204ed6a9f7da62e6
Carey <careycbaldwin@gmail.com>
2018-09-19 18:16:21 -0400

    added some tests to Display Component

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
 
 import Display from '../components/Display/Display';
 
@@ -11,5 +12,20 @@ describe('<Display />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
+  });
+
+  it('should return a component with classname: "component-display"', () => {
+    const component = shallow(<Display />);
+    console.log(component.length);
+    expect(component).toHaveLength(1);
+  })
+
+  it('should return the specified value', () => {
+    const component = shallow(<Display value = {"0"} />);
+    expect(component.contains(
+      <div className = "value">
+        0
+      </div>
+    )).toEqual(true);
   });
 });
