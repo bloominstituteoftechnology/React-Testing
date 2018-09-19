c40195c5b33575c4a71e68389ecaf7d8616653ef
mkerbleski <mkerbles@gmail.com>
2018-09-19 15:36:58 -0600

    completed display testing

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -8,8 +8,21 @@ import Display from '../components/Display/Display';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
-  });
+ it('renders without crashing', () => {
+   const div = document.createElement('div');
+   ReactDOM.render(<Display />, div);
+ });
+
+ it('should display 0 when no state.next or state.total', () => {
+   const wrapper = shallow(<Display value = "0" />);
+   const display = wrapper.find(".component-display");
+   expect(display.text()).toBe("0");
+ });
+
+ it('should display 0 when no state.next or state.total', () => {
+   const wrapper = shallow(<Display value = "90000000000000" />);
+   const display = wrapper.find(".component-display");
+   expect(display.text()).toBe("90000000000000");
+ });
+
 });
