49bb858fdb41a7161c613205039ceea17277786f
Ashwin <aswinjeganath@gmail.com>
2018-09-20 02:01:22 +0530

    Write button specs

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -12,4 +12,24 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+  it('calls clickHandler on click', () => {
+    const mock = jest.fn();
+    const wrapper = shallow(<Button clickHandler={mock} />);
+    // console.log(wrapper.props());
+    wrapper.find('button').simulate('click');
+    wrapper.find('button').simulate('click');
+    wrapper.find('button').simulate('click');
+
+    expect(mock).toHaveBeenCalledTimes(3);
+  });
+
+  it('calls the clickHandler with provided props', () => {
+    const mock = jest.fn();
+    const wrapper = shallow(<Button name="Ash" clickHandler={mock} />);
+
+    wrapper.find('button').simulate('click');
+
+    expect(mock).toHaveBeenCalledWith('Ash');
+  });
 });
