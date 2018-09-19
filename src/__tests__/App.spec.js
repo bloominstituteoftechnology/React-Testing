6853f7f47af4cca1877953a48d15fa4b9fd0e260
Lauren <llwor94@gmail.com>
2018-09-19 12:29:48 -0700

    init commit -m

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -9,7 +8,6 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    shallow(<App />);
   });
 });
