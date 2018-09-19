f747d8c93edcff37d46050dcc3bd6c787b459c65
codejoncode <jonathanjholloway@gmail.com>
2018-09-19 16:18:08 -0500

    added test to Display and Panel, using a function called wrapperFunc to keep the code dry which sets up wrapper and instance and returns instance.

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -6,10 +6,18 @@ import Adapter from 'enzyme-adapter-react-16';
 import Display from '../components/Display/Display';
 
 Enzyme.configure({ adapter: new Adapter() });
-
+function wrapperFunc () {
+  const wrapper = shallow(<Display />);
+  const instance = wrapper.instance(); 
+  return instance; 
+}
 describe('<Display />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+  it('should check value is a string', () => {
+    console.log(wrapperFunc())
+    //value isn't a thing i can check. instance is null.
+  })
 });
--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -6,10 +6,19 @@ import Adapter from 'enzyme-adapter-react-16';
 import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
-
+function wrapperFunc () {
+  const wrapper = shallow(<Panel />);
+  const instance = wrapper.instance(); 
+  return instance; 
+}
 describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+  it('should check if handleClick is a function', () => {
+    console.log(wrapperFunc())
+    expect(typeof wrapperFunc().handleClick).toEqual('function');
+  });
+
 });
