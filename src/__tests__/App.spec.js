575279afb3de80a521ad0e616a6f4d328ca44e15
codejoncode <jonathanjholloway@gmail.com>
2018-09-19 15:48:57 -0500

    Application test are complete may want to figure ut a way to test className's and the components that exist on it. For now there are test on the state properties and the handleClick function making sure the defaults are as desired to start

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -6,10 +6,28 @@ import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
-
+function wrapperFunc () {
+  const wrapper = shallow(<App />);
+  const instance = wrapper.instance(); 
+  return instance; 
+}
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  it('total should have total "0" by default', () => {
+    expect(wrapperFunc().state.total).toEqual("0");
+  });
+  it('next should be null', () => {
+    expect(wrapperFunc().state.next).toEqual(null);
+  });
+  it('operation should be null', () => {
+
+    expect(wrapperFunc().state.operation).toEqual(null);
+  });
+  it('handleClick should be a function', () => {
+    expect(typeof wrapperFunc().handleClick).toEqual('function')
+  });
+
 });
