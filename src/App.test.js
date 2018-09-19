871a9be28389daa0630e148841f38b74bd326aaf
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-19 15:25:35 -0700

    App.js testing if it renders children components

--- src/App.test.js
+++ src/App.test.js
@@ -5,11 +5,14 @@ import { shallow } from 'enzyme';
 
 import App from './App';
 import Button from './components/Button/Button';
+import Display from './components/Display/Display';
+import Panel from './components/Panel/Panel';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
 		shallow(<App />);
 	});
+
 	it('testing state, should be { total: "0", next: null, operation: null }', () => {
 		const wrapper = shallow(<App />);
 		const instance = wrapper.instance();
@@ -17,5 +20,15 @@ describe('<App />', () => {
 		expect(instance.state.total).toEqual('0');
 		expect(instance.state.next).toEqual(null);
 		expect(instance.state.operation).toEqual(null);
+	});
+
+	it('renders children components when passed in', () => {
+		const wrapper = shallow((
+			<div>
+				<Display />
+				<Panel />
+			</div>
+		));
+		expect(wrapper.contains(<Display /> && <Panel />)).toEqual(true);
 	});
 });
