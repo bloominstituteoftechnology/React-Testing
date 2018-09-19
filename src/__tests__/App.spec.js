ac51d61f7db760d9c697721e43b1153b3f69d5cd
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-19 15:15:01 -0400

    first test

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -12,4 +12,11 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should render a div', () => {
+    const wrapper = shallow(<App />);
+    const elements = wrapper.find('div');
+
+    expect(elements.length).toEqual(1);
+  })
 });
