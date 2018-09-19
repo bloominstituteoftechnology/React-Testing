bf823be775cf263064d387a8fe61fda29ccd7ec8
kamalnrf <kamalnrf@hotmail.com>
2018-09-20 02:30:30 +0530

    Added Tests for Button

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Button from '../components/Button/Button';
@@ -9,7 +9,24 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+    const wrapper = shallow(<Button/>)
+    expect(wrapper).toMatchSnapshot()
   });
+
+  it('check for clickhandler', () => {
+    const clickFn = jest.fn()
+    const wrapper = mount(<Button clickHandler={clickFn}/>)
+
+    expect(wrapper.props().clickHandler).toBeDefined()
+  })
+
+  it ('orange props check', () => {
+    const wrapper = mount(<Button orange />)
+    expect(wrapper.props().orange).toBe(true)
+  })
+
+  it ('wide props check', () => {
+    const wrapper = mount(<Button wide />)
+    expect(wrapper.props().wide).toBe(true)
+  })
 });
