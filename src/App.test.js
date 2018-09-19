8c780f37e9fcb638a53337d32d99aa52a392a3ef
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-19 15:17:16 -0700

    App component -testing state

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import App from './App';
+import Button from './components/Button/Button';
 
 describe('<App />', () => {
-
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);  
+		shallow(<App />);
 	});
+	it('testing state, should be { total: "0", next: null, operation: null }', () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
 
-	it('should match snapshot', () => {
-		const tree = renderer.create(<App />).toJSON();
-
-		expect(tree).toMatchSnapshot();
+		expect(instance.state.total).toEqual('0');
+		expect(instance.state.next).toEqual(null);
+		expect(instance.state.operation).toEqual(null);
 	});
 });
