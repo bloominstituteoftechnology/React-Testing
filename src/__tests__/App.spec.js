6395aecf32abb44c88b17f24fa772ddf271ee0a3
William <kwontaewoongah@gmail.com>
2018-09-19 15:16:53 -0700

    implemented testing for app component

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
@@ -9,7 +10,10 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    shallow(<App />)
   });
+  it('should match snapshot',()=>{
+    const tree=renderer.create(<App total='100'/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 });
