df6d5e613aefa2de55ed3467bffb3a920471e8c4
Charles Cortinas <crcortinas@gmail.com>
2018-09-19 17:46:49 -0500

    Initial Commit
    Initial Commit

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 
-import App from '../App';
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("matches snapshot", () => {
+    const tree = renderer.create(<App />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("initializes state correctly", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.state()).toEqual({
+      total: "0",
+      next: null,
+      operation: null
+    });
   });
 });
