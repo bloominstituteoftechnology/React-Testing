fc461744c2918a0e068abdd6f498a2cdc2a19a15
codejoncode <jonathanjholloway@gmail.com>
2018-09-19 15:58:56 -0500

    Button tested check that there is a function called handleClick

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -6,10 +6,19 @@ import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
+function wrapperFunc () {
+  const wrapper = shallow(<Button />);
+  const instance = wrapper.instance(); 
+  return instance; 
+}
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('handleClick is a function', () => {
+    expect(typeof wrapperFunc().handleClick).toEqual('function');
+  });
+
 });
