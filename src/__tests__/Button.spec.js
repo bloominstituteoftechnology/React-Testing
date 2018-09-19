abc8538d56e1f45ee9f2ebe07a9ab4e323f9b095
johnoro <officialjohnorourke@gmail.com>
2018-09-19 18:33:02 -0400

    Completed MVP with expandable tests.

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import Button from '../components/Button/Button';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+  let wrap;
+
+  beforeAll(() => {
+    wrap = shallow(<Button orange />);
+  });
+
+  describe('basic renders', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Button />, div);
+    });
+
+    it('renders correctly', () => {
+      expect(wrap).toMatchSnapshot();
+    });
+
+    it('renders a button', () => {
+      expect(wrap.children()).toHaveLength(1);
+      expect(wrap.find('button')).toHaveLength(1);
+    });
+  });
+
+  describe('props', () => {
+    it('should to use props correctly', () => {
+      expect(wrap.find('.orange')).toHaveLength(1);
+    });
   });
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import Display from '../components/Display/Display';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
+  let wrap;
+
+  beforeAll(() => {
+    wrap = shallow(<Display />);
+  });
+
+  describe('basic renders', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Display />, div);
+    });
+
+    it('renders correctly', () => {
+      expect(wrap).toMatchSnapshot();
+    });
+  });
+
+  describe('classes', () => {
+    it('should have the component-display class', () => {
+      expect(wrap.find('.component-display')).toHaveLength(1);
+    });
   });
 });
