44c765c25a3904ec9c6f8be749d689aa1e68b7d5
kamalnrf <kamalnrf@hotmail.com>
2018-09-20 02:52:16 +0530

    Added Tests for display

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Display from '../components/Display/Display';
+import App from '../App';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Display />, div);
+    const wrapper = shallow(<Display/>)
+    expect(wrapper).toMatchSnapshot()
   });
+
+  it ('value props check', () => {
+    const wrapper = mount(<Display value="30"/>)
+    expect(wrapper.props().value).toBeDefined()
+    expect(wrapper.props().value).toBe('30')
+  })
 });
