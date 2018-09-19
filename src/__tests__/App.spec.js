7ba137193e6babf25d7592fb75251a14cf155b26
Grant Reighard <grant.reighard@gmail.com>
2018-09-19 17:41:57 -0400

    Got tests working for all MVP components.

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
@@ -11,5 +12,46 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+
+  it('defaults to zero as the total in state', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.total).toBe('0');
+  });
+
+  it('defaults to null as next in state', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.next).toBe(null);
+  });
+
+  it('defaults to null as operation in state', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.operation).toBe(null);
+  });
+
+  it('should be a function', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(typeof instance.handleClick).toBe('function');
+  });
+
+  it('should match the snapshot', () => {
+    const tree = renderer.create(<App />).toJSON;
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('has a string for the button name', () => {
+    const wrapper = mount(<App />);
+
+    const button = wrapper.find('button').first();
+    expect(typeof button.text()).toBe('string');
   });
 });
