4736da56375ed37294021a673eb5133ce8dcdf56
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-09-19 16:32:12 -0500

    working on App tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 
-import App from '../App';
+import App from "../App";
 
-
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
   });
 });
