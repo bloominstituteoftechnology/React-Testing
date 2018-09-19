04ba0b34fc8ab2d167c6497f43830a4ea5ea0ac0
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-19 16:49:08 -0400

    A single App test and two Button tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+
 import App from '../App';
+import Display from '../components/Display/Display';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -12,4 +15,10 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should render a display and panel', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Display)).toHaveLength(1);
+    expect(wrapper.find(Panel)).toHaveLength(1);
+  })
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -12,4 +12,24 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+  it('should render the name prop', () => {
+    const name = "8";
+    const wrapper = shallow(<Button name={name} />);
+    const button = wrapper.find('button');
+
+    expect(button.text()).toEqual('8');
+  });
+
+  it('should perform click using name prop', () => {
+    let name = 'example';
+    let output;
+    function clickHandler(input) {
+      output = input;
+    }
+    const button = shallow(<Button name={name} clickHandler={clickHandler} />);
+    const divButton = button.find('.component-button > button');
+    divButton.simulate('click');
+    expect(output).toEqual(name);
+  });
 });
