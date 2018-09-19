91961aaf3a5fc05898e34f7a7c11596d75652518
francistse23 <wtse.ucsd@gmail.com>
2018-09-19 13:24:50 -0700

    Display and Panel tests written. Added tests for button and calculation function.

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import App from '../App';
 import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -10,5 +10,22 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('<Panel />', () => {
   it('shallow renders without crashing',  () => {
     shallow(<Panel />);
+  });
+  it('renders within <App />', () => {
+    const wrapper = mount(<App><Panel /></App>);
+    expect(wrapper.find('button').length).toBe(19);
+  });
+  it('checks the function of a button', () => {
+    const wrapper = mount(<App><Panel /></App>);
+      wrapper
+      .find('button')
+      .filterWhere(btn => btn.text() === '7')
+      .simulate('click');
+    expect(wrapper.state().next).toBe('7');
+    wrapper
+      .find('button')
+      .filterWhere(btn => btn.text() === '9')
+      .simulate('click');
+  expect(wrapper.state().next).toBe('79');
   });
 });
