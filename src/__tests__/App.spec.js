2a0987cd26cb95af5ad872b16d83fff569219b3e
William VanDolah <william.vandolah@gmail.com>
2018-09-19 15:48:37 -0500

    started

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
 
 import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('look at state, total should be zero', () => {
+    const wrapper = shallow(<App />);
+    const inst = wrapper.instance();
+   
+
+    expect(inst.state.total).toEqual('0')
+
+  })
+
+
+
+  //   it('should match snapshot', () => {
+  //   const tree = renderer.create(<App />).toJSON();
+  //   console.log(tree)
+  //   expect(tree).toMatchSnapshot();
+  // });
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import Button from '../components/Button/Button';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+  it('tests display name', () => {
+    const wrap = shallow(<Button name="test" />)
+
+    expect(wrap.text()).toEqual('test')
+  })
+
+  it('button test', () => {
+    const wrap = shallow(<Button name='test2' />)
+    const inst = wrap.instance();
+    const btn = wrap.find('button')
+    btn.simulate('click')
+    
+    // expect(inst.clickHandler).toEqual('fail')
+  })
+
 });
