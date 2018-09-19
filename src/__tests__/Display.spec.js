2fdd574756fc1022bbdc7d9b6f981e9e41329f9c
Josue Peralta <jperal77@gmail.com>
2018-09-19 17:18:47 -0500

    added setupTests.js, removed duplicate code

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+
 
 import Display from '../components/Display/Display';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
   it('renders without crashing', () => {
