57985c70589771c5c61ab056b71580748ea05295
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-09-19 12:33:28 -0700

    write tests for App, Button

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+  it('renders shallow without crashing', () => {
+    shallow(<App />);
+  });
+  it('state is initialized', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().total).toBe('0');
+    expect(wrapper.state().next).toBeFalsy();
+    expect(wrapper.state().operation).toBeFalsy();
   });
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import Button from '../components/Button/Button';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+  it('renders shallow without crashing', () => {
+    shallow(<Button />);
+  });
+  it('renders div with orange', () => {
+    const wrapper = shallow(<Button orange />);
+    const buttons = wrapper.find('.orange');
+
+    expect(buttons.length).toBe(1);
+  });
+  it('renders div with wide', () => {
+    const wrapper = shallow(<Button wide />);
+    const buttons = wrapper.find('.wide');
+
+    expect(buttons.length).toBe(1);
+  });
+  it('renders button with prop name', () => {
+    const wrapper = shallow(<Button name="abcd" />);
+    const buttons = wrapper.find('button');
+
+    expect(buttons.text()).toBe('abcd');
   });
 });
