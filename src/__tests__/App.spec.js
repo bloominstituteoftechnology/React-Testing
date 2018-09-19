50e8b3c7076883dd2684ea969da0c1a4598acd92
Lauren <llwor94@gmail.com>
2018-09-19 14:44:28 -0700

    some calculate tests done

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -20,7 +20,6 @@ describe('<App />', () => {
 
   it('should render 2 child components', () => {
     const wrapper = shallow(<App />);
-
     const elements = wrapper.find('.component-app');
 
     expect(elements.children().length).toEqual(2);
