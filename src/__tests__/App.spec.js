680edbbf71a604d8e919cf995cefcb3a112c419b
William <kwontaewoongah@gmail.com>
2018-09-19 21:46:54 -0700

    modified files to avoid duplication of configuration code

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
 
 import App from '../App';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     shallow(<App />)
   });
-  it('should match snapshot',()=>{
-    const tree=renderer.create(<App total='100'/>).toJSON();
-    expect(tree).toMatchSnapshot();
-  })
+ 
 });
