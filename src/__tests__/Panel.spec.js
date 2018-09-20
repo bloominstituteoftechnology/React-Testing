3cf4518e5b6344e0c57b35a12d069a3747af1591
William <kwontaewoongah@gmail.com>
2018-09-19 23:25:41 -0700

    finished panel

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-
+import { shallow, mount} from 'enzyme';
+import App from '../App';
 import Panel from '../components/Panel/Panel';
 
 describe('<Panel />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+    shallow(<Panel/>);
   });
+  it('changes app state',()=>{
+    const wrapper=mount(
+      <App>
+        <Panel></Panel>
+      </App>
+    )
+    wrapper.find('button').filterWhere(btn=>btn.text()==='7').simulate('click');
+    expect(wrapper.state().next).toBe('7');
+    wrapper.find('button').filterWhere(btn=>btn.text()==='+').simulate('click');
+    expect(wrapper.state().operation).toBe('+');
+    wrapper.find('button').filterWhere(btn=>btn.text()==='3').simulate('click');
+    expect(wrapper.state().total).toBe('7');
+    expect(wrapper.state().next).toBe('3');
+    wrapper.find('button').filterWhere(btn=>btn.text()==='=').simulate('click');
+    expect(wrapper.state().total).toBe('10');
+    expect(wrapper.state().operation).toBe(null);
+    expect(wrapper.state().next).toBe(null);
+  })
 });
