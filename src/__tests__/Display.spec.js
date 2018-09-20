5e39fcb2c37ca406b10d2268889dbfe48946bb9b
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-19 21:33:30 -0400

    Display.spec.js tests completed

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Display />, div);
+  });
+
+  it("renders display text", () => {
+    const wrapper = shallow(<Display value="1" />);
+    expect(
+      wrapper
+        .find("div")
+        .not(".component-display")
+        .text()
+    ).toEqual("1");
   });
 });
