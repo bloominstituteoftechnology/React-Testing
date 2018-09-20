249dffc7190ab6335561405507dbc901f56677be
William <kwontaewoongah@gmail.com>
2018-09-19 22:09:02 -0700

    finished buttons

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,6 +7,21 @@ import Button from '../components/Button/Button';
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
-    
+    shallow(<Button/>);
   });
+  it('renders button with orange',()=>{
+    const wrapper=shallow(<Button orange/>);
+    const buttons=wrapper.find('.orange');
+    expect(buttons.length).toBe(1);
+  })
+  it('renders button with wide',()=>{
+    const wrapper=shallow(<Button wide/>);
+    const buttons=wrapper.find('.wide');
+    expect(buttons.length).toBe(1);
+  })
+  it('renders button with prop name',()=>{
+    const wrapper=shallow(<Button name='Prudence'/>)
+    const buttons=wrapper.find('button');
+    expect(buttons.length).toBe(1);
+  })
 });
