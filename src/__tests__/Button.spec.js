9d499a07de75f8e5d00456a0cc5c96f5e85d89cc
decagondev <decagondevelopment@gmail.com>
2018-10-24 20:52:21 +0100

    (MVP) : add button : renders without crashing and snapshot tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Button from '../components/Button/Button';
+import Button from "../components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+describe("<Button />", () => {
+  it("renders without crashing", () => {
+    const comp = shallow(<Button />);
+    expect(comp).toMatchSnapshot();
   });
 });
