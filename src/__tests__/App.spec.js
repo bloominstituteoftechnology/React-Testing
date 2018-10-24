f4793ae7861c39b1e8f4838179bdc72541cb28c0
Andrew <ghobrial.andrew@gmail.com>
2018-10-24 17:37:34 -0400

    app.spec.js complete

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -8,8 +8,22 @@ import App from '../App';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  it('renders without crashing', () => {
+ 
+  it('renders a div correctly', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it("correctly renders the component-app div specifically", () => {
+    const wrapper = shallow(<App />);
+    const elements = wrapper.find(".component-app");
+     expect(elements.length).toBe(1);
+  });
+   
+  it("state value of total should be 0 for display by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.total).toBe("0");
+  });
+
 });
