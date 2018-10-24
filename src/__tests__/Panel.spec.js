019a6f6a1ea79a941d97f9608282e388ecba039f
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-24 18:02:50 -0400

    added <Panel /> tests, row test currently failing as expected because panel-row class has not been added to Panel.js

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -12,4 +12,20 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  test('should render a panel component', () => {
+    const wrapper = shallow(<Panel />);
+    const elements = wrapper.find('div.component-panel');
+
+    expect(elements.length).toBe(1);
+  });
+
+  test('should render five panel rows', () => {
+    const wrapper = shallow(<Panel />);
+    const elements = wrapper.find('div.panel-row');
+
+    expect(elements.length).toBe(5);
+  });
+
+
 });
