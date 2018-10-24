c3ad169e580e2c3e15e6c76d4708dbfe2c609d79
Andrew <ghobrial.andrew@gmail.com>
2018-10-24 18:11:39 -0400

    panel.spec.js basic tests complete

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -8,8 +8,16 @@ import Panel from '../components/Panel/Panel';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
+  
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it("confirms panel component correctly displays", () => {
+    const shallowWrapper = shallow(<Panel />);
+    const elements = shallowWrapper.find("div.component-panel");
+    expect(elements.length).toBe(1);
+  });
+
 });
