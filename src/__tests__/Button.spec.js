78c8f4c5aacb3544ef8c01bedebd513b2a9a7260
benny <oseguerabenny27@gmail.com>
2018-10-24 14:40:41 -0700

    Some simple tests, will add more - Benny Oseguera

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Button from '../components/Button/Button';
+import Button from "../components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Button />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Button />, div);
+  });
+
+  it("checks if there is a button", () => {
+    const wrapper = shallow(<Button />);
+
+    const elements = wrapper.find("button");
+
+    expect(elements.length).toBe(1);
   });
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
+import { toBuffer } from "ip";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Display />, div);
+  });
+
+  it("renders display", () => {
+    const wrapper = shallow(<Display />);
+    const elements = wrapper.find("div.component-display");
+
+    expect(elements.length).toBe(1);
   });
 });
