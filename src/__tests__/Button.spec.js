85c6255891b14c01a97f9efd2ee752f9792d97af
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-24 14:26:28 -0700

    add display render test, add button render test

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Button from '../components/Button/Button';
+import Button from "../components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
+describe("<Button />", () => {
+	it("renders without crashing", () => {
+		const div = document.createElement("div");
+		ReactDOM.render(<Button />, div);
+	});
+	it("renders the display", () => {
+		const wrapper = shallow(<Button />);
+		const instance = wrapper.instance();
+		const elements = wrapper.find("component-button");
+		expect(elements.length).toBe(0);
+	});
 });
