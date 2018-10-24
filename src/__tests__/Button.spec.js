63731eab6229f99abcccc9547c766c5cdbc6c83f
Alexandra <alexswartz9753@gmail.com>
2018-10-24 14:27:15 -0400

    created snapshot of Button component and added tests for Button

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Button from '../components/Button/Button';
@@ -9,7 +10,24 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+    shallow(<Button />);
   });
+
+  it('should match snapshot', ()=> {
+    const tree = renderer.create(<Button />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should display button', ()=> {
+    const wrapper = shallow(<Button />);
+    const display = wrapper.find('button');
+    expect(display.length).toBe(1);
+  });
+
+  it('should have a class component-button', ()=> {
+    const wrapper = shallow(<Button />);
+    const display = wrapper.find('.component-button');
+    expect(display.length).toBe(1);
+  });
+  
 });
