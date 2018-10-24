06f5a1c95c7ee4108377bba1b0fd5d7b34135585
michaelagard <mdagard@gmail.com>
2018-10-24 17:00:46 -0400

    add tests for App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -9,7 +9,16 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    // const div = document.createElement('div');
+    // ReactDOM.render(<App />, div);
+    shallow(<App />);
   });
+  it('renders correct UI', () => {
+    expect(shallow(<App />).find('div.component-app').length).toBe(1);
+  })
+  it('state should be total: "0", next: null, operation: null', () => {
+    expect(shallow(<App />).instance().state.total).toBe('0');
+    expect(shallow(<App />).instance().state.next).toBe(null);
+    expect(shallow(<App />).instance().state.operation).toBe(null);
+  })
 });
