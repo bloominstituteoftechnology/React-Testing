961189654c9aad9fbbed7338e7a2d4f2c2444364
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-24 17:31:13 -0400

    added some basic <App /> tests in App.spec.js

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -12,9 +12,29 @@ import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
+const wrapper = (shallow(<App />));
+const instance = wrapper.instance();
+
+describe('<App /> matching and component tests', () => {
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  test('render should match snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('<App /> should have className called component-app', () => {
+    const elements = wrapper.find('.component-app');
+    expect(elements.length).toBe(1);
+  });
+
+  test('application state should be null', () => {
+    expect(instance.state.operation).toBe(null);
+  });
+
+  // will add some more when MVP is complete, keeping tests basic in the interest of time
 });
