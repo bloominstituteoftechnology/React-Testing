fdb06e6be21a9524e6d876014c2704a0271b5e59
BrianARuff <thelonewolf198927@gmail.com>
2018-10-24 18:59:22 -0400

    react testing

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 
-import App from '../App';
+import App from "../App";
+import { wrap } from "module";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+describe("<App />", () => {
+  const wrapper = shallow(<App />);
+  const elements = wrapper.find("div.component-app");
+  const instance = wrapper.instance();
+  it("should match snapshot", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
+  it("renders without crashing", () => {
+    wrapper;
+  });
+  it("should only have one div with a class name of component-app", () => {
+    expect(elements.length).toBe(1);
+  });
+  it("should have two children elements", () => {
+    expect(elements.children().length).toBe(2);
+  });
+  it("should have App.state.total total of '0' by default", () => {
+    expect(instance.state.total).toBe("0");
+  });
+  it("should have App.state.total of 1337 after being set", () => {
+    instance.setState({ total: "1337" });
+    expect(instance.state.total).toBe("1337");
+  });
+  
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import renderer from 'react-test-renderer';
 import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
+  const elements
+  it('should render w/o crashing', () => {
+
+  })
 });
