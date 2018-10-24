280b2675869edcaae992d34752e1deafb01cb823
Max <maxkajiwara@gmail.com>
2018-10-24 14:29:39 -0700

    Move enzyme config to setupTests.js

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import Button from '../components/Button/Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Button />, div);
+	});
 });
