f427a12dff2ef4347c1b2e6fd19c33c04db429f2
decagondev <decagondevelopment@gmail.com>
2018-10-24 23:51:21 +0100

    (STRETCH) : calculate : should return a total of -10 if start with a total of 10 and we give a next of 20 and an operation of - test

--- src/__tests__/calculate.spec.js
+++ src/__tests__/calculate.spec.js
@@ -16,4 +16,21 @@ describe("calculate", () => {
       operation: null
     });
   });
+
+  it("should return a total of -10 if start with a total of 10 and we give a next of 20 and an operation of -", () => {
+    expect(
+      calculate(
+        {
+          total: "10",
+          next: "20",
+          operation: "-"
+        },
+        "="
+      )
+    ).toEqual({
+      total: "-10",
+      next: null,
+      operation: null
+    });
+  });
 });
