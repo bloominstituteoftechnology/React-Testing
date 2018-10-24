95fa6fa96038089e26486b307962f5098e50f5bc
Timothy Hoang <timh1203@gmail.com>
2018-10-24 17:50:40 -0400

    Add tests to Button component

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 import App from '../App';
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Button from '../components/Button/Button';
 
+
 describe('<Button />', () => {
+  const wrapper = (shallow(<Button />))
+  const instance = wrapper.instance();
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Button />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+  it('should shallow render without crashing', () => {
+    wrapper;
+  })
+  it('should have a div with a className of "component-button"', () => {
+    expect(wrapper.find('div').hasClass('component-button')).toBe(true);
+  })
+  it('should have a button component', () => {
+    expect(wrapper.find('div.button')).tobeTruthy;
+  })
+  it('should check it button component exists', () => {
+    expect(wrapper.find('div').exists('button')).tobeTruthy;
+  })
 });
