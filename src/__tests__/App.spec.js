6efd0951f691c3795ebd7b6e3e3647960712bbb9
Nicocchi <nicocchilover@gmail.com>
2018-10-24 16:54:15 -0400

    updated app test

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
 
 import App from '../App';
 import Display from '../components/Display/Display';
@@ -18,4 +17,15 @@ describe('<App />', () => {
   it('renders Panel without crashing', () => {
     shallow(<Panel />);
   });
+
+  it('state is initially set', () => {
+    const wrapper = shallow(<App />);
+    const state = {
+      total: '0',
+      next: null,
+      operation: null
+    };
+
+    expect(wrapper.state()).toEqual(state);
+  })
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
 
 import Display from '../components/Display/Display';
 
