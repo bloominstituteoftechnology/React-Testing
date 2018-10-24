b25ea479f037b88fb9802ffbde28414a9bb57091
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-24 14:09:49 -0700

    add render display, add next should be null test

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import App from '../App';
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
+describe("<App />", () => {
+	it("renders without crashing", () => {
+		const div = document.createElement("div");
+		ReactDOM.render(<App />, div);
+	});
+	it("renders the display", () => {
+		const wrapper = shallow(<App />);
+		const elements = wrapper.find(".component-app");
+		expect(elements.length).toBe(1);
+	});
+	it("next and operatio should be null by default", () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+
+		expect(instance.state.next).toBe(null);
+	});
 });
