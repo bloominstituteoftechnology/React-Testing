26c7aac08c6710099416ff79414b62a9d1c5a182
decagondev <decagondevelopment@gmail.com>
2018-10-24 23:58:05 +0100

    (POST MVP / STRETCH) : made it more dry

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
+import { shallow } from "enzyme";
+import "../testsetup";
 import Button from "../components/Button/Button";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Button />", () => {
   it("renders without crashing and matches the snapshot", () => {
--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
+import { shallow } from "enzyme";
+import "../testsetup";
 import Panel from "../components/Panel/Panel";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Panel />", () => {
   it("should render without crashing and match snapshot", () => {
