d25eb9e45f74d1a53c8268df3f0ac6f0b854808b
Nicocchi <nicocchilover@gmail.com>
2018-10-24 15:38:18 -0400

    basic app tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -3,9 +3,19 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 import App from '../App';
+import Display from '../components/Display/Display';
+import Panel from '../components/Panel/Panel';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     shallow(<App />);
+  });
+
+  it('renders Display without crashing', () => {
+    shallow(<Display />);
+  });
+
+  it('renders Panel without crashing', () => {
+    shallow(<Panel />);
   });
 });
