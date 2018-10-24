409802c719c8b552fbd1cdac24ea62a24d357f64
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-24 17:18:07 -0400

    fixed typo in package.json, added imports to App.spec.js

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+
+// component imports
 
 import App from '../App';
+import Display from '../components/Display/Display';
+import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
