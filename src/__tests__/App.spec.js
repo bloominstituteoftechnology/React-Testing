9b75e95dc95bed223e81b0f7455a33109542d35e
Nicocchi <nicocchilover@gmail.com>
2018-10-24 15:21:28 -0400

    added a setupTests.js file

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    shallow(<App />);
   });
 });
