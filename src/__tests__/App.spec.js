64d7eef3f2a0b7139d5ceb9bf9b9a185fa8fe6c2
alejandrok93 <alejandrok93@gmail.com>
2018-10-24 13:37:57 -0700

    Added first React test

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import App from '../App';
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<App />, div);
+  });
+
+  it("renders component div properly", () => {
+    const wrapper = shallow(<App />);
+    const elements = wrapper.find(".component-app");
+
+    expect(elements.length).toBe(1);
   });
 });
