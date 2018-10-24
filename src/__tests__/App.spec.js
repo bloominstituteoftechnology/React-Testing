c0e3f9804e810050f869d64081edb852e02425cc
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-24 14:40:33 -0700

    Made 5 tests for Button Component.

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
+// import renderer from 'react-test-renderer';
 
+import '../setupTests.js';
 import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    // const div = document.createElement('div');
+    // ReactDOM.render
+  shallow(<App />);
+  // , div);
   });
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
+import '../setupTests.js';
 import Button from '../components/Button/Button';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Button />).toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
+
+  it('renders without crashing', () => {
+  shallow(<Button />);
+  });
+
+  it('should render a button', () => {
+    const wrapper = shallow(<Button />);
+
+    const elements = wrapper.find('button');
+
+    expect(elements.length).toBe(1);
+  });
+
+  it('renders a button with a component-button class', () => {
+    const wrapper = shallow(<Button />);
+
+    const elements = wrapper.find('.component-button');
+
+    expect(elements.length).toBe(1);
+  });
+
+  it('renders an orange button', () => {
+    const wrapper = shallow(<Button orange />);
+
+    const elements = wrapper.find('.orange');
+
+    expect(elements.length).toBe(1);
+  });
+
+  it('renders a wide button', () => {
+    const wrapper = shallow(<Button wide />);
+
+    const elements = wrapper.find('.wide');
+
+    expect(elements.length).toBe(1);
+  });
+
+  
+
+  // it('simulates click events', () => {
+  //   const wrapper = shallow(<Button />);
+  //   const instance = wrapper.instance();
+
+  //   const button = wrapper.find('button');
+
+  //   button.simulate('click');
+  //   expect(instance).to.have.property('name');
+
+  // });
 });
