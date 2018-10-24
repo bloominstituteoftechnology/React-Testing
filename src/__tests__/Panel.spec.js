51618191d97261ad07322d9839cb1b7a3c502b18
Keiran <keirankozlowski@gmail.com>
2018-10-24 17:24:31 -0400

    Finished panel testing.

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -12,4 +12,11 @@ describe('<Panel />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it('renders 19 buttons', () => {
+    const wrapper = shallow(<Panel />);
+    const buttons = wrapper.find('Button');
+
+    expect(buttons).toHaveLength(19);
+  });
 });
