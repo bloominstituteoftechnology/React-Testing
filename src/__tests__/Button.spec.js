43f829e56c468b38e590547f58be90d9ae5671c9
Andrew <ghobrial.andrew@gmail.com>
2018-10-24 17:55:46 -0400

    button.spec.js basic tests complete

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -8,8 +8,16 @@ import Button from '../components/Button/Button';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
+  
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+  it("button component correctly displays", () => {
+    const wrapper = shallow(<Button />);
+    const button = wrapper.find("button");
+    expect(button.length).toBe(1);
+  });
+
 });
