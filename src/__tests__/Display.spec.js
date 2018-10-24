80f5c0ac3f31dccbe4d595f372285b5628e81c7f
michaelagard <mdagard@gmail.com>
2018-10-24 18:21:45 -0400

    add display and panel tests to spec files

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import renderer from 'react-test-renderer';
 import Display from '../components/Display/Display';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Display />', () => {
+describe.only('<Display />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+  it('matches snapshot', () => {
+    expect(renderer.create(<Display />).toJSON()).toMatchSnapshot;
+  })
+  it('renders correct UI', () => {
+    expect(shallow(<Display />).find('.component-display').length).toBe(1);
+  });
+  // it('renders props correctly', () => {
+  //   expect(shallow(<Display value="0" />).props().this.props).to.equal("0"); // couldn't get prop to check corretly. :(
+  // })
 });
