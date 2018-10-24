21f7dc02a6e273e79e1765ecbebfd6050009269b
decagondev <decagondevelopment@gmail.com>
2018-10-24 21:03:01 +0100

    (MVP) : add button : should render with the classNames of 'component-button orange' if we pass the 'orange' prop from the calling or container component and matches the snapshot

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -14,10 +14,17 @@ describe("<Button />", () => {
   });
 
   describe("Prop Tests", () => {
-    it("should render with className 'component-button wide' if we pass in the 'wide' prop from the calling or container component and matches the snapshot", () => {
+    it("should render with the classNames of 'component-button wide' if we pass in the 'wide' prop from the calling or container component and matches the snapshot", () => {
       const comp = shallow(<Button wide />);
       expect(comp.find(".component-button")).toHaveLength(1);
       expect(comp.find(".wide")).toHaveLength(1);
+      expect(comp).toMatchSnapshot();
+    });
+
+    it("should render with the classNames of 'component-button orange' if we pass the 'orange' prop from the calling or container component and matches the snapshot", () => {
+      const comp = shallow(<Button orange />);
+      expect(comp.find(".component-button")).toHaveLength(1);
+      expect(comp.find(".orange")).toHaveLength(1);
       expect(comp).toMatchSnapshot();
     });
   });
