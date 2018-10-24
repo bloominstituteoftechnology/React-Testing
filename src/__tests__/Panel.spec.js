75418a50d232cbe7d701dbc494e6417255c449ad
Vera Simon <ghjkl>
2018-10-24 15:34:46 -0700

    React-Testing
    Added some tests to Panel.spec.js

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import App from '../App';
 import Panel from '../components/Panel/Panel';
 
 describe('<Panel />', () => {
@@ -8,10 +9,18 @@ describe('<Panel />', () => {
 		shallow(<Panel />);
 	});
 
-	it('should contain 19 <Button /> nodes', () => {
+	it('should contain 19 <Button /> nodes and 6 <div> nodes', () => {
 		const wrapper = shallow(<Panel />);
-		const elements = wrapper.find('Button');
+		const buttons = wrapper.find('Button');
+		const divs = wrapper.find('div');
 
-		expect(elements).toHaveLength(19);
+		expect(buttons).toHaveLength(19);
+		expect(divs).toHaveLength(6);
+	});
+
+	it('should have a prop named "clickHandler"', () => {
+		const wrapper = shallow(<App />);
+		const panel = wrapper.find('Panel');
+		expect(panel.prop('clickHandler')).toHaveLength(1);
 	});
 });
