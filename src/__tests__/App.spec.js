b6dc793ccf2aca450962dbe7ae0b5706e32e99fb
Lucas Beemer <lucasbeemer@gmail.com>
2018-10-24 14:41:55 -0700

    App.spec tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -11,5 +10,23 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+
+  it('total should be 0 by default', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.total).toEqual('0');
+  });
+
+  it('next should be null by default', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.next).toEqual(null);
+  });
+
+  it('operation should be null by default', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.operation).toEqual(null);
   });
 });
--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
