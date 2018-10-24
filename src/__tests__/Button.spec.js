f25bdb6056c3beffd58a07e404572d96ef8dfbb5
Tsai Huang <tsaihuangsd@gmail.com>
2018-10-24 15:54:55 -0700

    Initial Commit
    test cases for Button and Panel classes

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -8,8 +8,28 @@ import Button from '../components/Button/Button';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
+  const wrapper = shallow(<Button />);
+
+  //Check if Button renders
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  //Check if renders a div with a className 'component-button' by default
+  it('renders a div with className "component-button"', ()=>{
+    const elements = wrapper.find("div.component-button");
+    expect(elements.length).toBe(1);
+  })
+  //Check if renders a div with a className 'component-button orange' with this.props.orange
+  it('renders a div with className "component-button orange" with this.props.orange', ()=>{
+    const wrapper = shallow(<Button orange />)
+    const elements = wrapper.find("div").hasClass("orange");
+    expect(elements).toBe(true);
+  })
+  //Check if renders a div with a className 'component-button wide' with this.props.wide
+  it('renders a div with className "component-button wide" with this.props.wide', ()=>{
+    const wrapper = shallow(<Button wide />)
+    const elements = wrapper.find("div").hasClass("wide");
+    expect(elements).toBe(true);
+  })
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -8,8 +8,16 @@ import Display from '../components/Display/Display';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Display />', () => {
+  const wrapper = shallow(<Display />);
+  //Check if render Display
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+
+  //Check if renders a component-panel class
+  it('renders a "component-panel" class', ()=>{
+    const elements = wrapper.find("div.component-display");
+    expect(elements.length).toBe(1);
+  })
 });
