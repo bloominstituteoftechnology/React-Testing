125aae6adcd2fe345a3c7939daa4a18136839b41
decagondev <decagondevelopment@gmail.com>
2018-10-24 20:57:12 +0100

    (MVP) : add button : should render with className 'component-button wide' if we pass in the 'wide' prop from the calling or container component and matches the snapshot test

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -8,8 +8,17 @@ import Button from "../components/Button/Button";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Button />", () => {
-  it("renders without crashing", () => {
+  it("renders without crashing and matches the snapshot", () => {
     const comp = shallow(<Button />);
     expect(comp).toMatchSnapshot();
+  });
+
+  describe("Prop Tests", () => {
+    it("should render with className 'component-button wide' if we pass in the 'wide' prop from the calling or container component and matches the snapshot", () => {
+      const comp = shallow(<Button wide />);
+      expect(comp.find(".component-button")).toHaveLength(1);
+      expect(comp.find(".wide")).toHaveLength(1);
+      expect(comp).toMatchSnapshot();
+    });
   });
 });
