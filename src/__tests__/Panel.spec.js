7ac02dc2196a2d152814903f550383467506fec0
Max <maxkajiwara@gmail.com>
2018-10-24 15:57:26 -0700

    Add tests

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -9,4 +9,22 @@ describe('<Panel />', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<Panel />, div);
 	});
+
+	it('renders five divs', () => {
+		const wrapper = shallow(<Panel />).children();
+
+		const elements = wrapper.find('div');
+
+		expect(elements.length).toBe(5);
+	});
+
+	it('renders nineteen buttons', () => {
+		const wrapper = shallow(<Panel />);
+
+		const elements = wrapper.find('Button');
+
+		expect(elements.length).toBe(19);
+	});
+
+	// should call props.clickhandler on button click
 });
