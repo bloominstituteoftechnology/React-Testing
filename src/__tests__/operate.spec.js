00115eaeb079392382474d159ae2a526190aa150
decagondev <decagondevelopment@gmail.com>
2018-10-24 22:57:48 +0100

    (STRETCH) : operate : accepts numbers as input test

--- src/__tests__/operate.spec.js
+++ src/__tests__/operate.spec.js
@@ -4,4 +4,8 @@ describe("operate", () => {
   it("accepts strings as input", () => {
     expect(operate("555", "343", "+")).toEqual("898");
   });
+
+  it("accepts numbers as input", () => {
+    expect(operate(555, 343, "+")).toEqual("898");
+  });
 });
