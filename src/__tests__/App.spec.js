5ac5f358692880dfd71748c2862f752a26aced48
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-24 16:44:57 -0500

    MVP - App Testing - 1) Initiated project with yarn install v1.9.4 [1/4] Resolving packages... success Already up-to-date. Done in 1.16s. 2) Added tests into App.spec.js for App.js.

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -5,6 +5,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import App from '../App';
 
+
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
@@ -12,4 +13,31 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should have `total`, `next`, and `operation` in state', () => {
+    const component = shallow(<App />);
+    
+    expect(component.state('total')).toEqual('0');
+    expect(component.state('next')).toBeDefined();
+    expect(component.state('operation')).toBeDefined();
+  });
+
+  it('should have `handleClick` method that updates state when clicked', () => {
+    const component = shallow(<App />);
+
+    component.instance().handleClick('5');
+
+    expect(component.state('total')).toBeNull();
+    expect(component.state('next')).toEqual('5');
+    expect(component.state('operation')).toBeNull();
+  });
+
+  it('should have className `component-app` within a div', () => {
+    const wrapper = shallow(<App />);
+
+    const elements = wrapper.find('div.component-app');
+
+    expect(elements.length).toBe(1); 
+  });
+
 });
