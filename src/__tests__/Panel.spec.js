8520a882c5fd84406007db69c6b2bbfcdab01225
Eva <evalee904@gmail.com>
2018-10-25 08:57:51 -0500

    mvp.

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import renderer from 'react-test-renderer';
 import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+    shallow(<Panel />);
   });
+
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Panel />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 });
