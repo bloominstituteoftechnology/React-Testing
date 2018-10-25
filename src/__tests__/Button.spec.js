7f9017f3ddec95d68e8cabac9ecfb181e1e2033e
BrianARuff <thelonewolf198927@gmail.com>
2018-10-24 22:09:59 -0400

    more testing stuff, no idea what I'm actually doing, I'm reallly bad at TDD

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -4,6 +4,7 @@ import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import renderer from "react-test-renderer";
 import Button from "../components/Button/Button";
+import { wrap } from "module";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -13,5 +14,16 @@ describe("<Button />", () => {
   it("should match snapshot", () => {
     expect(tree).toMatchSnapshot();
   });
-  it("should render w/o crashing", () => {});
+  it("should render w/o crashing", () => {
+    wrapper;
+  });
+  it("should have a div with class name component-button", () => {
+    expect(wrapper.find("component-button")).toBeTruthy();
+  });
+  it("should have a button", () => {
+    expect(wrapper.find("button").length).toBe(1);
+  });
+  it("simulates click", () => {
+    expect(wrapper.find('button').simulate('onClick').props().name);
+  });
 });
