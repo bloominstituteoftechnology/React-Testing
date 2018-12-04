798e9c951d20d39c2b250b0eab092ab8b5ebcf5d
Lee Len Formento <leebatef@gmail.com>
2018-12-04 15:43:03 -0500

    App tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+
 
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
-});
+  it('should render without crashing', () => {
+    shallow(<App />);
+  }); // should render without crashing
+
+  it('should render matched snapshots', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  }); // should render matched snapshots
+
+  it('should render <div className="component-app">', () => {
+    const wrapper = shallow(<App />);
+    const elements = wrapper.find('.component-app');
+    expect(elements.length).toBe(1);
+  }); // should render <div className="component-app
+
+}); // <App />
