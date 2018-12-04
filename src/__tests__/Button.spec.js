7d671a9e7d32925cbebfea5b0ac2c31b9ec2f0f9
Lee Len Formento <leebatef@gmail.com>
2018-12-04 15:05:40 -0500

    Test them buttons

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -8,8 +7,26 @@ import Button from '../components/Button/Button';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-  });
-});
+  it('should render without crashing', () => {
+    shallow(<Button />);
+  }); // should w/o crashing
+
+  it('should render the button', () => {
+    const wrapper = shallow(<Button />);
+    const elements = wrapper.find('button');
+    expect(elements.length).toBe(1);
+  }) // should render the button
+
+  it('should render an orange button', () => {
+    const wrapper = shallow(<Button orange />);
+    const elements = wrapper.find('.orange');
+    expect(elements.length).toBe(1);
+  }); // should render an orange button
+
+  it('should render a wide button', () => {
+    const wrapper = shallow(<Button wide />);
+    const elements = wrapper.find('.wide');
+    expect(elements.length).toBe(1);
+  }) // should render a wide button
+
+}); // <Button />
