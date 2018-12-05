cdbddddfd535aee48a954272f5ac6ff29db8ae99
Axolotl <samuelmachat@gmail.com>
2018-12-05 17:48:18 -0500

    button tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,4 +7,25 @@ describe('<Button />', () => {
   it('renders without crashing', () => {
     shallow(<Button />)
   })
+
+  it('should render a button', () => {
+    const wrapper = shallow(<Button />)
+
+    expect(wrapper.find('button').length).toBe(1)
+  })
+
+  it('should render a button with a name', () => {
+    const wrapper = shallow(<Button name="9" />)
+
+    expect(wrapper.find('button').render()[0].children[0].data).toBe('9')
+  })
+
+  it('should execute a passed function on click', () => {
+    const mockFunction = jest.fn()
+    const wrapper = shallow(<Button clickHandler={mockFunction} />)
+
+    wrapper.find('button').simulate('click')
+
+    expect(mockFunction).toHaveBeenCalledTimes(1)
+  })
 })
