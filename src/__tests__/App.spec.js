b1f5a012b5b607bda8709dd9a5cb861fbe4cd8cf
bummings <eabummings@gmail.com>
2018-12-05 18:34:26 -0500

    added more tests to the app component

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -13,4 +13,24 @@ describe('<App />', () => {
     const snapshot = renderer.create(<App />).toJSON();
     expect(snapshot).toMatchSnapshot();
   });
+
+  it('should have state set to zero upon load', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.total).toBe('0');
+  });
+
+  it('should pass the value zero to the display upon load', () => {
+    const wrapper = shallow(<App />);
+    const display = wrapper.find('Display');
+
+    expect(display.props()).toEqual({ value: '0' });
+  });
+
+  it('should render a div with component-app class', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('div.component-app').length).toBe(1);
+  });
 });
--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Panel from '../components/Panel/Panel';
 
 describe('<Panel />', () => {
-	it('renders without crashing', () => {
-		shallow(<Panel />);
-	});
+  it('renders without crashing', () => {
+    shallow(<Panel />);
+  });
+
+  // it('should handle click', () => {
+  //   const wrapper = shallow(<Panel />);
+  //   const instance = wrapper.instance();
+
+  //   const button = wrapper.find('.component-panel');
+
+  //   expect(button).toBe(true);
+  // });
 });
