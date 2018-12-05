645855840fe712c6d78105579fc1999784417610
shawn <shawnantonucci2018@gmail.com>
2018-12-05 14:10:23 -0800

    setup button and display tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Button from '../components/Button/Button';
+import { wrap } from 'module';
 
 describe('<Button />', () => {
 	it('renders without crashing', () => {
@@ -11,7 +12,15 @@ describe('<Button />', () => {
 	it("should render a button", () => {
 		const wrapper = shallow(<Button />);
 	
-		expect(wrapper.find("div.component-button").length).toBe(1);
-	  });
+		expect(wrapper.find('div.component-button').length).toBe(1);
+	});
+
+	it("check if clicked", () => {
+		const handleClick = jest.fn();
+		const wrapper = shallow(<Button name="%" clickHandler={handleClick} />);
+		
+		wrapper.find('button').simulate('click')
+		expect(handleClick).toBeCalled();
+	});
 	
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -7,4 +7,15 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
+
+	it("should render a Display", () => {
+		const wrapper = shallow(<Display />);
+	
+		expect(wrapper.find("div.component-display").length).toBe(1);
+	});
+
+	// it("check props", () => {
+	// 	const wrapper = shallow(<Display value="1" />);
+	// 	expect(wrapper.props().value).to.equal(1);
+	// });
 });
