6c7d03087f50bd6da7ad5f827351703be39c84f5
Riley-Brown <riley@riley.gg>
2018-12-05 18:53:26 -0500

    added tests for components

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import App from '../App';
+import App from "../App";
 
-describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+});
+
+describe("<App />", () => {
+  it("value state should be 0 by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.total).toBe("0");
+  });
+});
+
+describe("<App />", () => {
+  it("operation state should be null by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.operation).toBe(null);
+  });
+});
+
+describe("<App />", () => {
+  it("next state should be null by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.next).toBe(null);
+  });
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
 
-describe('<Display />', () => {
-	it('renders without crashing', () => {
-		shallow(<Display />);
-	});
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    shallow(<Display />);
+  });
+});
+
+describe("<Display />", () => {
+  it("should be a div with the className of component-display", () => {
+    const wrapper = shallow(<Display />);
+    expect(wrapper.find("div.component-display"));
+  });
 });
