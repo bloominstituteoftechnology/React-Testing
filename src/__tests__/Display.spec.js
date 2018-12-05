1bd1ac1deb715d3040a65cf4b5c1f0f24fcaee52
pedro <pedroluismontesinosrivera@gmail.com>
2018-12-05 18:16:58 -0500

    mvp done

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Display from '../components/Display/Display';
 
@@ -7,4 +8,14 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
+
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<Display/>).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	})
+
+	it('has a div with a "component-display" className ', () => {
+		const wrapper = shallow(<Display/>);
+		expect(wrapper.find('div.component-display').length).toBe(1);
+	})
 });
