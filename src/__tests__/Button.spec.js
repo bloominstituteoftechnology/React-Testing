9e402cf832aa688c9d66727b8a647a128b1e23d1
Mindysueasaurus <mymindyyeager@gmail.com>
2018-12-05 16:44:32 -0600

    test for buttons finished

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -12,4 +12,26 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+  it('should have a div with the classname: component-button', () => {
+    const wrapper = shallow(<Button/>)
+    const elements = wrapper.find('.component-button')
+    expect(elements.exists()).toBe(true)
+  });
+
+  it('should have classnames of wide or orange if passed as props', () => {
+    const buttonOrange = shallow(<Button orange/>)
+    const buttonWide = shallow(<Button wide />)
+    const button = shallow(<Button/>)
+   
+    expect(buttonWide.find('.wide').exists()).toBe(true)
+    expect(buttonOrange.find('.orange').exists()).toBe(true)
+    expect(button.find('.wide').exists()).toBe(false)
+    expect(button.find('.orange').exists()).toBe(false)
+  });
+
+  it('has a click handler passed as props', () => {
+		const wrapper = shallow(<Button />);
+		expect(wrapper.find('.component-button').length).toBe(1);
+	});
 });
