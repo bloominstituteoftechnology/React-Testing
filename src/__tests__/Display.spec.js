e9bf616009287f829280b16aa8ba1c04c7fc8529
Mindysueasaurus <mymindyyeager@gmail.com>
2018-12-05 16:39:24 -0600

    tests done for display

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -12,4 +12,22 @@ describe('<Display />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
+
+  it('should have a div with the classname: "component-display"', () => {
+    const wrapper = shallow(<Display/>)
+    const elements = wrapper.find("div.component-display")
+    expect(elements.exists()).toBe(true)
+  });
+
+  it('should have a div with the value of "0"', () => {
+    const wrapper = shallow(<Display value={"0"}/>)
+    const elements = wrapper.find("div.component-display div")
+    expect(elements.text()).toBe("0")
+  });
+
+  it('renders a child div inside the main div', () => {
+		const wrapper = shallow(<Display />);
+		const children = wrapper.find('div.component-display').children();
+		expect(children.find('div').length).toBe(1);
+	});
 });
