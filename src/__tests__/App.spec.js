40a85c88f44c6a206c627a26c941e3881a8aae85
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-12-05 16:07:23 -0500

    snapshot test for components

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
 describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
+	});
+
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<App />).toJSON();
+
+		expect(snapshot).toMatchSnapshot();
+	});
+
+	it('should render Display and Panel components', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Display').length).toBe(1);
+		expect(wrapper.find('Panel').length).toBe(1);
 	});
 });
