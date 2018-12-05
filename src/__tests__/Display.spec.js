0c931b5c5bb6d3245bada7a3e4875593b039fe1e
KHollobaugh <khollobaugh@gmail.com>
2018-12-05 16:47:36 -0500

    added tests to Panel.spec.js

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
 import Display from '../components/Display/Display';
@@ -12,6 +13,11 @@ describe('<Display />', () => {
 		const wrapper = shallow(<Display />);
 		expect(wrapper.find('div.component-display').length).toBe(1);
 	});
+
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<Display />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	  })
 
 	it('should render a child div containing a value', () => {
 		const wrapper = shallow(<Display />);
--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
 import Panel from '../components/Panel/Panel';
@@ -7,4 +8,21 @@ describe('<Panel />', () => {
 	it('renders without crashing', () => {
 		shallow(<Panel />);
 	});
+
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<Panel />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	  })
+
+	it('renders a div', () => {
+		const wrapper = shallow(<Panel />);
+		expect(wrapper.find('div.component-panel').length).toBe(1);
+	})
+
+	it('should render 5 child divs', () => {
+		const wrapper = shallow(<Panel />);
+		const display = wrapper.find('div.component-panel').children();
+		expect(display.find('div').length).toBe(5);
+	})
 });
+
