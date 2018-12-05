35c087abc921952d3ada19353322b7b3eb4ba552
Peter Pham <phamhere@gmail.com>
2018-12-05 15:50:02 -0700

    added tests for Display

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
 
-describe('<Display />', () => {
-	it('renders without crashing', () => {
-		shallow(<Display />);
-	});
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    shallow(<Display />);
+  });
+  it("should render a div with component-display class", () => {
+    const wrapper = shallow(<Display />);
+    expect(wrapper.find("div.component-display").length).toBe(1);
+  });
+  it("should have undefined as default props", () => {
+    const wrapper = shallow(<Display />);
+    expect(wrapper.props().value).toEqual(undefined);
+  });
+  it("should check if className component-display exists", () => {
+    const wrapper = shallow(<Display />);
+    expect(wrapper.find(".component-display").exists()).toEqual(true);
+  });
 });
