d216e5b6ef619799196df42a332d62712be8c390
Michael Checo <checomichael2@gmail.com>
2018-12-05 16:06:25 -0500

    initial commit, created 1st snapshot

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import renderer from 'react-test-renderer';
 import Panel from '../components/Panel/Panel';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Panel />, div);
+  it.skip('renders without crashing', () => {
+   shallow(<Panel />);
   });
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Panel />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 });
