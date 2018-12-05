b81b9be6e0bd8f5f4a45b93c3eef23c81bfb363c
lajawanti <myemail>
2018-12-05 16:01:33 -0500

    App.spec.js Complete

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
 
 import App from '../App';
 
+//TEST  FOR  App.js
 describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<App />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	});
+
+	//Will check this.state.total set to 0
+	it("should have the variable 'total' defined and initially set to zero in state", () => {
+		  const comp = shallow(<App />);
+		  expect(comp.state("total")).toEqual("0");
+	});
+
+	it("should have the variables 'operation' and 'next' defined in state", () => {
+		const comp = shallow(<App />);
+		expect(comp.state("next")).toBeDefined();
+		expect(comp.state("operation")).toBeDefined();
+	});
+	
 });
