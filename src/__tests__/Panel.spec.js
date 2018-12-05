c7cfe87bad407a12d672bbea4fbbee8104efdcbe
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-05 17:28:10 -0600

    completed tests for panel spec js

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -7,4 +7,22 @@ describe('<Panel />', () => {
 	it('renders without crashing', () => {
 		shallow(<Panel />);
 	});
+
+	it("should render a div with component-panel class", () => {
+		const wrapper = shallow(<Panel />);
+		
+    	expect(wrapper.find("div.component-panel").length).toBe(1);
+  	});
+	
+	it('should have a div with the classname: "component-panel"', () => {
+		const wrapper = shallow(<Panel />);
+		
+		expect(wrapper.find('div.component-panel').exists()).toBe(true);
+	});
+
+	it('should have 19 buttons', () => {
+		const wrapper = shallow(<Panel />);
+		
+		expect(wrapper.find('Button').length).toBe(19);
+	});
 });
