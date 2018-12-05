bffa66e030275a895d3eed23901752fdf2cc9f0c
Michael Checo <checomichael2@gmail.com>
2018-12-05 16:44:50 -0500

    tests for App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -4,12 +4,42 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import App from '../App';
+import calculate from '../logic/calculate';
 
 Enzyme.configure({ adapter: new Adapter() });
-
+jest.mock('../logic/calculate');
+calculate.mockImplementation((state, button) => {
+  const { total, next } = state;
+  return { total: String(Number(total) + 1), next: next, operation: button }
+})
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+  it('passes state.next if it is defined', () => {
+    const nextVal = 'next';
+    const component = shallow(<App />)
+    component.setState({ next: nextVal, total: 'total'})
+    const displayComponent = component.find('Display')
+    expect(displayComponent.props()).toEqual({ value: nextVal})
+  })
+  it('passes 0 to Display value prop by default', () => {
+    const AppComponent = shallow(<App />);
+    const DisplayComponent = AppComponent.find('Display');
+    expect(DisplayComponent.props()).toEqual({ value: '0' });
+  });
+  it('works with clickHandlers correctly', () => {
+    const buttonVal = '+';
+    const testState = { total: '1' , next: '1', operation: '-'};
+    const returnVal = { total: '2', next: '1', operation: '+' };
+    
+    const AppComponent = shallow(<App />);
+    const { clickHandler } = AppComponent.find('Panel').props();
+    AppComponent.setState(testState);
+    clickHandler(buttonVal);
+    expect(calculate).toHaveBeenCalledTimes(1);
+    expect(calculate).toHaveBeenCalledWith(testState, buttonVal);
+    expect(AppComponent.state()).toEqual(returnVal);
   });
 });
