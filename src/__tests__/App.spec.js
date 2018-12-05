47c3a1a811ab022fd41f96da80f8c982309e719c
Jordan Stoddard <jjsincorporated@gmail.com>
2018-12-05 13:14:22 -0800

    4 tests complete on App component

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer'
 
 import App from '../App';
 
@@ -12,5 +13,14 @@ describe('<App />', () => {
 		const display = wrapper.find('div.component-app')
 		expect(display.length).toBe(1)
 	});
+	it('should match snapshot', () => {
+		const snapshot = renderer.create(<App />).toJSON()
+		expect(snapshot).toMatchSnapshot()
+	})
+	it('state total should be string 0 by default', () => {
+		const wrapper = shallow(<App />)
+		const instance = wrapper.instance()
+		expect(instance.state.total).toBe('0')
+	})
 
 });
