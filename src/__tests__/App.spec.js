18d21d289f8647821ca42fcff2776573e1b37521
Kat Fries <katherynejf@gmail.com>
2018-12-05 16:56:51 -0500

    added tests for classNames and default state for total, next and operation

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
@@ -7,4 +8,30 @@ describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+
+	it('matches snapshot', () => {
+		// it('matches snapshot', () => {
+			const snapshot = renderer.create(<App />).toJSON();
+			expect(snapshot).toMatchSnapshot();
+	});
+
+	it("should have total set to '0' by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.total).toBe('0');
+	});
+	
+	it("should have next set to null by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.next).toBe(null);
+	});
+	
+	it("should have next set to null by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.operation).toBe(null);
+  });
+	
+
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,4 +7,26 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+
+// 	render() {
+// 		const classNames = [
+// 				'component-button',
+// 				this.props.orange ? 'orange' : '',
+// 				this.props.wide ? 'wide' : '',
+// 		];
+// 		return (
+// 				<div className={classNames.join(' ').trim()}>
+// 						<button onClick={this.handleClick}>
+// 								{this.props.name}
+// 						</button>
+// 				</div>
+// 		);
+// }
+
+	it('renders a button with a div with a component-button class', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.find('div.component-button').length).toBe(1)
+	});
 });
+
+// <button onClick={this.toggle} className='toggle-btn'>Toggle</button>
