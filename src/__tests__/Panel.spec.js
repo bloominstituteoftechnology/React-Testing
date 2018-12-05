6702c9502a2f3cd5c27eac574b941ce8afabd11e
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-05 15:32:19 -0600

    panel tests complete

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import Button from '../components/Button/Button.js'
 import Panel from '../components/Panel/Panel';
 
 describe('<Panel />', () => {
 	it('renders without crashing', () => {
 		shallow(<Panel />);
 	});
+
+	it('button component is there', () => {
+		const wrapper = shallow(<Panel />)
+		const button = wrapper.find(<Button />)
+
+		expect(button).toEqual({})
+	})
+
+	it('renders a div with class of component-panel', () => {
+		const wrapper = shallow(<Panel />)
+		const panel = wrapper.find('div.component-panel')
+		expect(panel.length).toEqual(1)
+	})
+
+	it('has handleClick function', () => {
+		const wrapper = shallow(<Panel />)
+		const handleClick = wrapper.find('handleClick()')
+	
+		expect(handleClick).toEqual({})
+	})
+
 });
