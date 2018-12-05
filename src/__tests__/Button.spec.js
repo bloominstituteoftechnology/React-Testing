1e4810e6df695c56976f7b019c01b75822756a30
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-05 16:00:41 -0600

    wrote button tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,4 +7,21 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+
+	it('has a button', () => {
+		const wrapper = shallow(<Button />)
+		const button = wrapper.find('button')
+		expect(button.length).toBe(1)
+	})
+
+	it('has a this.props.name object', () => {
+		const wrapper = shallow(<Button />)
+		expect(wrapper.find('this.props.name')).toEqual({})
+	})
+
+	it('has a handleClick function', () => {
+		const wrapper = shallow(<Button />)
+		expect(wrapper.find('handleClick')).toEqual({})
+	})
+
 });
