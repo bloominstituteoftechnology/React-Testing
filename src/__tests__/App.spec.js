41142998087fb0122a07153b7659fa57cf3127e3
AM_Myrick <amyrick.820@gmail.com>
2018-12-05 15:55:57 -0500

    completes component render tests on App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -7,4 +7,22 @@ describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+
+	it("should have a div with classname component-app", () => {
+		const wrapper = shallow(<App/>);
+		const div = wrapper.find("div.component-app");
+		expect(div.exists()).toBe(true);
+	});
+
+	it("should render a component called Display", () => {
+		const wrapper = shallow(<App/>);
+		const display = wrapper.find('Display');
+		expect(display.exists()).toBe(true);
+	});
+
+	it("should render a component called Panel", () => {
+		const wrapper = shallow(<App/>);
+		const panel = wrapper.find('Panel');
+		expect(panel.exists()).toBe(true);
+	});
 });
