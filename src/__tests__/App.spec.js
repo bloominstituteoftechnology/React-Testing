642dd8ee7b692a9fe03cfaa3f8821d49edf2b924
koennecker <koenneckerb@gmail.com>
2018-12-05 15:19:12 -0700

    Complete tests for App.js

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
 
-import App from '../App';
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
 
-describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+  it("renders a div", () => {
+    expect(shallow(<App />).find(".component-app").length).toBe(1);
+  });
+
+  it("total is 0", () => {
+    expect(shallow(<App />).instance().state.total).toBe("0");
+  });
 });
