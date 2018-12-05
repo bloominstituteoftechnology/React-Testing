cfb7f7314836a927afba160459b369fcca79e01b
bummings <eabummings@gmail.com>
2018-12-05 16:26:58 -0500

    wired up and ready to start writing tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
 describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it.skip('matches snapshot', () => {
+    const snapshot = renderer.create(<App />).toJSON();
+    expect(snapshot).toMatchSnapshot();
+  });
 });
