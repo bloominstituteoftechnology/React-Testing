cddce70155ed8eef02734261ee0ae0645f1fbc8f
AM_Myrick <amyrick.820@gmail.com>
2018-12-05 16:30:21 -0500

    completes functionality tests on App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import App from '../App';
 
-describe('<App />', () => {
+describe('<App /> rendering', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
@@ -25,4 +25,78 @@ describe('<App />', () => {
 		const panel = wrapper.find('Panel');
 		expect(panel.exists()).toBe(true);
 	});
+});
+
+describe('<App /> functionality', () => {
+	it("should update state on button click", () => {
+		const wrapper = shallow(<App/>);
+		const instance = wrapper.instance();
+		let buttons = wrapper.find('Panel').dive().find('Button').at(4).dive().find('button');
+		buttons.simulate('click');
+		expect(instance.state.next).toBe("7");
+
+		buttons = wrapper.find('Panel').dive().find('Button').at(8).dive().find('button');
+		buttons.simulate('click');
+		expect(instance.state.next).toBe("74");
+
+		buttons = wrapper.find('Panel').dive().find('Button').at(5).dive().find('button');
+		buttons.simulate('click');
+		expect(instance.state.next).toBe("748");
+	})
+
+	it("should add and update state", () => {
+		const wrapper = shallow(<App/>);
+		const instance = wrapper.instance();
+		const buttonPress = (index) => {
+			return wrapper.find('Panel').dive().find('Button').at(index).dive().find('button').simulate("click");
+		}
+
+		buttonPress(13);
+		buttonPress(15);
+		buttonPress(13);
+		buttonPress(18);
+		expect(instance.state.total).toBe("4");
+	})
+
+	it("should multiply and update state", () => {
+		const wrapper = shallow(<App/>);
+		const instance = wrapper.instance();
+		const buttonPress = (index) => {
+			return wrapper.find('Panel').dive().find('Button').at(index).dive().find('button').simulate("click");
+		}
+
+		buttonPress(13);
+		buttonPress(7);
+		buttonPress(13);
+		buttonPress(18);
+		expect(instance.state.total).toBe("4");
+	})
+
+	it("should divide and update state", () => {
+		const wrapper = shallow(<App/>);
+		const instance = wrapper.instance();
+		const buttonPress = (index) => {
+			return wrapper.find('Panel').dive().find('Button').at(index).dive().find('button').simulate("click");
+		}
+
+		buttonPress(13);
+		buttonPress(3);
+		buttonPress(13);
+		buttonPress(18);
+		expect(instance.state.total).toBe("1");
+	})
+
+	it("should subtract and update state", () => {
+		const wrapper = shallow(<App/>);
+		const instance = wrapper.instance();
+		const buttonPress = (index) => {
+			return wrapper.find('Panel').dive().find('Button').at(index).dive().find('button').simulate("click");
+		}
+
+		buttonPress(13);
+		buttonPress(11);
+		buttonPress(13);
+		buttonPress(18);
+		expect(instance.state.total).toBe("0");
+	})
 });
