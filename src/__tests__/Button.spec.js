ee4959d3e7835e1f3c00217a10d024ead29f4461
lajawanti <myemail>
2018-12-05 17:50:11 -0500

    Button.spec.js

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Button from '../components/Button/Button';
 
@@ -7,4 +8,17 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<Button />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	});
+
+	it("should render with the classNames of 'component-button orange' if we pass the 'orange' prop from the calling or container component and matches the snapshot", () => {
+		const comp = shallow(<Button orange />);
+		expect(comp.find(".component-button")).toHaveLength(1);
+		expect(comp.find(".orange")).toHaveLength(1);
+		expect(comp).toMatchSnapshot();
+	});
+
 });
