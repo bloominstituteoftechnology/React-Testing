079852ead4e9d30ba1e74c2cbc5555fc0448ec9a
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-05 17:19:06 -0600

    completed tests for display spec js

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -7,4 +7,22 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
+
+	it('should have a div with the classname: "component-display"', () => {
+		const wrapper = shallow(<Display />);
+		
+		expect(wrapper.find("div.component-display").exists()).toBe(true);
+	});
+
+	it('should have a div with the value of "0"', () => {
+		const wrapper = shallow(<Display value={"0"}/>);
+
+		expect(wrapper.find("div.component-display div").text()).toBe("0");
+	});
+
+	it('should have a div with the value of "5"', () => {
+		const wrapper = shallow(<Display value={"5"}/>);
+
+		expect(wrapper.find("div.component-display div").text()).toBe("5");
+	});  
 });
