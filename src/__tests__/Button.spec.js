6424b1ae5a9a7b9da1514293e0c25ca3f1144dc2
Matthew Wright <matthewbwright@gmail.com>
2018-12-05 15:16:50 -0700

    tests button component for div

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -4,7 +4,14 @@ import { shallow } from 'enzyme';
 import Button from '../components/Button/Button';
 
 describe('<Button />', () => {
-	it('renders without crashing', () => {
-		shallow(<Button />);
-	});
+  it('renders without crashing', () => {
+    shallow(<Button />);
+  });
+
+  it('renders a div', () => {
+    const wrapper = shallow(<Button />);
+    const elements = wrapper.find('div');
+
+    expect(elements.length).toEqual(1);
+  });
 });
