7c5f129505b1ed5de8f99d25e3718cf3a833df72
unknown <brandongrych@gmail.com>
2018-12-05 16:49:13 -0600

    tests for app and display done

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import '../setupTests.js';
 
+// Components
 import Button from '../components/Button/Button';
 
 describe('<Button />', () => {
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import '../setupTests.js';
 
+// Components
 import Display from '../components/Display/Display';
 
 describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
+	});
+
+	it('renders a div with a "component-display" class', () => {
+		const wrapper = shallow(<Display />);
+		expect(wrapper.find('div.component-display').length).toBe(1);
+	});
+
+	it('renders another div with class "value" inside the "component-display" class div', () => {
+		const wrapper = shallow(<Display />);
+		expect(wrapper.find('div.component-display>div').length).toBe(1);
+	});
+
+	describe('Display props', () => {
+		it('should render a 0 inside the "value" class div', () => {
+			const wrapper = shallow(<Display value = '0' />);
+			expect(wrapper.find('div.value').text()).toBe('0');
+		});
+
+		it('should render a -23 inside the "value" class div', () => {
+			const wrapper = shallow(<Display value = '-23' />);
+			expect(wrapper.find('div.value').text()).toBe('-23');
+		});
 	});
 });
