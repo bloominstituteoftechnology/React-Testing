8f8dfac6651f0981127cde356618322da2f3c71d
sean-one <basement365@gmail.com>
2018-12-05 14:45:44 -0800

    check for component-panel in Panel component

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -8,6 +8,12 @@ describe('<Panel />', () => {
 		shallow(<Panel />);
 	});
 
+	it('check for component-panel', () => {
+		const wrapper = shallow(<Panel />);
+		const panel = wrapper.find('div.component-panel');
+		expect(panel.length).toBe(1);
+	});
+
 	it('check for all buttons', () => {
 		const wrapper = shallow(<Panel />);
 		const buttons = wrapper.find('Button');
