4eca69ef283f095577837a8afc12c8755b1c0f2d
bkwillia052 <bkwillia052@gmail.com>
2018-12-05 17:56:44 -0600

    Added Button component tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -4,8 +4,25 @@ import { shallow } from 'enzyme';
 import Button from '../components/Button/Button';
 
 describe('<Button />', () => {
-	it('renders without crashing', () => {
-		shallow(<Button />);
+	it('renders shallow without crashing', () => {
+	  shallow(<Button />);
 	});
-	it('')
-});
+	it('renders div with orange', () => {
+	  const wrapper = shallow(<Button orange />);
+	  const buttons = wrapper.find('.orange');
+  
+	  expect(buttons.length).toBe(1);
+	});
+	it('renders div with wide', () => {
+	  const wrapper = shallow(<Button wide />);
+	  const buttons = wrapper.find('.wide');
+  
+	  expect(buttons.length).toBe(1);
+	});
+	it('renders button with prop name', () => {
+	  const wrapper = shallow(<Button name="abcd" />);
+	  const buttons = wrapper.find('button');
+  
+	  expect(buttons.text()).toBe('abcd');
+	});
+  });
