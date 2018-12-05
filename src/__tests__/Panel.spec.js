788de4d02d9a3182e2902bc2d238a8eabb8f1e7e
jsmnglng <jsmn.glng@gmail.com>
2018-12-05 13:07:03 -0800

    Done testing Panel component

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
-import Panel from '../components/Panel/Panel';
+import Panel from "../components/Panel/Panel";
+import Button from "../components/Button/Button";
 
-describe('<Panel />', () => {
-	it('renders without crashing', () => {
-		shallow(<Panel />);
-	});
+describe("<Panel />", () => {
+  it("renders without crashing", () => {
+    shallow(<Panel />);
+  });
+
+  it("should match snapshot", () => {
+    const snapshot = renderer.create(<Panel />).toJSON();
+
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  it("should render div with component-panel class", () => {
+    const wrapper = shallow(<Panel />);
+
+    expect(wrapper.find("div.component-panel").length).toBe(1);
+  });
+
+  it("should render 19 <Button /> components", () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find(Button).length).toBe(19);
+  });
 });
