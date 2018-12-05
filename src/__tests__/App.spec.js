5baa85497c28509b08788589e68f982207878ac3
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-12-05 14:53:45 -0800

    mvp - set up and App.js test

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import {shallow} from "enzyme";
+import renderer from "react-test-renderer";
 
-import App from '../App';
+import App from "../App";
 
-describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it.skip("matches snapshot", () => {
+    const snapshot = renderer.create(<App />).toJSON();
+
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  it("should render a div with component-app class", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find("div.component-app").length).toBe(1);
+  });
+
+  it("total should be zero by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.total).toBe("0");
+  });
+
+  it("should contain a Display component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("Display").length).toBe(1);
+  });
+
+  it("should contain a Panel component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("Panel").length).toBe(1);
+  });
 });
