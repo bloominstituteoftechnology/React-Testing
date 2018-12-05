cdfe833d4a7a4b8866cc2868088633e792c63fff
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-12-05 16:36:03 -0700

    adds button tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -12,5 +12,21 @@ describe('<App />', () => {
 	it.skip('matches snapshot', () => {
 		const snapshot = renderer.create(<App />).toJSON();
 		expect(snapshot).toMatchSnapshot();
-	})
+	});
+
+	it('should have a function called handleClick', () => {
+		const wrapper = shallow(<App />);
+		const button = wrapper.find({ buttonName });
+		wrapper.instance().handleClick();
+		// expect(wrapper.state().toEqual());
+		// button.simulate('click');
+
+	});
+
+	it('should render a div called component-app', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.component-app').length).toBe(1);
+	});
+
+
 });
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,4 +7,14 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+
+	it('should render as `component-button orange` when passed props.orange', () => {
+		const wrapper = shallow(<Button orange/>);
+		expect(wrapper.find('.orange').length).toBe(1);
+	});
+
+	it('should render as `component-button wide` when passed props.wide', () => {
+		const wrapper = shallow(<Button wide/>);
+		expect(wrapper.find('.wide').length).toBe(1);
+	})
 });
