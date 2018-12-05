274fc87d8e3cdec52e904d2dbfca774608fffa95
Carlos Lantigua <clantigua954@gmail.com>
2018-12-05 16:18:57 -0500

    tested the button component

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Panel from '../components/Panel/Panel';
 
@@ -7,4 +8,9 @@ describe('<Panel />', () => {
 	it('renders without crashing', () => {
 		shallow(<Panel />);
 	});
+});
+
+it('matches snapshot', () => {
+	const snapshot = renderer.create(<Panel />).toJSON();
+	expect(snapshot).toMatchSnapshot();
 });
