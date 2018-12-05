844de3361bd8bc21cf7a316c059566e4de08cb18
shawn <shawnantonucci2018@gmail.com>
2018-12-05 13:33:40 -0800

    setup tests for App component

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import Panel from '../components/Panel/Panel';
+import Panel from "../components/Panel/Panel";
 
-describe('<Panel />', () => {
-	it('renders without crashing', () => {
-		shallow(<Panel />);
-	});
+describe("<Panel />", () => {
+  it("renders without crashing", () => {
+    shallow(<Panel />);
+  });
+
+  it("should render a panel", () => {
+    const wrapper = shallow(<Panel />);
+
+    expect(wrapper.find("div.component-panel").length).toBe(1);
+  });
+
+
 });
