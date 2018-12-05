fa51070f694c9d823651a702363e0b40ec524a6a
Brian G <brian.gregorius@gmail.com>
2018-12-05 17:09:12 -0500

    added test to app

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from '../App';
+import Display from '../components/Display/Display';
 
 describe('<App />', () => {
 	it('renders without crashing', () => {
@@ -11,6 +12,24 @@ describe('<App />', () => {
 	it("should have state 'total' of 0", () => {
 		const wrapper = shallow(<App />);
 		const instance = wrapper.instance();
-		expect(instance.state.total).toBe('0');
+		expect(instance.state.total).toEqual('0');
+	});
+
+	it("should have state 'next' of null", () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+		expect(instance.state.next).toBe(null);
+	});
+
+	it("should have state 'operation' of null", () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+		expect(instance.state.operation).toBe(null);
+	});
+
+	it("There should be 19 buttons", () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+		expect(wrapper.find('Panel').dive().find('Button').length).toBe(19);
 	});
 });
