af0ddd8e1af60fc3bf878a96690b9b8c241a5637
rytwalker <rytwalker@gmail.com>
2018-12-05 15:47:39 -0500

    Added tests to check initial state of App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import App from '../App';
 
 describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+  it('should have a state.total that defaults to "0"', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.total).toBe('0');
+  });
+  it('should have a state.next that defaults to null', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.next).toBe(null);
+  });
+  it('should have a state.operation that defaults to null', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.operation).toBe(null);
+  });
 });
