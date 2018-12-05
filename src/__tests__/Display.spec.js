7323aef82106ee241c1672b86c0e9f95f1e24088
SavannahGreuel <smallidgesavannah@gmail.com>
2018-12-05 15:56:16 -0700

    added display testing

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -7,4 +7,25 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
+
+	it('renders a div with className "component-display"', () => {
+		const wrapper = shallow(<Display />);
+
+		expect(wrapper.find('div.component-display').length).toBe(1)
+	});
+
+	it('the component display should have one child element', () => {
+		const wrapper = shallow(<Display />);
+
+		const children = wrapper.find('div.component-display').children();
+
+		expect(children.find('div').length).toBe(1)
+	});
+
 });
+
+/* 
+TESTS
+-renders a div with calssName 'Component-display"
+-the component display should have one child element
+*/
