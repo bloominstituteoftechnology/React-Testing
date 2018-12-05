e31d3e9f179419702586a996ddd09e4bb5109c4a
Yusuf Nafey <yusufnafey@yahoo.com>
2018-12-05 14:51:09 -0800

    app testing complete

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -7,4 +7,29 @@ describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+
+	describe('state', () => {
+		it('total is 0', () => {
+			const wrapper = shallow(<App />);
+			const instance = wrapper.instance();
+			expect(instance.state.total).toBe('0');   
+		});
+	
+		it('next is null', () => {
+			const wrapper = shallow(<App />);
+			const instance = wrapper.instance();
+			expect(instance.state.next).toBe(null);   
+		});
+	
+		it('operation is null', () => {
+			const wrapper = shallow(<App />);
+			const instance = wrapper.instance();
+			expect(instance.state.operation).toBe(null);   
+		});
+	});
+
+	it('2 components in children', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.children().length).toBe(2);
+	});
 });
