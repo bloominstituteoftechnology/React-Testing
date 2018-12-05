88e3fa988d458b91adba8597024f5bb1aa15a516
KHollobaugh <khollobaugh@gmail.com>
2018-12-05 15:15:14 -0500

    added tests to App.spec.js

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import renderer from 'react-test-renderer';
+import Display from '../components/Display/Display';
 import App from '../App';
 
 describe('<App />', () => {
@@ -8,7 +9,26 @@ describe('<App />', () => {
 		shallow(<App />);
 	});
 
-	it('should render Display and Panel components', () => {
-		
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<App />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	})
+
+	it('should default the total to 0', () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+		expect(instance.state.total).toBe('0');
+	})
+
+	it('should default next to null', () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+		expect(instance.state.next).toBe(null);
+	})
+
+	it('should default operation to null', () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+		expect(instance.state.operation).toBe(null);
 	})
 });
