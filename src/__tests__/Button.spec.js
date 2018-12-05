0ff8d7f4e6e336efeae5cc7e90ee8ebc7559d040
koennecker <koenneckerb@gmail.com>
2018-12-05 15:37:03 -0700

    Complete tests for Button.js

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import Button from '../components/Button/Button';
+import Button from "../components/Button/Button";
 
-describe('<Button />', () => {
-	it('renders without crashing', () => {
-		shallow(<Button />);
-	});
+describe("<Button />", () => {
+  it("renders without crashing", () => {
+    shallow(<Button />);
+  });
+  it("renders a div", () => {
+    expect(shallow(<Button />).find("div").length).toBe(1);
+  });
+  it("div has 1 child", () => {
+    expect(shallow(<Button />).find("div").children.length).toBe(1);
+  });
+  it("renders a button", () => {
+    expect(shallow(<Button />).find("button").length).toBe(1);
+  });
 });
