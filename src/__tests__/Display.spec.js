39b69ffe17828265a81fc5a5caa53aaf7ba0a6fc
Carlos Lantigua <clantigua954@gmail.com>
2018-12-05 17:31:03 -0500

    tested 9 items in two components

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -8,9 +8,27 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
-});
 
-it('matches snapshot', () => {
-	const snapshot = renderer.create(<Display />).toJSON();
-	expect(snapshot).toMatchSnapshot();
+	it('matches snapshot', () => {
+		const snapshot = renderer.create(<Display />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	});
+
+	it('should render a display without crashing', () => {
+		const wrapper = shallow(<Display />);
+		const display = wrapper.find('div');
+		expect(display.length).toBe(2);
+	}); // should render 2 divs for display
+
+	it('should render a div with a classname of conmponent display', () => {
+		const wrapper = shallow(<Display />);
+		const display = wrapper.find('.component-display');
+		expect(display.length).toBe(1);
+	});
+
+	it('should render a div with className="component-display"', () => {
+		const wrapper = shallow(<Display />);
+		const display = wrapper.find('.component-display');
+		expect(display.length).toBe(1);
+	});
 });
