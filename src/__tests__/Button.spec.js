0299fb755cdb567f0dccf6072a27ac4999e74a86
Michael Checo <checomichael2@gmail.com>
2018-12-05 16:29:55 -0500

    tests for Button component

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -12,4 +12,27 @@ describe('<Button />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+  it('executes clickHandler with names prop', () => {
+    let name = 'blob';
+    let output;
+    function clickHandler(input) {
+      output = input;
+    }
+    const button = shallow(<Button name={name} clickHandler={clickHandler} />)
+    const divButton = button.find('.component-button > button');
+    divButton.simulate('click');
+    expect(output).toEqual(name);
+  })
+  it('receives classNames from props', () => {
+    const button = shallow(<Button orange wide />);
+    const divButton = button.find('.component-button');
+    expect(divButton.hasClass('orange')).toEqual(true);
+    expect(divButton.hasClass('wide')).toEqual(true);
+  })
+  it('If no orange or wide props are passed button only has component-button class', () => {
+    const button = shallow(<Button />);
+    const divButton = button.find('.component-button');
+    expect(divButton.hasClass('orange')).toEqual(false);
+    expect(divButton.hasClass('wide')).toEqual(false);
+  })
 });
