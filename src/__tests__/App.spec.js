57edf66c00429c140fd50fabbd1040c1e69a43c1
Elvis Ibarra <ielvisd@gmail.com>
2018-12-05 15:20:23 -0800

    Test added to App.spec.js that tests if a div with 'component-app' class renders

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import App from '../App';
 
 describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('matches snapshot', () => {
+    const snapshot = renderer.create(<App />).toJSON();
+
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  it('should render an div with a component-app class', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('div.component-app').length).toBe(1);
+  });
 });
