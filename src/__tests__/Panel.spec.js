a10f1e7826e97783528b8485adbd5f478088ab7e
Matthew Wright <matthewbwright@gmail.com>
2018-12-05 16:16:45 -0700

    checks for component-panel div

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -4,7 +4,17 @@ import { shallow } from 'enzyme';
 import Panel from '../components/Panel/Panel';
 
 describe('<Panel />', () => {
-	it('renders without crashing', () => {
-		shallow(<Panel />);
-	});
+  it('renders without crashing', () => {
+    shallow(<Panel />);
+  });
+
+  it('renders a component-panel div', () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find('div.component-panel').length).toBe(1);
+  });
+
+  it('renders the buttons of the calculator', () => {
+    const wrapper = shallow(<Panel />);
+    expect(wrapper.find('Button').length).toBe(19);
+  });
 });
