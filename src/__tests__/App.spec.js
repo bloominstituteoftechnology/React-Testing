d8174d073de20280467e8ac4d96ec26809545434
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-12-05 17:24:45 -0500

    added snapshot

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 
 import App from "../App";
 
@@ -31,5 +32,9 @@ describe("App Component", () => {
     const app = shallow(<App />);
     const headers = app.find("Panel");
     expect(headers.length).toEqual(1);
+  });
+  it("matches snapshot", () => {
+    const snapshot = renderer.create(<App />).toJSON();
+    expect(snapshot).toMatchSnapshot();
   });
 });
