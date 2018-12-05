1556fe68c7751cbb0b70239c4768ce77f9c251aa
koennecker <koenneckerb@gmail.com>
2018-12-05 15:41:54 -0700

    Complete tests for Display.js

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
 
-describe('<Display />', () => {
-	it('renders without crashing', () => {
-		shallow(<Display />);
-	});
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    shallow(<Display />);
+  });
+  it("renders the display", () => {
+    expect(shallow(<Display />).find(".component-display").length).toBe(1);
+  });
+  it("display has one child", () => {
+    expect(
+      shallow(<Display />).find(".component-display").children.length
+    ).toBe(1);
+  });
 });
