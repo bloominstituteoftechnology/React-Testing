ffb6e59e5e6a85dcabf7ea5b08ce93df2d01fb1b
SavannahGreuel <smallidgesavannah@gmail.com>
2018-12-05 16:06:28 -0700

    added panel testing, MVP Complete

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -7,4 +7,22 @@ describe('<Panel />', () => {
 	it('renders without crashing', () => {
 		shallow(<Panel />);
 	});
+
+	it('should render 1 overall div with classname : component-panel', () => {
+		const wrapper = shallow(<Panel />)
+		expect(wrapper.find('div.component-panel').length).toBe(1)
+
+	});
+
+	it('should render 19 button components', () => {
+		const wrapper = shallow(<Panel />)
+		const buttons = wrapper.find('Button');
+		expect(buttons.length).toBe(19)
+
+	});
 });
+/* 
+TESTS
+-renders one overall class: component-panel
+-should render 19 button components
+*/
