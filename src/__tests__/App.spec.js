665efa795881558bedfb99ec255cd2b8b12a2e99
adamhinckley <adamhinckley@mac.com>
2018-12-05 15:00:22 -0700

    Some tests on App.js written

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
-import App from '../App';
+import App from "../App";
 
-describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("matches snapshot", () => {
+    const snapshot = renderer.create(<App />).toJSON();
+
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  it("should default to 0", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.total).toBe("0");
+  });
+
+  it("should default to null", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(instance.state.next).toBe(null);
+  });
 });
