73f0087232cd5d9aa13881c91dc90ecd7fda5798
Gannon <gkleblanc@gmail.com>
2018-12-05 17:07:32 -0500

    added threer tests to app.spec.js and made sure they were all passing

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -8,3 +8,25 @@ describe('<App />', () => {
 		shallow(<App />);
 	});
 });
+
+it('should render a div with component-app class', () => {
+	const wrapper = shallow(<App />);
+	expect(wrapper.find('div.component-app').length).toBe(1);
+});
+it('should start at zero', () => {
+	const wrapper = shallow(<App />);
+	const instance = wrapper.instance();
+	expect(instance.state.total).toBe('0');
+});
+it('should pass 0 to Display component at instantiation', () => {
+	const wrapper = shallow(<App />);
+	const display = wrapper.find('Display');
+	expect(display.props()).toEqual({ value: '0' });
+})
+it('should pass state.total', () => {
+	const wrapper = shallow(<App />);
+	const testTotal = 'testTotal';
+	wrapper.setState({ total: testTotal });
+	const display = wrapper.find('Display');
+	expect(display.props()).toEqual({ value: testTotal });
+});
