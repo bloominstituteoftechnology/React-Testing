b5faa8194978a1e2b58210d22d3554b37bbeff60
Rahul Desai <rd825@georgetown.edu>
2018-12-05 16:04:00 -0500

    Completed display tests

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -7,4 +7,15 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
+
+	it('renders a div with class component display', () => {
+		const wrapper = shallow(<Display />);
+		expect(wrapper.find('div.component-display').length).toBe(1);
+	});
+
+	it('renders a child div inside the main div', () => {
+		const wrapper = shallow(<Display />);
+		const children = wrapper.find('div.component-display').children();
+		expect(children.find('div').length).toBe(1);
+	});
 });
