d2f1213cebd93ba4f9b98e30a37fe9539f5eb612
Hunter315 <hunter.mckay.smith@gmail.com>
2018-12-05 13:14:05 -0700

    basic app shallow test on Display and Panel

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import React from "react";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
+import Display from "../components/Display/Display";
+import Panel from "../components/Panel/Panel";
 
-import App from '../App';
+import App from "../App";
 
-describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+  it("renders Display without crashing", () => {
+    shallow(<Display />);
+  });
+  it("renders Panel without crashing", () => {
+    shallow(<Panel />);
+  });
 });
