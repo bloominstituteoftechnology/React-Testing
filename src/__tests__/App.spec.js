00646a0605f0f01768198da1f52ba5482a4da5eb
Brad Mortensen <b.mort86@gmail.com>
2018-12-05 14:30:56 -0700

    Adds tests for React Components

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -22,5 +22,5 @@ describe('<App />', () => {
     const instance = wrapper.instance();
 
     expect(instance.state.total).toBe('0');
-  });
+    });
 });
--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -7,4 +7,14 @@ describe('<Panel />', () => {
 	it('renders without crashing', () => {
 		shallow(<Panel />);
 	});
+	it('should render a div with classname component-panel', () => {
+    const wrapper = shallow(<Panel />);
+		//returns an array with one div 
+    expect(wrapper.find('div.component-panel').length).toBe(1);
+	});
+	it('should render 19 button components with different functionality', () => {
+    const wrapper = shallow(<Panel />);
+		//returns an array with the 19 buttons of the calculator app
+    expect(wrapper.find('Button').length).toBe(19);
+	});
 });
