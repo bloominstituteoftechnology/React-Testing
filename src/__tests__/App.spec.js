c87f3f1a77fda0e4a11ba6381d0d270f0e1ae6ea
Drewsup123c <drewsup123c@gmail.com>
2018-12-05 15:08:04 -0700

    finished 1-2 tests for all components

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -7,4 +7,12 @@ describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+	it('Renders Div with display', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Display').length).toBe(1)
+	})
+	it('Renders Div with panel', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Panel').length).toBe(1)
+	})
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow,mount } from 'enzyme';
 
 import Display from '../components/Display/Display';
 
 describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
+	});
+
+	it('Displays correctly when props change', () => {
+		const wrapper = mount(<Display value="0" />);
+		expect(wrapper.props().value).toBe('0');
+		wrapper.setProps({ value: '123' });
+		expect(wrapper.props().value).toBe('123');
 	});
 });
