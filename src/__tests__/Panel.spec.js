d465eb1fcac4e9fa28b49c21d3805fbce73069df
Katie Cleary <katiecleary@airmaster.lan>
2018-12-05 17:32:32 -0500

    added panel.js tests for rendering, handler, and component length

--- src/__tests__/Panel.spec.js
+++ src/__tests__/Panel.spec.js
@@ -6,19 +6,23 @@ describe('<Panel />', () => {
 	it('renders without crashing', () => {
 		shallow(<Panel />);
 	});
+	it('handleClick function is present', () => {
+		const wrapper = shallow(<Panel />);
+		const instance = wrapper.instance().handleClick;
+		expect(instance).toBeInstanceOf(Function);
+	});
+	it.skip('matches snapshot', () => {
+		const snapshot = renderer.create(<Panel />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	});
+	it('check for component-panel class', () => {
+		const wrapper = shallow(<Panel />);
+		const componentPanel = wrapper.find('div.component-panel');
+		expect(componentPanel.length).toBe(1);	
+	});
+	it('renders all 19 Button components for the calculator', () => {
+		const wrapper = shallow(<Panel />);
+		expect(wrapper.find('Button').length).toBe(19);
+
+	});
 });
-/*
-
-- does it render without blowing up
-- does it render the right UI
-- state/props handled correctly
-- events/methods working correctly
-
-Specifications
-
-- renders the display
-- should be "off" by default
-- should have a toggle button
-- clicking the toggle button toggles the switch
-- should display the switch's state
-*/
