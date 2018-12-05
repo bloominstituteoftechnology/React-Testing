c6606efa917a0784e5e9198476b1eaf1dee2b2fb
Yusuf Nafey <yusufnafey@yahoo.com>
2018-12-05 15:12:27 -0800

    added button tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,4 +7,22 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+
+	it('renders div', () => {
+		const wrapper = shallow(<Button />);
+		expect(wrapper.find('div').length).toBe(1);
+	});
+	
+	it('has class name component-button', () => {
+		const wrapper = shallow(<Button />);
+		expect(wrapper.find('div.component-button').length).toBe(1);
+	});
+
+	it('class props passed down', () => {
+		const orange = shallow(<Button orange />);
+		const wide = shallow(<Button wide />);
+
+		expect(orange.find('.orange').exists()).toBe(true);
+		expect(wide.find('.wide').exists()).toBe(true);
+	});
 });
