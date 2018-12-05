f283fe95e208d2edf972d61d7ad68ba9a6e18956
samsisle <samko9522@gmail.com>
2018-12-05 14:03:32 -0800

    Finish App.spec tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -4,7 +4,14 @@ import { shallow } from 'enzyme';
 import App from '../App';
 
 describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('should correctly render initial state', () => {
+    const inst = shallow(<App />).instance();
+    expect(inst.state.total).toBe('0');
+    expect(inst.state.next).toBe(null);
+    expect(inst.state.operation).toBe(null);
+  });
 });
