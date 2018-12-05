a92188442ba45b5b6b7868d66edb2a45bfa37908
Edward J <edjeong99@gmail.com>
2018-12-05 18:08:38 -0500

    init

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import renderer from 'react-test-renderer';
 import Button from '../components/Button/Button';
 
 describe('<Button />', () => {
