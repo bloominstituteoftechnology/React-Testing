9cdd5b87b9e8aa58c390fc3399335936685c34f7
JacobABrennan <jacobabrennan@gmail.com>
2018-12-05 18:50:13 -0500

    Add tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,4 +7,46 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+    test('handles clicks', () => {
+        const clickTracker = jest.fn();
+        const buttonWrapper = shallow(
+            <Button
+                clickHandler={clickTracker}
+                name="test"
+            />,
+        );
+        const innerButton = buttonWrapper.find('button');
+        innerButton.simulate('click');
+        expect(clickTracker).toHaveBeenCalledTimes(1);
+    });
+    test('can be orange (or not)', () => {
+        expect(
+            shallow(<Button orange />)
+            .hasClass('component-button orange')
+        );
+        expect(
+            shallow(<Button />)
+            .hasClass('component-button orange')
+        ).toBeFalsy();
+    });
+    test('can be wide (or not)', () => {
+        expect(
+            shallow(<Button wide />)
+            .hasClass('component-button wide')
+        );
+        expect(
+            shallow(<Button />)
+            .hasClass('component-button wide')
+        ).toBeFalsy();
+    });
+    test('can be wide and orange', () => {
+        expect(
+            shallow(<Button wide orange />)
+            .hasClass('component-button orange wide')
+        );
+        expect(
+            shallow(<Button orange wide />)
+            .hasClass('component-button orange wide')
+        );
+    });
 });
--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Display from '../components/Display/Display';
 
 describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
-	});
+    });
+    test('matches snapshot', () => {
+        const instance = renderer.create(
+            <Display value="test string" />,
+        );
+        const snapshotJson = instance.toJSON();
+        expect(snapshotJson).toMatchSnapshot();
+    });
+    test('random text', () => {
+        const testContents = `Test: ${Math.random()}`;
+        const displayWrapper = shallow(
+            <Display value={testContents} />,
+        );
+        expect(displayWrapper.text()).toBe(testContents);
+    });
+    test('has correct className', () => {
+        expect(
+            shallow(<Display value="~" />)
+            .hasClass('component-display')
+        ).toBeTruthy();
+    });
 });
