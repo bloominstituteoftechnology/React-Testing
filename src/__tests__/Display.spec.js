bd29757357ecc9eab592145ddbc3c02813ae3e73
DaftBeowulf <ajmboosh@gmail.com>
2018-12-05 15:01:59 -0600

    initial pull request

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import Display from '../components/Display/Display';
+import Display from "../components/Display/Display";
 
-describe('<Display />', () => {
-	it('renders without crashing', () => {
-		shallow(<Display />);
-	});
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    shallow(<Display />);
+  });
+
+  it('has the prop "value" being passed down to it', () => {
+    const wrapper = shallow(<Display />);
+    const instance = wrapper.instance();
+
+    expect(instance.props.length).toBe(1);
+  });
 });
+
+/**
+ * 1) accepts a prop 'value' from App.js
+ * 2) displays that value in a div
+ * 3) //TODO:???
+ */
