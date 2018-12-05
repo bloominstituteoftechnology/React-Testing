7f69b9a7280e4a0c2e832087b28b80e3ee9779fe
Brad Mortensen <b.mort86@gmail.com>
2018-12-05 13:43:59 -0700

    Builds Test for App Component

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -7,4 +7,20 @@ describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+	it.skip('matches snapshot', () => {
+    const snapshot = renderer.create(<App />).toJSON();
+
+    expect(snapshot).toMatchSnapshot();
+	});
+	it('should render a div with class component-app which holds Display an Panel component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('div.component-app').length).toBe(1);
+	});
+	it('should be 0 by default', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.total).toBe('0');
+  });
 });
