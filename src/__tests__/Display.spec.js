88086dc4f855917df9a0050c8e9ed1d3cae5e066
Edward J <edjeong99@gmail.com>
2018-12-05 18:40:04 -0500

    button, display test done

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -7,4 +7,20 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
+
+
+	it('renders the div with correct class name', () => {
+		const wrapper = shallow(<Display />);
+		const display = wrapper.find('.component-display');
+		expect(display.exists()).toEqual(true);
+	});
+
+
+
+	it('check the prop', () => {
+		const wrapper = shallow(<Display />);
+		expect(wrapper.props().value).toBe(undefined);});
+
+
+
 });
