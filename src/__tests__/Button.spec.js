536189e2b619feac826c84693bd2aa2ad8e6337c
Gannon <gkleblanc@gmail.com>
2018-12-05 17:41:42 -0500

    added tests for buttons

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -8,3 +8,27 @@ describe('<Button />', () => {
 		shallow(<Button />);
 	});
 });
+
+it('renders with component button class if no display props are passed to it', () => {
+	const wrapper = shallow(<Button />);
+	const buttonDiv = wrapper.find('.component-button');
+	expect(buttonDiv.hasClass('orange')).toBe(false);
+	expect(buttonDiv.hasClass('wide')).toBe(false);
+});
+it('executes clickHandler', () => {
+	const name = 'testClick';
+	let output;
+	function clickHandler(input) {
+		output = input;
+	};
+	const wrapper = shallow(<Button name={name} clickHandler={clickHandler} />);
+	const buttonDiv = wrapper.find('.component-button > button');
+	buttonDiv.simulate('click');
+	expect(output).toBe(name);
+});
+it('receives class name as props', () => {
+	const wrapper = shallow(<Button orange wide />);
+	const buttonDiv = wrapper.find('.component-button');
+	expect(buttonDiv.hasClass('orange')).toBe(true);
+	expect(buttonDiv.hasClass('wide')).toBe(true);
+});
