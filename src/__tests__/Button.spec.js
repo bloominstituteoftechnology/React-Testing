f6e363ae5a25ed6ff08ec7ca4461691e77a740ad
Rahul Desai <rd825@georgetown.edu>
2018-12-05 15:52:01 -0500

    Completed button tests

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,4 +7,19 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+
+	it('has appropriate props', () => {
+		const wrapper = shallow(<Button />);
+		if (wrapper.props().orange) {
+			expect(wrapper.props().orange).toBe('orange')
+		}
+		if (wrapper.props().wide) {
+			expect(wrapper.props().wide).toBe('wide')
+		}
+	});
+
+	it('has a click handler passed as props', () => {
+		const wrapper = shallow(<Button />);
+		expect(wrapper.find('.component-button').length).toBe(1);
+	});
 });
