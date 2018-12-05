acc61187e39bf6c4d52712e493d1302a471f64eb
Jordan Stoddard <jjsincorporated@gmail.com>
2018-12-05 12:58:58 -0800

    two component tests on App.spec

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -7,4 +7,10 @@ describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+	it('should render div with className component-app', () => {
+		const wrapper = shallow(<App />)
+		const display = wrapper.find('div.component-app')
+		expect(display.length).toBe(1)
+	});
+
 });
