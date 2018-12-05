b0a71bd050973b183db718752cf30672e31c5f01
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-12-05 17:21:26 -0500

    tests Display component for correct render, initial display value, and value changing when new props are received

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 
+import App from '../App';
 import Display from '../components/Display/Display';
 
 describe('<Display />', () => {
@@ -14,4 +15,24 @@ describe('<Display />', () => {
 
 		expect(snapshot).toMatchSnapshot();
 	});
+
+	it('should render div within div.component-display', () => {
+		const wrapper = shallow(<Display />);
+		expect(wrapper.find('div').length).toBe(2);
+		expect(wrapper.find('div.component-display').length).toBe(1);
+		expect(wrapper.find('div.component-display div').length).toBe(1);
+	});
+
+	it('initial display should be 0', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find(Display).find('.component-display div').text()).toBe('0');
+		wrapper.unmount();
+	});
+
+	it('displayed value should change when props are received', () => {
+		const wrapper = shallow(<Display value="value" />);
+		expect(wrapper.text()).toBe('value');
+		wrapper.setProps({ value: 'new value' });
+		expect(wrapper.text()).toBe('new value');
+	})
 });
