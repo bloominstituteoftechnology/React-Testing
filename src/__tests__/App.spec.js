982c6109a381d473b312bfd380996147e5b6673b
Brian G <brian.gregorius@gmail.com>
2018-12-05 16:55:14 -0500

    added first test of app

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -7,4 +7,10 @@ describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
 	});
+
+	it("should have state 'total' of 0", () => {
+		const wrapper = shallow(<App />);
+		const instance = wrapper.instance();
+		expect(instance.state.total).toBe('0');
+	});
 });
