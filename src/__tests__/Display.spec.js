180c68cca8a280b40affb6d0ec78d177093c2eb0
Matthew Wright <matthewbwright@gmail.com>
2018-12-05 15:59:47 -0700

    checks for a child div inside of the div

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -18,4 +18,10 @@ describe('<Display />', () => {
     let display = wrapper.find('.component-display div');
     expect(display.text()).toBe('3');
   });
+
+  it('renders a div with a child div', () => {
+    const wrapper = shallow(<Display />);
+    const children = wrapper.find('div.component-display').children();
+    expect(children.find('div').length).toBe(1);
+  });
 });
