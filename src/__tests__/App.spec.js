ce89cde5ab2b8c2e465e4c6ce483d246f868cfa6
Peter Pham <phamhere@gmail.com>
2018-12-05 15:29:19 -0700

    added tests for App

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import App from '../App';
+import App from "../App";
 
-describe('<App />', () => {
-	it('renders without crashing', () => {
-		shallow(<App />);
-	});
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+  it("should render a div with component-app class", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div.component-app").length).toBe(1);
+  });
+  it("state should be total:0,next:null,operation:null by default", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.total).toBe("0");
+    expect(instance.state.next).toBe(null);
+    expect(instance.state.operation).toBe(null);
+  });
+  it("should pass 0 to Display component at instantiation", () => {
+    const wrapper = shallow(<App />);
+    const display = wrapper.find("Display");
+
+    expect(display.props()).toEqual({ value: "0" });
+  });
 });
