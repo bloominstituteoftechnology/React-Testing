280bf38e4b45ee8227ae0c211e0ff5937353a602
Katie Cleary <katiecleary@airmaster.lan>
2018-12-05 17:19:11 -0500

    added button.js tests for rendering, UI, handler, and props

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -7,19 +7,33 @@ describe('<Button />', () => {
 	it('renders without crashing', () => {
 		shallow(<Button />);
 	});
+
+	it.skip('matches snapshot', () => {
+		const snapshot = renderer.create(<Button />).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	});
+	
+	it('handleClick function is present', () => {
+		const wrapper = shallow(<Button />);
+		const instance = wrapper.instance().handleClick;
+		expect(instance).toBeInstanceOf(Function);
+	});
+	describe('props are being passed down correctly into button', () => {
+	it('name - props being passed correctly', () => {
+		const wrapper = shallow(<Button name="AC" />);	
+		const buttonProp = wrapper.find('button');
+		expect(buttonProp.text()).toEqual('AC');
 });
-/*
+it('orange - class - props being passed correctly', () => {
+	const wrapper = shallow(<Button orange />);	
+	const buttonProp = wrapper.find('.orange');
+	expect(buttonProp).toHaveLength(1)
+});
+it('wide- class - props being passed correctly', () => {
+	const wrapper = shallow(<Button wide />);	
+	const buttonProp = wrapper.find('.wide');
+	expect(buttonProp).toHaveLength(1);
+});
 
-- does it render without blowing up
-- does it render the right UI
-- state/props handled correctly
-- events/methods working correctly
-
-Specifications
-
-- renders the display
-- should be "off" by default
-- should have a toggle button
-- clicking the toggle button toggles the switch
-- should display the switch's state
-*/
+});
+});
