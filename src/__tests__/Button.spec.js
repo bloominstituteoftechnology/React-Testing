ed0c78318e653f4778e49e64f1473f5c900f0312
Trevor Lewis <tenzingtitan@gmail.com>
2018-12-06 13:04:52 -0700

    Late, but MVP finished

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import Button from '../components/Button/Button';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('<Button />', () => {
-	it('renders without crashing', () => {
-		shallow(<Button />);
-	});
+  it('renders without crashing', () => {
+    shallow(<Button />);
+  });
+  
+  it('renders div with orange', () => {
+    const wrapper = shallow(<Button orange />);
+    const buttons = wrapper.find('.orange');
+     expect(buttons.length).toBe(1);
+  });
+
+  it('renders div with wide', () => {
+    const wrapper = shallow(<Button wide />);
+    const buttons = wrapper.find('.wide');
+     expect(buttons.length).toBe(1);
+  });
+
+  it('renders button with prop name', () => {
+    const wrapper = shallow(<Button name="abcd" />);
+    const buttons = wrapper.find('button');
+     expect(buttons.text()).toBe('abcd');
+  });	  
 });
