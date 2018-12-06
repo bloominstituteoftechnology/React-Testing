ebb8a1f8242c857760ade40333d2a979d7cf353e
ssmodish <smodish@hotmail.com>
2018-12-05 19:08:13 -0500

    having trouble thinking of how to test these

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -4,7 +4,19 @@ import { shallow } from 'enzyme';
 import Button from '../components/Button/Button';
 
 describe('<Button />', () => {
-	it('renders without crashing', () => {
-		shallow(<Button />);
-	});
+  it('renders without crashing', () => {
+    shallow(<Button />);
+  });
+
+  it('needs to be tested', () => {
+
+  });
+
+  it('over and over', () => {
+
+  });
+
+  it('and over again', () => {
+
+  });
 });
