ebae8dc95594321d26033d22969fb4cf141136d2
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-12-05 17:00:58 -0700

    adds display tests

--- src/__tests__/Display.spec.js
+++ src/__tests__/Display.spec.js
@@ -7,4 +7,18 @@ describe('<Display />', () => {
 	it('renders without crashing', () => {
 		shallow(<Display />);
 	});
+
+	it('should have a div with className `component-display', () => {
+		const wrapper = shallow(<Display />);
+		expect(wrapper.find('.component-display').length).toBe(1);
+	});
+
+	it('should return a value', () => {
+		const wrapper = shallow(<Display />);
+		expect(wrapper.contains(
+			<div>
+				
+			</div>
+		))
+	})
 });
